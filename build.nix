6a9426f8c00bafcc6e1852c9b617bf94f61e934d
Cole Mickens <cole.mickens@gmail.com>
2018-10-24 00:23:43 -0700

    reorganize

--- build.nix
+++ build.nix
@@ -1,18 +1,9 @@
 let
-  # import nixos-unstable
-  # this is for visibility so you can see what versions of nixpkgs are prebuilt
-
-  #nixpkgs = (import ./nixpkgs); # TODO: remove this when nixos-unstable advances and has `sway-beta`
-
-  nixpkgs =
-    if builtins.pathExists /etc/nixpkgs
-    then /etc/nixpkgs
-    else (import ./nixpkgs);
-
+  nixpkgs = (import ./nixpkgs);
   pkgs = import nixpkgs {
     overlays = [ (import ./default.nix) ];
   };
 
 in
-  with pkgs; [ swaypkgs ]
+  pkgs.swaypkgs
 
--- default.nix
+++ default.nix
@@ -1,27 +1,18 @@
 self: super:
-
 let
-  wlroots          = super.callPackage ./wlroots { inherit self super; };
-  sway-beta        = super.callPackage ./sway-beta { inherit self super; };
-  grim             = super.callPackage ./grim {};
-  slurp            = super.callPackage ./slurp {};
-  wlstream         = super.callPackage ./wlstream {};
-  waybar           = super.callPackage ./waybar {};
-  redshift-wayland = super.callPackage ./redshift-wayland {
-    inherit (super.python3Packages) python pygobject3 pyxdg wrapPython;
-    geoclue = super.geoclue2;
+swaypkgs = {
+  fmt              = self.callPackage ./fmt { inherit self super; };
+  wlroots          = self.callPackage ./wlroots {};
+  sway-beta        = self.callPackage ./sway-beta {};
+  grim             = self.callPackage ./grim {};
+  slurp            = self.callPackage ./slurp {};
+  wlstream         = self.callPackage ./wlstream {};
+  waybar           = self.callPackage ./waybar {};
+  redshift-wayland = self.callPackage ./redshift-wayland {
+    inherit (self.python3Packages) python pygobject3 pyxdg wrapPython;
+    geoclue = self.geoclue2;
   };
-
-  swaypkgs = {
-    inherit wlroots;
-    inherit sway-beta;
-    inherit grim;
-    inherit slurp;
-    inherit wlstream;
-    inherit waybar;
-    inherit redshift-wayland;
-  };
+};
 in
-  # also expose the list of packages as a package for
-  # easy building from `build.nix`.
   swaypkgs // { inherit swaypkgs; }
+
