18a8b5f39af2090a1bec9a9b8f705f7c1db6ac03
Cole Mickens <cole.mickens@gmail.com>
2018-10-24 01:37:16 -0700

    fmt: builds using clean upstream patch

--- fmt/default.nix
+++ fmt/default.nix
@@ -1,7 +1,15 @@
 { self, super }:
 
 let
+  rev = "57ae5189351665715c98b3b6ca8595b30d83033f";
+  sha256 = "1gy93mb1s1mq746kxj4c564k2mppqp5khqdfa6im88rv29cvrl4y";
 in
   super.fmt.overrideAttrs (old: rec {
-    patches = [ ./0001-add-fmt.pc.patch ];
+    patches = [
+      (self.pkgs.fetchpatch {
+        url = "https://github.com/fmtlib/fmt/pull/916/commits/${rev}.patch";
+        sha256 = "${sha256}";
+      })
+    ];
   })
+
