b92cb9e63765d8ecaa60c89c371c2abd70b9ff0a
Cole Mickens <cole.mickens@gmail.com>
2018-11-12 02:56:57 -0800

    autoupdates

--- sway-beta/metadata.nix
+++ sway-beta/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "80a1c340a9c8c8aac5fe4dd7bc1dc15b43fce0dc";
-  sha256 = "13h8facfg0bsmsg6aqy97y27zggvcijn9pf9h6fz3agkzxmj1vqw";
-  revdate = "2018-11-10T13:06:11Z";
+  rev = "ee6b0ce24ae74cfae5d41985cacdff6065c8f534";
+  sha256 = "1s478s0zrbpam9xfp7mcclppwglqqh5j1kv4h7jhqmgrdk45p0jr";
+  revdate = "2018-11-11T21:16:28Z";
 }
