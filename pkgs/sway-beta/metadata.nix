2b4abd86a1d24c7cdf84be57034551ad9d8ae2a1
Cole Mickens <cole.mickens@gmail.com>
2019-01-06 18:15:04 -0800

    update: mako, sway-beta, wlroots
    Signed-off-by: Cole Mickens <cole.mickens@gmail.com>

--- pkgs/sway-beta/metadata.nix
+++ pkgs/sway-beta/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "bb5b168b6fd8f304b575746abf3a36c066bb6c01";
-  sha256 = "08s2qv6nhrfnh83c0fgb66b1w3147ks2n3psac365sxni7iq5zq0";
-  revdate = "2019-01-05T20:21:11Z";
+  rev = "688f4137a573d87f591c86edafdaf7c0f4d0aee4";
+  sha256 = "0pajnhy8qsfv0icwli5d9kvw0ch70lwz0kb41lw769q13q5vk4hn";
+  revdate = "2019-01-06T12:59:39Z";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "aebd74243f8f23a3a6a0f82531977b569629ebe2";
-  sha256 = "19li614dyv8p11wdcya4hhp8b6gnj186hlfxmyvhkb7gldhqffnb";
-  revdate = "2019-01-04T15:02:33Z";
+  rev = "e1eceddecff801c7bd7b113704b74df4c6a63ca6";
+  sha256 = "18jfv616p33yivcnfhd5hxp03x4c0dkvrpgcmq35yfr9mi05cm8k";
+  revdate = "2019-01-06T13:39:20Z";
 }
