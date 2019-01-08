a9d285ea061db41c2a740299d13c42aa5fc36df0
Cole Mickens <cole.mickens@gmail.com>
2019-01-08 13:33:14 -0800

    update:: wlroots, sway-beta, wl-clipboard, wmfocus
    Signed-off-by: Cole Mickens <cole.mickens@gmail.com>

--- pkgs/wmfocus/metadata.nix
+++ pkgs/wmfocus/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "d828eb712debbec1ae3d67d7a571e6e6a1c004dd";
-  sha256 = "0k964p5rxj3jcl42hs2ff1np5mlw3llyy5lc6490hilqb8680914";
-  revdate = "2018-12-13T23:02:22Z";
+  rev = "7cdbd7f6dabe2932828886ffad05a90df3555e3b";
+  sha256 = "0jx0h2zyghs3bp4sg8f3vk5rkyprz2dqfqs0v72vmkp3cvgzxbvs";
+  revdate = "2019-01-08T11:15:20Z";
 }
