350727f67a99b318648b60cd02d55a26885ac038
Cole Mickens <cole.mickens@gmail.com>
2019-01-31 11:28:50 -0800

    update: sway-beta, wayfire
    Signed-off-by: Cole Mickens <cole.mickens@gmail.com>

--- nixpkgs/nixos-unstable/metadata.nix
+++ nixpkgs/nixos-unstable/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "bc41317e24317b0f506287f2d5bab00140b9b50e";
-  sha256 = "1skl7icprfdq9wj1nivqyhylimgq7vy950h2m1lpbz2xjb3dk15k";
-  revdate = "2019-01-20T17:32:34Z";
+  rev = "f2a1a4e93be2d76720a6b96532b5b003cc769312";
+  sha256 = "1yjk6ffnm6ahj34yy2q1g5wpdx0m1j7h8i4bzn4x78g9chb0ppy4";
+  revdate = "2019-01-30T03:43:03Z";
 }
--- pkgs/sway-beta/metadata.nix
+++ pkgs/sway-beta/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "aee9942145119444e5592846ac7e1a0d89e8a3e0";
-  sha256 = "1191q7mr5almacffqmnb9imdgbs4c08jqiwl9vgl313bdjxrgb3i";
-  revdate = "2019-01-30T21:55:22Z";
+  rev = "204e1f47122d5a48865802dd1fb94fe0f05c2401";
+  sha256 = "0jai305fzf93z45c2la14rlz946jwl2xdxfqkki8bkmj1rmp4874";
+  revdate = "2019-01-31T14:37:57Z";
 }
