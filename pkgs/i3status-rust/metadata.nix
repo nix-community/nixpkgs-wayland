e9c0712497091854b6933b6d046c02d5a6c7e5cc
Cole Mickens <cole.mickens@gmail.com>
2019-01-31 18:35:58 -0800

    update: i3status-rust
    Signed-off-by: Cole Mickens <cole.mickens@gmail.com>

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "31a595ee2b7ca84c3205560d96ec7bcf8ce02d0b";
-  sha256 = "0chqckgrw41qb72xbv45c157v2rk710yjbshrw66c03c2zf4c389";
-  revdate = "2018-12-24T17:01:06Z";
+  rev = "90e9448d4861dea676865423b347075bb32acc42";
+  sha256 = "0nk2330gv9ac6ainbna38pzzds75dg1nzy2xgnvybgvhs54iahn2";
+  revdate = "2019-02-01T00:19:41Z";
 }
