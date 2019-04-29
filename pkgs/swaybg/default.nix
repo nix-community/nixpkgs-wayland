e91a333a97201a02806d86666012fe31115cfdb7
Alex Rice <alexrice999@hotmail.co.uk>
2019-04-29 18:50:52 +0100

    Swaybg: init

---
+++ pkgs/swaybg/default.nix
@@ -0,0 +1,35 @@
+{ stdenv, fetchFromGitHub
+, meson, ninja, pkgconfig
+, cairo, wayland, wayland-protocols
+, gdk_pixbuf, nonPngSupport ? true
+, scdoc, buildDocs ? true
+}:
+
+let
+  metadata = import ./metadata.nix;
+in
+stdenv.mkDerivation rec {
+  pname = "swaybg";
+  version = metadata.rev;
+
+  src = fetchFromGitHub {
+    owner = "swaywm";
+    repo = "swaybg";
+    rev = version;
+    sha256 = metadata.sha256;
+  };
+
+  nativeBuildInputs = [ pkgconfig meson ninja ] ++ stdenv.lib.optional buildDocs [ scdoc ];
+
+  buildInputs = [ cairo wayland wayland-protocols ] ++ stdenv.lib.optional nonPngSupport [ gdk_pixbuf ];
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "Wallpaper tool for Wayland compositors";
+    homepage    = https://github.com/swaywm/swaybg;
+    license     = licenses.mit;
+    platforms   = platforms.linux;
+    maintainers = with maintainers; [  ];
+  };
+}
