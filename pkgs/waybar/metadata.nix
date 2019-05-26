aecdd96729305f4badb2fe41a4c00d22e79b053c
Cole Mickens <cole.mickens@gmail.com>
2019-05-26 14:22:42 -0700

    updates

--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -1,5 +1,5 @@
 {
-  rev = "07147878a9ee31e03a4193f27d766a4e0921b58f";
-  sha256 = "1nylg60wisxkliqr6fs64wza852rnknjhxxs73kf02hnww7ac9wj";
-  revdate = "2019-05-24T07:49:56Z";
+  rev = "6e73c6db616ef1edbd12d910428f69f22eabc77b";
+  sha256 = "1kkj1nm4ybrb1yfjvhj5cw9wmhph4bnqxi6i2ljrqfixygh5qxgs";
+  revdate = "2019-05-26T21:16:09Z";
 }
