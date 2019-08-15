b73df4995cd485652719a1783bb959bbbe602e74
Cole Mickens <cole.mickens@gmail.com>
2019-08-15 21:16:37 +0200

    wdisplays: init

---
+++ pkgs/wdisplays/default.nix
@@ -0,0 +1,36 @@
+{stdenv, fetchFromGitHub
+, meson, ninja, pkgconfig
+, gtk3, epoxy
+, wayland, wayland-protocols
+, scdoc, buildDocs ? true
+}:
+
+let
+  metadata = import ./metadata.nix;
+in
+stdenv.mkDerivation rec {
+  pname = "wdisplays";
+  version = metadata.rev;
+
+  src = fetchFromGitHub {
+    owner = "cyclopsian";
+    repo = "wdisplays";
+    rev = version;
+    sha256 = metadata.sha256;
+  };
+
+  nativeBuildInputs = [ pkgconfig meson ninja ] ++ stdenv.lib.optional buildDocs scdoc;
+  buildInputs = [ gtk3 epoxy wayland wayland-protocols ];
+  mesonFlags = [ "-Dauto_features=enabled" ]
+    ++ stdenv.lib.optional (!buildDocs) "-Dman-pages=disabled";
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "GUI display configurator for wlroots compositors";
+    homepage    = "https://github.com/cyclopsian/wdisplays";
+    #license     = licenses.mit; # TODO 
+    platforms   = platforms.linux;
+    maintainers = with maintainers; [ colemickens ];
+  };
+}
---
+++ pkgs/wdisplays/metadata.nix
@@ -0,0 +1,5 @@
+{
+  rev = "ff536e196d5cfb7cd9a5ae8f3d49e719123b7f85";
+  sha256 = "12kmjvxxvfv98nkgw7c1kcdrmrwvi5a4vs7rz8vb1yh6vw2wn21w";
+  revdate = "2019-08-15T11:39:33Z";
+}
