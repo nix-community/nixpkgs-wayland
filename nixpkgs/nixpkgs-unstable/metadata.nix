885f401a81142e7d08a0a06c71c6927fc7e1bc17
Cole Mickens <cole.mickens@gmail.com>
2019-11-03 03:08:47 -0800

    updates

--- nixpkgs/nixpkgs-unstable/metadata.nix
+++ nixpkgs/nixpkgs-unstable/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/nixos/nixpkgs-channels";
   branch = "nixpkgs-unstable";
   url = "https://github.com/nixos/nixpkgs-channels/archive/nixpkgs-unstable.tar.gz";
-  rev = "d901b961a950ecf6731659dcebabab74d1fe7f8d";
-  sha256 = "1xmjkhjivx0rw5xi1z24h02qz286a7bfswf13xqn7mk85r7h4mg3";
-  revdate = "2019-10-31 20:42:06 -0500";
+  rev = "39e559212716e200347c5af4aa09940ea55b13b4";
+  sha256 = "08b4nkj5xwjkh66gsdipjja5y9gs8ns40jbzdb9ijxs2zsq8z2yy";
+  revdate = "2019-11-01 12:29:11 +0000";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "3ebf079a9a120a27fc1008a62e7f99d5d166b745";
-  sha256 = "1ik88zls7m3fz0y4wr13zhi6542ila4g00pqvidbvjvfdp0zkwbq";
-  revdate = "2019-11-03 00:17:23 +0000";
+  rev = "626c98d754253de10a7db0482e44459c7f75a85d";
+  sha256 = "104wlaa53ivf5p1fjnsfarfgyba4bkx657717vzfd7r7k6xlpy1v";
+  revdate = "2019-11-03 10:13:47 +0100";
 }
