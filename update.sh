07c6e91321fed15fe72715c7d971782dc71a4392
Cole Mickens <cole.mickens@gmail.com>
2019-11-15 19:26:32 -0800

    update.sh another updateg

--- update.sh
+++ update.sh
@@ -21,80 +21,69 @@ function update() {
   sha256="$(nix eval --raw -f "${metadata}" sha256)"
   skip="$(nix eval -f "${metadata}" skip || true)"
 
-  if [[ "${skip}" == "true" ]]; then
-    return 0
-  fi
+  newdate="${date}"
+  if [[ "${skip}" != "true" ]]; then
+    # Determine RepoTyp (git/hg)
+    if   nix eval --raw -f "${metadata}" repo_git; then repotyp="git";
+    elif nix eval --raw -f "${metadata}" repo_hg;  then repotyp="hg";
+    else echo "unknown repo_typ" && exit -1;
+    fi
 
-  # Determine RepoTyp (git/hg)
-  if   nix eval --raw -f "${metadata}" repo_git; then repotyp="git";
-  elif nix eval --raw -f "${metadata}" repo_hg;  then repotyp="hg";
-  else echo "unknown repo_typ" && exit -1;
-  fi
-
-  # Update Rev
-  if [[ "${repotyp}" == "git" ]]; then
-    repo="$(nix eval --raw -f "${metadata}" repo_git)"
-    newrev="$(git ls-remote "${repo}" "${branch}" | awk '{ print $1}')"
-  elif [[ "${repotyp}" == "hg" ]]; then
-    repo="$(nix eval --raw -f "${metadata}" repo_hg)"
-    newrev="$(hg identify "${repo}" -r "${branch}")"
-  fi
-  
-  if [[ "${rev}" != "${newrev}" ]]; then
-    # Update RevDate
-    d="$(mktemp -d)"
+    # Update Rev
     if [[ "${repotyp}" == "git" ]]; then
-      git clone -b "${branch}" --single-branch --depth=1 "${repo}" "${d}"
-      newdate="$(cd "${d}"; git log --format=%ci --max-count=1)"
+      repo="$(nix eval --raw -f "${metadata}" repo_git)"
+      newrev="$(git ls-remote "${repo}" "${branch}" | awk '{ print $1}')"
     elif [[ "${repotyp}" == "hg" ]]; then
-      hg clone "${repo}#${branch}" "${d}"
-      newdate="$(cd "${d}"; hg log -r1 --template '{date|isodate}')"
+      repo="$(nix eval --raw -f "${metadata}" repo_hg)"
+      newrev="$(hg identify "${repo}" -r "${branch}")"
     fi
-    rm -rf "${d}"
+    
+    if [[ "${rev}" != "${newrev}" ]]; then
+      # Update RevDate
+      d="$(mktemp -d)"
+      if [[ "${repotyp}" == "git" ]]; then
+        git clone -b "${branch}" --single-branch --depth=1 "${repo}" "${d}"
+        newdate="$(cd "${d}"; git log --format=%ci --max-count=1)"
+      elif [[ "${repotyp}" == "hg" ]]; then
+        hg clone "${repo}#${branch}" "${d}"
+        newdate="$(cd "${d}"; hg log -r1 --template '{date|isodate}')"
+      fi
+      rm -rf "${d}"
 
-    # Update Sha256
-    # TODO: nix-prefetch without NIX_PATH?
-    if [[ "${typ}" == "pkgs" ]]; then
-      newsha256="$(NIX_PATH=nixpkgs=https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz
-        nix-prefetch \
-          -E "(import ./build.nix).nixosUnstable.${pkgname}" \
-          --rev "${newrev}" \
-          --output raw)"
-    elif [[ "${typ}" == "nixpkgs" ]]; then
-      # TODO: why can't nix-prefetch handle this???
-      url="$(nix eval --raw -f "${metadata}" url)"
-      newsha256="$(NIX_PATH=nixpkgs=https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz
-        nix-prefetch-url --unpack "${url}")"
+      # Update Sha256
+      # TODO: nix-prefetch without NIX_PATH?
+      if [[ "${typ}" == "pkgs" ]]; then
+        newsha256="$(NIX_PATH=nixpkgs=https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz
+          nix-prefetch \
+            -E "(import ./build.nix).nixosUnstable.${pkgname}" \
+            --rev "${newrev}" \
+            --output raw)"
+      elif [[ "${typ}" == "nixpkgs" ]]; then
+        # TODO: why can't nix-prefetch handle this???
+        url="$(nix eval --raw -f "${metadata}" url)"
+        newsha256="$(NIX_PATH=nixpkgs=https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz
+          nix-prefetch-url --unpack "${url}")"
+      fi
+
+      # TODO: do this with nix instead of sed?
+      sed -i "s/${rev}/${newrev}/" "${metadata}"
+      sed -i "s/${date}/${newdate}/" "${metadata}"
+      sed -i "s/${sha256}/${newsha256}/" "${metadata}"
     fi
-
-    # TODO: do this with nix instead of sed?
-    sed -i "s/${rev}/${newrev}/" "${metadata}"
-    sed -i "s/${date}/${newdate}/" "${metadata}"
-    sed -i "s/${sha256}/${newsha256}/" "${metadata}"
   fi
-}
 
-function update_readme_entries() {
-  typ="${1}"
-  pkg="${2}"
-  metadata="${pkg}/metadata.nix"
-  pkgname="$(basename "${pkg}")"
-  branch="$(nix eval --raw -f "${metadata}" branch)"
-  rev="$(nix eval --raw -f "${metadata}" rev)"
-  date="$(nix eval --raw -f "${metadata}" revdate)"
-  sha256="$(nix eval --raw -f "${metadata}" sha256)"
-  skip="$(nix eval -f "${metadata}" skip || true)"
   if [[ "${skip}" == "true" ]]; then
-    date="${date} (pinned)"
+    newdate="${newdate} (pinned)"
   fi
   if [[ "${typ}" == "pkgs" ]]; then
     desc="$(nix eval --raw "(import ./build.nix).nixosUnstable.${pkgname}.meta.description")"
     home="$(nix eval --raw "(import ./build.nix).nixosUnstable.${pkgname}.meta.homepage")"
-    pkgentries=("${pkgentries[@]}" "| [${pkgname}](${home}) | ${date} | ${desc} |");
+    pkgentries=("${pkgentries[@]}" "| [${pkgname}](${home}) | ${newdate} | ${desc} |");
   elif [[ "${typ}" == "nixpkgs" ]]; then
-    nixpkgentries=("${nixpkgentries[@]}" "| ${pkgname} | ${date} |");
+    nixpkgentries=("${nixpkgentries[@]}" "| ${pkgname} | ${newdate} |");
   fi
 }
+
 function update_readme() {
   replace="$(printf "<!--pkgs-->")"
   replace="$(printf "%s\n| Package | Last Update | Description |" "${replace}")"
@@ -122,24 +111,14 @@ function update_readme() {
       > README2.md; mv README2.md README.md
 }
 
-# check and make sure git is all clean and g2g
-
 for p in nixpkgs/*; do
   update "nixpkgs" "${p}"
-  update_readme_entries "nixpkgs" "${p}"
 done
-
-if [[ "${1:-}" != "" ]]; then
-  update "pkgs" "pkgs/${1}"
-else
-  for p in pkgs/*; do
-    update "pkgs" "${p}"
-  done
-fi
 
 for p in pkgs/*; do
-  update_readme_entries "pkgs" "${p}"
+  update "pkgs" "${p}"
 done
+
 update_readme
 
 nix-build --no-out-link build.nix -A all \
