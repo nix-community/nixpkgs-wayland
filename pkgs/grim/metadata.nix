57a75066557fba5147486004ce27703e8a41bfca
Cole Botkens <cole.mickens+colebot@gmail.com>
2019-12-10 13:03:54 +0000

    auto-updates

--- pkgs/grim/metadata.nix
+++ pkgs/grim/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/emersion/grim";
   branch = "master";
-  rev = "a9af6088d5e6eb31c4c12a659b4641e9398e33e9";
-  sha256 = "15d30aiwb2aj9nz9wvn9m4gz00jr7xsnx0s584nr9hkgcqrn26hn";
-  revdate = "2019-07-20 19:11:00 +0300";
+  rev = "112f302b1962da1e9852bbc4b78e18ceae9c9578";
+  sha256 = "1shbm1350gs618a13pn3v6zbzvrml1sm073i9h69b81lsrbykx4f";
+  revdate = "2019-12-10 13:59:43 +0100";
 }
