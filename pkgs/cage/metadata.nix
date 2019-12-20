48d7c0c997b8a9bdf636020f3163ca114f4b89e6
Cole Botkens <cole.mickens+colebot@gmail.com>
2019-12-20 16:02:25 +0000

    auto-updates

--- pkgs/cage/metadata.nix
+++ pkgs/cage/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/Hjdskes/cage";
   branch = "master";
-  rev = "dd87c1fab588c75729ff888cd8814a3b48ee18fa";
-  sha256 = "11qjhcapw801608a0h6w4am47f5ggs4ms6j7fhichwv3zp2lbxjp";
-  revdate = "2019-12-20 12:35:41 +0100";
+  rev = "90e70c68825d42f653fd7dc5c33a06b3f7477744";
+  sha256 = "04s5l2z4ixhnbislwps15avin27285098psgqm5a4zdfrak4qnck";
+  revdate = "2019-12-20 16:51:11 +0100";
 }
