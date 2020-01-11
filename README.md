71ea69f7676864251d0251b5508173b9c56d9036
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-01-11 09:32:26 +0000

    auto-updates

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ These packages were mostly recently built against:
 <!--nixpkgs-->
 | Channel | Last Channel Commit Time |
 | ------- | ------------------------ |
-| nixos-unstable | 2020-01-07 12:32:18 +0100 |
+| nixos-unstable | 2020-01-09 15:57:05 -0800 |
 | nixpkgs-unstable | 2020-01-03 03:43:17 -0500 |
 <!--nixpkgs-->
 
--- nixpkgs/nixos-unstable/metadata.nix
+++ nixpkgs/nixos-unstable/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/nixos/nixpkgs";
   branch = "nixos-unstable";
   url = "https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz";
-  rev = "e1eedf29e5d22e6824e614d75449b75a2e3455d6";
-  sha256 = "1v237cgfkd8sb5f1r08sms1rxygjav8a1i1jjjxyqgiszzpiwdx7";
-  revdate = "2020-01-07 12:32:18 +0100";
+  rev = "e4134747f5666bcab8680aff67fa3b63384f9a0f";
+  sha256 = "19pca75ds77c6nq6f3c6iky7m3alkpva2vbjas1c7xgngzy0mw4i";
+  revdate = "2020-01-09 15:57:05 -0800";
 }
