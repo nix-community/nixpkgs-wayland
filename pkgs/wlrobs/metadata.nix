c770e4a4fe5091861fb133f44e4ddc663cde24bf
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-01-11 04:02:57 +0000

    auto-updates

--- pkgs/wlrobs/metadata.nix
+++ pkgs/wlrobs/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wlrobs";
   branch = "default";
-  rev = "94fa8e3d57e0";
-  sha256 = "01ks2afn6qgamq5mq9zdx7dm2k1j08nink95bagp4iy855bayk5f";
+  rev = "8345bf985e39";
+  sha256 = "0j01wkhwhhla4qx8mwyrq2qj9cfhxksxaq2k8rskmy2qbdkvvdpb";
   revdate = "2019-03-16 15:06 -0700";
 }
