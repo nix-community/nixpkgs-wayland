880a232f785b90329b7e524452d521d91e19c2b0
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-01-30 08:04:01 +0000

    auto-updates

--- nixpkgs/nixpkgs-unstable/metadata.nix
+++ nixpkgs/nixpkgs-unstable/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/nixos/nixpkgs";
   branch = "nixpkgs-unstable";
   url = "https://github.com/nixos/nixpkgs/archive/nixpkgs-unstable.tar.gz";
-  rev = "98cc98efe1512684f0e1329ac4b31e950d965dc4";
-  sha256 = "12f14aypw0b7gybcpdbkxqzvbv1qnjic442g2dfvjvhpzxsfyyrs";
-  revdate = "2020-01-29 21:43:19 -0500";
+  rev = "642134d5ad76f19027fdd40cd1adcf18a0dea037";
+  sha256 = "0x43mrxxq81r9h1n18ygkgh7360s3r5h282z7ax54c0xl4b21ryg";
+  revdate = "2020-01-29 22:52:33 -0800";
 }
