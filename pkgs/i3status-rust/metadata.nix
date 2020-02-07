b2a67d5928323723516153637a2537f59f54a548
Cole Mickens <cole.mickens@gmail.com>
2020-02-07 00:36:54 -0800

    i3status: manual update

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://github.com/greshake/i3status-rust";
   branch = "master";
-  rev = "d39c765ac0b1a78958f02c210c039ba73897be48";
-  sha256 = "047ivrp70kwsm4792ing8dvgh161cmayzy9ij6ww61fbkb4slr1i";
-  cargoSha256 = "16rgg0fy50n0z0kal52iaxiqwhw4qpjvzyqwaldm29fq9c0105d1";
-  revdate = "2020-02-02 22:30:57 -0500";
+  rev = "fe7ea993435acd1b98af5b02544f48825fe51d49";
+  sha256 = "11p2gq2sxv71kdqiv2f3yv09j3pnlar5gcj4n43a21sh01cmcbsm";
+  cargoSha256 = "1sxbakfyxg84wrv65gq03j3xf44c942cyknqkqm68z6dby5dfp6z";
+  revdate = "2020-02-06 15:24:26 -0500";
 }
