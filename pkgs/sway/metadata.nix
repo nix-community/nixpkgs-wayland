fd8b711199dc0cdb62e517b5c01fb24c230fb4e9
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-02-11 10:05:59 +0000

    auto-updates

--- pkgs/sway/metadata.nix
+++ pkgs/sway/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/swaywm/sway";
   branch = "master";
   upattr = "sway-unwrapped";
-  rev = "803e89018ab899a44e1ca24758d981dff27d5986";
-  sha256 = "00ac2wr8jj3pbmib7qy3f8wybj84zanzxlg7im58hhqhz4zwlrzf";
-  revdate = "2020-02-10 21:54:00 +0100";
+  rev = "31a83bd48d9aad2a039565fc39f1a52bd0cdf17d";
+  sha256 = "1lz7aqhz4g1ny42xkmg0pbmcs14dnql1yx9b4x8fvky5fqpkgdh7";
+  revdate = "2020-02-11 10:17:45 +0100";
 }
