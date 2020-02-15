3cc877241bf2012d247e69afb37d7b7b9d6f3b4a
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-02-15 00:03:05 +0000

    auto-updates

--- pkgs/mako/metadata.nix
+++ pkgs/mako/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/emersion/mako";
   branch = "master";
-  rev = "175b8d8ab752bcc29da39ff89eca0b274af66628";
-  sha256 = "143ri40hzds65qxjr0w7m84wivyy0n9i4dqi6x0g9s2sc3zc7wc1";
-  revdate = "2020-02-10 17:49:11 +0100";
+  rev = "b933be475152fea7b35a0515ca09e3c442fc4cfa";
+  sha256 = "08892kv27q576ngjb5jw5176bhabsy3y4s9cqhwfn2pza6cbmmff";
+  revdate = "2020-02-15 00:56:32 +0100";
 }
