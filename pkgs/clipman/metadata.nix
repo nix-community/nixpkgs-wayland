a0e7992e493ca598cff949e53a9852143b23718a
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-02-23 14:02:32 +0000

    auto-updates

--- pkgs/clipman/metadata.nix
+++ pkgs/clipman/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://github.com/yory8/clipman";
   branch = "master";
-  rev = "f2cd6b8ef32927cd449480c6356407f8b1f17234";
-  sha256 = "0b27rgyjwvdm9qjq824imda3yjbxxcwwyv8rzzk95ggvyayxw8an";
+  rev = "2b30ada83da35aca870fe911eb314b8ceb7b09a2";
+  sha256 = "13m0xgamlbn8z0yjq7x5xxghl9q2l4v4ysxw0skmrm5nfypisrrs";
   modSha256 = "0aw0ng8pk8qzn1iv79iw0v9zr8xdc8p9xnigr3ij86038f7aqdhv";
-  revdate = "2020-02-19 14:27:47 +0100";
+  revdate = "2020-02-23 14:54:10 +0100";
 }
