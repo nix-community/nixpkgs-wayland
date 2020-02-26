39a9bc666369d4e8aff69409a63df2f5c54d8ea6
Cole Mickens <cole.mickens@gmail.com>
2020-02-25 16:19:14 -0800

    updates

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wofi";
   branch = "default";
-  rev = "4ec4e85e808d";
-  sha256 = "0xi75qiziwgv9ry7j0v1k456fshxff00d5n5h5sclsk0hbv974qq";
+  rev = "df096e4ef210";
+  sha256 = "1d3492140xl7w5is0q8ln0nnx61hvw5hmka0lz628n8b8gkcj20r";
   cargoSha256 = "0lryc48wdyz6fmlhgsxgqc0pvv8c85dhq5y8rbhmw1p3kzvj8w0k";
   revdate = "2019-08-17 18:34 -0700";
 }
