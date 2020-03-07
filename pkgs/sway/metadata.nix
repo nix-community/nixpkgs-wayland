5b83c4cb21642b56e0582f4c8828dc6b85dcc1fe
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-03-07 00:11:17 +0000

    auto-updates

--- pkgs/sway/metadata.nix
+++ pkgs/sway/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/swaywm/sway";
   branch = "master";
   upattr = "sway-unwrapped";
-  rev = "9d0aa0cb839624265c366281922a58708a9bcb9a";
-  sha256 = "1giyrxlqrsbwbi6fwyy7nk3xvsf4a83l386843hmp3mf213x59cg";
-  revdate = "2020-03-06 23:57:43 +0100";
+  rev = "e81d9fde667e7a06989b63f1091b81a12885f194";
+  sha256 = "1biijk5wa39v8kwgnaai2i2cf8zv6s53gbdc1h746nygdqpyi8pq";
+  revdate = "2020-03-07 00:35:35 +0100";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "7bce056f1d9490cf04166d3eb0183c9eec2204ff";
-  sha256 = "15vg0lan10r2pd7z6prg03aqrp86dmmr78lv9y5vip125n30j21i";
-  revdate = "2020-03-06 21:35:00 +0100";
+  rev = "e7f8cc6801bc28ccc8aac8aabd2896cbeec8e369";
+  sha256 = "0q7kwwq0ddqg29mc70q5wazpqa2vb1a4bgr33wj5kd0rmzs2mrp1";
+  revdate = "2020-03-07 00:25:01 +0100";
 }
