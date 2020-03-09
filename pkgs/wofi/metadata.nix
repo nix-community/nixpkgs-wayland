5b207b707e2e48db0fd0999ee2c5e7a259326c2e
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-03-09 22:02:53 +0000

    auto-updates

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wofi";
   branch = "default";
-  rev = "e88797002b62";
-  sha256 = "0lrzwg1mqnhgsdxi5iqlhxhh5i4svz5qw6ykbsr0w8fxybgh693g";
+  rev = "a2cff8729d80";
+  sha256 = "0grwz9in05c8jhprj8rn4vscm4v8vdahgr4chijjdn8xg8f1yfxp";
   cargoSha256 = "0lryc48wdyz6fmlhgsxgqc0pvv8c85dhq5y8rbhmw1p3kzvj8w0k";
   revdate = "2019-08-17 18:34 -0700";
 }
