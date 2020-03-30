6ee01f01369d9d1afc648a88ad31c2b79c65728d
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-03-30 14:03:13 +0000

    auto-updates

--- pkgs/kanshi/metadata.nix
+++ pkgs/kanshi/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/emersion/kanshi";
   branch = "master";
-  rev = "dc9f4c8fc3bfa8ff6739d75eca6410454fe59786";
-  sha256 = "0i9lzbwd7x8lcvd8axxlx978z67asmwmgj6afz5wrl2cam863bf1";
-  revdate = "2020-02-22 21:24:07 +0100";
+  rev = "ef4c7f083ddbbf6784b3be9be6f7d9edd0eb9157";
+  sha256 = "074zdjz8k9igyhaxq0696df5qfmd264yxjlrc6kqfy4ppridrrgw";
+  revdate = "2020-03-30 15:45:31 +0200";
 }
