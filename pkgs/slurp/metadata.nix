f48f5a7a686efed57a0bf88cf0854bd4bc883a36
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-03-30 15:05:58 +0000

    auto-updates

--- pkgs/slurp/metadata.nix
+++ pkgs/slurp/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/emersion/slurp";
   branch = "master";
-  rev = "d1f9ad52516b69484ed2d686beeeb36850eb2862";
-  sha256 = "0kbj1sqhvx634hgx3c3bndyz7w9iiq711psg4r74pas4138zg0bi";
-  revdate = "2020-03-25 16:56:48 +0100";
+  rev = "9b239ee460f04c26b5eebb4526bf1c3f08de3d2c";
+  sha256 = "02rs1cykzn92frg67xyp5qph9wankcfn6arbsbw9lcaw0j3ycvj7";
+  revdate = "2020-03-30 16:54:32 +0200";
 }
