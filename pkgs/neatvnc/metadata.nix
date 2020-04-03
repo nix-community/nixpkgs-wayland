e5e98c24d1682df29bfb106d28efb289bee7268c
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-04-03 01:03:07 +0000

    auto-updates

--- pkgs/neatvnc/metadata.nix
+++ pkgs/neatvnc/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/any1/neatvnc";
   branch = "master";
-  rev = "10a3fd62384782ad8f9244e6c4d25ea177bc1cfc";
-  sha256 = "12zcrmlsccl4avh7nkmpp20iilxxzr5mj6fw6zp08biyi08a8h9c";
-  revdate = "2020-04-01 22:53:22 +0000";
+  rev = "17e0d6036f3d8cb1ec61b5ffbc8b70368c22ba8e";
+  sha256 = "1p0pljw32v8jyzxpp7cbn8cgj1811yvd5lhp60x1fskbbhpy79di";
+  revdate = "2020-04-03 00:18:54 +0000";
 }
