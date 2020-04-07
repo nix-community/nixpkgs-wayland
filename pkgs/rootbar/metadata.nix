562a77ed7c00b3ab53d9319d0609c90c620c82f4
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-04-07 03:03:10 +0000

    auto-updates

--- pkgs/rootbar/metadata.nix
+++ pkgs/rootbar/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/rootbar";
   branch = "default";
-  rev = "2136ad27eafc";
-  sha256 = "11lp1npln9ihsdg4mj1ky8gy6jab8h8qzprpnsz83vdarqbs2hw8";
+  rev = "0c38a4a02836";
+  sha256 = "1irj5b8gm21kdwlxbhkl87nkwf7idijass1y1vwcligbxk84dz0q";
   revdate = "2019-03-18 10:47 -0700";
 }
