f8d5f526453cebd5153b1ea03be3419d7f200b28
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-04-10 10:05:31 +0000

    auto-updates

--- pkgs/wlfreerdp/metadata.nix
+++ pkgs/wlfreerdp/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/FreeRDP/FreeRDP";
   branch = "master";
-  rev = "5ab2bed8749747b8e4b2ed431fd102bc726be684";
-  sha256 = "0d2559v0z1jnq6jlrvsgdf8p6gd27m8kwdnxckl1x0ygaxs50bqc";
-  revdate = "2020-04-09 18:00:51 +0200";
+  rev = "969b990237f171d78ca3b0eaa21109fb9499ec89";
+  sha256 = "1h9iq0q7qxa38kx35vbmqirml1wziqrw6dwmrdwv0njrlwzvzf6k";
+  revdate = "2020-04-10 11:53:09 +0200";
 }
