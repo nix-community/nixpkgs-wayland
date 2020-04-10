f5acf2745f9359910df8d9021fe0820deebc0b09
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-04-10 14:09:30 +0000

    auto-updates

--- pkgs/wlfreerdp/metadata.nix
+++ pkgs/wlfreerdp/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/FreeRDP/FreeRDP";
   branch = "master";
-  rev = "08fd2876b98ebf45114d0b5cfe6ea948adbc0025";
-  sha256 = "1237gyz5rzp1mldqjzy0scs2ms30d65l0qmacygnw245l4v2k7i4";
-  revdate = "2020-04-10 15:01:15 +0200";
+  rev = "03fa903116fe4837522a5855a2fa07132b0b7379";
+  sha256 = "11dsb5pmxppj13vkkny0dz320xhvd66cskxx6mzvpdpingp29x21";
+  revdate = "2020-04-10 15:36:30 +0200";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "d10f8a98ec171791cd81de807e1c400af25b1e2b";
-  sha256 = "1f5024h66hhi3kzqiacm9jf7n0g55ais11q7y46m6wahhgkfdc8g";
-  revdate = "2020-04-09 00:16:03 +0000";
+  rev = "83c1ba77834d7710918e4fe15c5cb40c4736d6db";
+  sha256 = "1vd43yp7qs8hj8mj52haddpk8ygpkin0bjxjd87hl9p776w359vx";
+  revdate = "2020-04-10 15:52:20 +0200";
 }
