05ca05705d0e62ad2a80d2635d2d4385b74162ad
Cole Mickens <cole.mickens@gmail.com>
2020-04-21 21:10:49 -0700

    wldash: manual update

--- pkgs/wldash/metadata.nix
+++ pkgs/wldash/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://github.com/kennylevinsen/wldash";
   branch = "master";
-  rev = "833f289cd0ff446ceffb61cc34a2d8cb97a3f039";
-  sha256 = "1k6j0pjbjbrc024i8j256hvlwy82hs5fy5nd4rsvcpxdgxx9n8dn";
-  cargoSha256 = "05i5w8sycd1s4nk6s0lla4fhn11nvma1c370wgirr7mdkkvf6wqb";
-  revdate = "2020-03-28 02:22:50 +0100";
+  rev = "319bcdd3556b6eec58d077e0def1f2ef296968a3";
+  sha256 = "0gcann3sdcj7wad0ah53j80ykbrcgbcys96m7zg8z4pdqjlcq5k0";
+  cargoSha256 = "1yplnisgzjq4p0mnl16zvhrmbf20mf70mczhy0im2ka1mfjzr26l";
+  revdate = "2020-04-22 01:31:22 +0200";
 }
