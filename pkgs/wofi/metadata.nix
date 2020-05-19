5ab273afbb454f4d70469e1339899e5a1b5d7592
Cole Mickens <cole.mickens@gmail.com>
2020-05-18 21:08:35 -0700

    wofi: remove unused cargoSha256 from metadata

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -1,8 +1,7 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wofi";
   branch = "default";
-  rev = "666b1da4f99b";
-  sha256 = "1910svhxp2r4rj8ybx1l8pc2w44chv0vv7xkmga2v6xxqiihv1s1";
-  cargoSha256 = "0lryc48wdyz6fmlhgsxgqc0pvv8c85dhq5y8rbhmw1p3kzvj8w0k";
+  rev = "14e50fb5d18f";
+  sha256 = "142lbjqhggrvw2x7c2lx07mdgbba6ixksyzx2cmv32a71w6ynawy";
   revdate = "2019-08-17 18:34 -0700";
 }
