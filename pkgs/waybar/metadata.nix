f9e5049942c1a5b6efd0cb082b6f0dddf16d149d
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-05-30 11:06:26 +0000

    auto-updates: waybar, wayfire

--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/Alexays/Waybar";
   branch = "master";
-  rev = "e96a0bf7996cc78715ce08b3a23767b08fb1acf8";
-  sha256 = "1fknpjy7s9dk6ny3hc9ilgsha0r98q41is2y0dw5bwk6206di0d6";
-  revdate = "2020-05-30 09:59:22Z";
+  rev = "adaf84304865e143e4e83984aaea6f6a7c9d4d96";
+  sha256 = "015yrld38iglahm4r6drfiq38xndin44chrmqcnh8klfs3rqviy4";
+  revdate = "2020-05-30 10:07:38Z";
 }
--- pkgs/wayfire/metadata.nix
+++ pkgs/wayfire/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/WayfireWM/wayfire";
   branch = "master";
-  rev = "837ba4aa9eecc639b1b7522256eeeb62893db8dd";
-  sha256 = "05xlncpfr0hrpk9sqr3rplkhq344bpnaw1z5kwvmm2p7x5ayb4g2";
-  revdate = "2020-05-27 22:23:21Z";
+  rev = "f7d3c51552684ac7527d58210d8ebdff2a4bbca1";
+  sha256 = "10pl38z3hz1sgz708qiz4ix6k5rzja7kiawplisvq9dnkxhwmldy";
+  revdate = "2020-05-30 10:52:17Z";
 }
