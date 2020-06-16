0781ae50878bbe0ee15d3c4011daabba6a6bb577
Cole Mickens <cole.mickens@gmail.com>
2020-06-16 16:37:09 -0700

    update.sh: don't push every dep downloaded

--- update.sh
+++ update.sh
@@ -170,10 +170,6 @@ fi
 
 set -x
 
-cachix push -w "${cache}" &
-CACHIX_PID="$!"
-trap "kill ${CACHIX_PID}" EXIT
-
 nix-build-uncached -build-flags "\
   --option \"extra-binary-caches\" \"https://cache.nixos.org https://nixpkgs-wayland.cachix.org\" \
   --option \"trusted-public-keys\" \"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=\" \
