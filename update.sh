eac07e56ff2d3f7ff47fdd4d53289266e6f2fce4
Cole Mickens <cole.mickens@gmail.com>
2020-06-18 17:28:15 -0700

    update.sh: fixup for now by undoing nbu work

--- update.sh
+++ update.sh
@@ -170,9 +170,10 @@ fi
 
 set -x
 
-nix-build-uncached -build-flags "\
-  --option \"extra-binary-caches\" \"https://cache.nixos.org https://nixpkgs-wayland.cachix.org\" \
-  --option \"trusted-public-keys\" \"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=\" \
-  --option \"build-cores\" \"0\" \
-  --option \"narinfo-cache-negative-ttl\" \"0\" \
-  --keep-going " build.nix | cachix push "${cache}"
+#nix-build-uncached -build-flags "\
+nix-build \
+  --option "extra-binary-caches" "https://cache.nixos.org https://nixpkgs-wayland.cachix.org" \
+  --option "trusted-public-keys" "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" \
+  --option "build-cores" "0" \
+  --option "narinfo-cache-negative-ttl" "0" \
+  --keep-going build.nix | cachix push "${cache}"
