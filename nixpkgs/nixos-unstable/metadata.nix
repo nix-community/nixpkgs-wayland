92d391fd5c853df68bc23c766a4783bd7a4aef38
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-07-01 07:37:57 +0000

    auto-updates: nixos-unstable

--- nixpkgs/nixos-unstable/metadata.nix
+++ nixpkgs/nixos-unstable/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/nixos/nixpkgs";
   branch = "nixos-unstable";
   url = "https://github.com/nixos/nixpkgs/archive/nixos-unstable.tar.gz";
-  rev = "22a81aa5fc15b2d41b12f7160a71cd4a9f3c3fa1";
-  sha256 = "14gx5fsqibdn2cxp7gymfrz2vcnwiwwjnxqlnysczz8dqihnrpa7";
-  revdate = "2020-06-22 15:52:02Z";
+  rev = "b3251e04ee470c20f81e75d5a6080ba92dc7ed3f";
+  sha256 = "0hld390gb3q055sa7j1rzxl1hzaxgagc60vrqmkk8fnfpv813g75";
+  revdate = "2020-06-30 18:12:44Z";
 }
