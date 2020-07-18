6dcd326d9b335c1d531c0f67bb92344dc0262550
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-07-18 15:02:44 +0000

    auto-updates: lavalauncher, neatvnc,

--- pkgs/lavalauncher/metadata.nix
+++ pkgs/lavalauncher/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://git.sr.ht/~leon_plickat/lavalauncher";
   branch = "master";
-  rev = "2572a39fbc1b0119ebef97a5b3440aa990dbc2c3";
-  sha256 = "0563wv20149fs7i0940c0xyhdg7y0nnhsffiqijpw4a0z04a2cmp";
-  revdate = "2020-07-18 03:12:20Z";
+  rev = "80f07dca050b338c0804976e8370e178b468d28c";
+  sha256 = "1q3jnqfjbxdn3jwddq3vn5mbdlr26b79x43ca04hhd7vibp9nfr7";
+  revdate = "2020-07-18 14:24:01Z";
 }
--- pkgs/neatvnc/metadata.nix
+++ pkgs/neatvnc/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/any1/neatvnc";
   branch = "master";
-  rev = "683776cdf09c9dce10df4898f15aa5dc61f8f8aa";
-  sha256 = "06x23pjixbnazji06wnq07ig10nr6g49dzl1bjra30qpc1hy6zw2";
-  revdate = "2020-07-16 18:14:52Z";
+  rev = "eb4b9d71a4d02c42fc6a24d210cf61a17444667f";
+  sha256 = "0j63s4y918bxmfw3djy5slaw0f85khjv61ip3j8i8qwq1n9471nl";
+  revdate = "2020-07-18 14:04:12Z";
 }
