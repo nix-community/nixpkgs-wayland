51ba5d516430efd8f40cd26f2d419a958c3c693d
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-07-22 11:11:40 +0000

    auto-updates: wldash,

--- pkgs/wldash/metadata.nix
+++ pkgs/wldash/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://github.com/kennylevinsen/wldash";
   branch = "master";
-  rev = "12ea4e7f6005e6662c479279df9061f01eb9bcc6";
-  sha256 = "0zanhslnsn6dnbk76930lw9pbhg6c26qz1kgnqs196wavdvac0cn";
-  cargoSha256 = "1vxij4r0ik2gmflsvc1cxksgy90jh243rbvxy3i3s8inh1wfkfb1";
-  revdate = "2020-07-21 23:44:06Z";
+  rev = "2e49b98b82427fde046554b9ead825680348fb17";
+  sha256 = "1w1b8gznswdrd778si4pnz67957mqgsvfag0kpdazzhq1mmcxxz4";
+  cargoSha256 = "1vzwnz0a86idwxbfjmhb6rakv7h9ypsbayjngdndpc82vggnkmmc";
+  revdate = "2020-07-22 10:42:48Z";
 }
