a7aa868d0558ec0e2e7707716f149fe57b3e6fac
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-07-24 07:06:32 +0000

    auto-updates: lavalauncher, wayfire,

--- pkgs/wayfire/metadata.nix
+++ pkgs/wayfire/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/WayfireWM/wayfire";
   branch = "master";
-  rev = "6d85e319db787002c60895b8418a1a6716834f20";
-  sha256 = "1n0bpsf8wsv3s1p40vmcknk3h8wixl8madwmi65qv5lfb1mxw3b2";
-  revdate = "2020-07-23 08:46:23Z";
+  rev = "3bebb360e55ce23b668fb5b04c45a137421356f7";
+  sha256 = "1r5xrh5q5zw1xpfshpzs3m8zd4ydbsbmsd2ybq1cdv7xa3r05j4n";
+  revdate = "2020-07-24 06:19:15Z";
 }
