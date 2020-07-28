84db20e5d05e66deccdca79c792213b43bdd458c
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-07-28 23:06:22 +0000

    auto-updates: gtk-layer-shell,

--- pkgs/gtk-layer-shell/metadata.nix
+++ pkgs/gtk-layer-shell/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/wmww/gtk-layer-shell";
   branch = "master";
-  rev = "b0c0eeba8639315d1719fffb8b92ac0a1f0758ee";
-  sha256 = "00wxw8587wlkxqhqilm5ln6izgsy0hxfjyqpnk9hm4snw0s39jac";
-  revdate = "2020-07-28 20:55:32Z";
+  rev = "15100707f446a85b5c3bbd7b84aaf878a9fcfe15";
+  sha256 = "0kas84z44p3vz92sljbnahh43wfj69knqsy1za729j8phrlwqdmg";
+  revdate = "2020-07-28 21:50:00Z";
 }
