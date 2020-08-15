b33814c73115485b2f930b03d8b2867782dfe879
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-08-15 09:24:54 +0000

    auto-updates: aml, waybar,

--- pkgs/aml/metadata.nix
+++ pkgs/aml/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/any1/aml";
   branch = "master";
-  rev = "d1783ccb26ad094858633401095085c9de683asdf";
-  sha256 = "sha256-9Oi/iYUS1POjP/10YYI38ea9Fjz5593+nsrkZMfMbrI=";
+  rev = "d1783ccb26ad094858633401095085c9de683841";
+  sha256 = "1ckfrk3n9r6akvzdvrzr7hbbvrpi6y162x7x7yiz7m0jhn4vzs7l";
   revdate = "2020-07-26 14:57:11Z";
 }
--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/Alexays/Waybar";
   branch = "master";
-  rev = "591eb2ea38a7df53305fa39bb05492e1036c767e";
-  sha256 = "sha256-UJSEleGytZsiGDUeLfXdt0ljDfwgDeqPv/LpVbNxtrc=";
-  revdate = "2020-08-14 20:12:48Z";
+  rev = "0cf3b25d50c4ef0fc3b57913685add5fd65e2c42";
+  sha256 = "1pci4xgr1x4fq6mi8s7zzvk3m8smrm09igg6sp330kgz3qzpbj4l";
+  revdate = "2020-08-15 08:59:12Z";
 }
