65a9eb4f3df5abf4346adf68519d6d31eda33f40
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-08-21 08:09:56 +0000

    auto-updates: wayfire,

--- pkgs/wayfire/metadata.nix
+++ pkgs/wayfire/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/WayfireWM/wayfire";
   branch = "master";
-  rev = "66e9a6480105efc52c9e9264c7881c8efd4b6286";
-  sha256 = "0jflybnr2c94l3fzd1d6bavi2m8hwddnpirs7pgrhyx9kfn7mma6";
+  rev = "d054a085ca6d259e7b606a81c7bd2d256a4b58f9";
+  sha256 = "1iks8x1pf7jyk4cypw8sdp6wb3m90a9b5zzvqnxiyw32ay52vhyn";
   revdate = "2020-08-15 15:55:56Z";
 }
