62dca626d46dc49bdace6d363ba1ad0f5dcf5654
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-08-24 22:42:14 +0000

    auto-updates: emacs, i3status-rust,

--- pkgs/emacs/metadata.nix
+++ pkgs/emacs/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/masm11/emacs";
   upattr = "emacs-pgtk";
   branch = "pgtk";
-  rev = "003cc352e0991a3d8f0323bf0620bf58080527d9";
-  sha256 = "1l3wyjqbdb1lpqmksknmcp8gsia5k0a2lbc1gcqw2nglmz0366ps";
+  rev = "c76a6148b8e87dd9e487247d27b3d1e614a38f47";
+  sha256 = "01xv0r0diwhmspwy8z8igji3byk2apslb1as9yhirwb3xz0wr91y";
   revdate = "2020-08-18 05:03:18Z";
 }
--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://github.com/greshake/i3status-rust";
   branch = "master";
-  rev = "25807c856336dfd605e506c7189ea363dad9ad7d";
-  sha256 = "0fw08dvgg18nj4cr1nw213w4jhkvflif1s3zglanb4rf9c61377j";
-  cargoSha256 = "1sz7sqa0r90apg928bx2ysa200vpyb2780zhwb3dbm35nxjsqpb3";
+  rev = "18db269e7702208416447e822e72adc672bbc621";
+  sha256 = "007sa9hac2i23pxssswyiccbw4r5hay6gfsmpra5kkbc40f38ka4";
+  cargoSha256 = "0g7dncq3hyrr852isr0ikm3636sijiwdgik16jaqzfiafrpi6pia";
   revdate = "2020-08-14 16:00:22Z";
 }
