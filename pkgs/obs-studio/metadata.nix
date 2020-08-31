c91b6672928fd5b75181cb4cc780f20bad887e66
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-08-31 21:13:04 +0000

    auto-updates: obs-studio, wayfire,

--- pkgs/obs-studio/metadata.nix
+++ pkgs/obs-studio/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/GeorgesStavracas/obs-studio";
   branch = "feaneron/egl-wayland";
-  rev = "b948513cf54bcd1625d3144c261cae3f38938b24";
-  sha256 = "sha256-35bdYjBxS/MeLw14auAE01U6Rz1SqLO71X4J0ldJT+0=";
+  rev = "54a06467bb0d206a182e56f1c8f59e5fd2e811ce";
+  sha256 = "0vifg50d42bqjiybz87079xjvhgh0773rfpwa7gfp6y7klc9zn1m";
   revdate = "2020-08-20 15:39:31Z";
 }
--- pkgs/wayfire/metadata.nix
+++ pkgs/wayfire/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/WayfireWM/wayfire";
   branch = "master";
-  rev = "0257f43ab3f26a3141c5d1d0799e29e557807881";
-  sha256 = "12jvanngylv0vnsypd8v82rklp7rka4197kb1hji7rxp8hmx8m9j";
+  rev = "06b22f000b24b62791a0684fcb315fb7fa157b89";
+  sha256 = "195qzk20rfb4n0qczmargc6navsf73isisy3cl2j635613379yy0";
   revdate = "2020-08-15 15:55:56Z";
 }
