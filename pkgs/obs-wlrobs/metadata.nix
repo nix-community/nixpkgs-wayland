9e7cffc2fb1d395be6899fa74f506130ef3e6e3a
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-01 02:04:22 +0000

    auto-updates: obs-wlrobs,

--- pkgs/obs-wlrobs/metadata.nix
+++ pkgs/obs-wlrobs/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wlrobs";
   branch = "default";
-  rev = "c0b86a1f57b8";
-  sha256 = "sha256-GiYNV8KRWTStbmI3BhHmR46jYCPxhdj/OB2FQmkcot4=";
+  rev = "bcc570977627";
+  sha256 = "1faiq2gdb7qis3m1hilm4pz8lkmkab75vzm608dbiazahhybf96p";
   revdate = "2020-08-12 12:00:28";
 }
