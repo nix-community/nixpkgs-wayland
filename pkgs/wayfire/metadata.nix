dcd9fba00fd2a329008b7b88e5b8fc1b4c1624f8
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-01 11:09:46 +0000

    auto-updates: wayfire, wlroots,

--- pkgs/wayfire/metadata.nix
+++ pkgs/wayfire/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/WayfireWM/wayfire";
   branch = "master";
-  rev = "06b22f000b24b62791a0684fcb315fb7fa157b89";
-  sha256 = "195qzk20rfb4n0qczmargc6navsf73isisy3cl2j635613379yy0";
+  rev = "f8ad40eab037e2fd2bee13c7d5cb99f2bc6d77dc";
+  sha256 = "1414yck9rzvhm9ybhnpi41vasyvgq5n3fzdjnz5hiv3nkssb5v0m";
   revdate = "2020-08-15 15:55:56Z";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "65abd4e92af497bbe1ae1085fa16b1b811a3c652";
-  sha256 = "1fkq9j7ifc2l90xj1qnhss6vbad8z80lhmlvk41rdm9zxl7p0a3d";
+  rev = "971de474f0a423cc8f444c92d2e1a8334db8b55d";
+  sha256 = "16f195c7ix9vlfzv9mlbm4nlnzmci2zap9040rz98jv4ni94g8g7";
   revdate = "2020-08-17 09:33:57Z";
 }
