4fb1264f3e9d5c7fcd02b1d9d46653a9f04328f5
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-13 23:04:54 +0000

    auto-updates: glpaper,

--- pkgs/glpaper/metadata.nix
+++ pkgs/glpaper/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/glpaper/";
   branch = "default";
-  rev = "e3ec1d03384f";
-  sha256 = "1c1nvyhjxkx51caz63mfhnnmwwx0j9idfpginap84fi9ma1bs657";
+  rev = "074661697bc5";
+  sha256 = "09gwgbcknbh1fqplw3ihkr587kf013z8dzgjq7i6ncmzn8fgnb36";
   revdate = "2020-08-15 15:03:52";
 }
