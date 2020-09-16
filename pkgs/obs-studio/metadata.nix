91f0471f0aeff691639b7519469b98e9e93f7507
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-16 20:05:16 +0000

    auto-updates: obs-studio, wlroots,

--- pkgs/obs-studio/metadata.nix
+++ pkgs/obs-studio/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/GeorgesStavracas/obs-studio";
   branch = "feaneron/egl-wayland";
-  rev = "1ef1cbd99ac7830675f06912687c656b6cfc4d7c";
-  sha256 = "1mi97n9lm2s1ia03vk8a3ds9w10npiidc58z8hncm4rsagw6z9iq";
+  rev = "796b3946341556cc5da2d71906800c629315203c";
+  sha256 = "1dvkqfw1l1rpghfi8m5xpk6k5k3k9j5h39md8z00ggzvl743hkqp";
   revdate = "2020-08-20 15:39:31Z";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "8ad2cc39eb420c22dde7e49c01bde916b7bc58cc";
-  sha256 = "0b381lln94a6487ac4217v4qcd2q84ya15pjb7xlhrs29hi2ymx2";
+  rev = "87836dcb55e15cd1f19f5549d4fcd666135dba15";
+  sha256 = "0110d4ndw2wrbqvry8dl59c33dgxzcd891n6vhjs17vd3j3jcxx9";
   revdate = "2020-08-17 09:33:57Z";
 }
