88208f4f31e3ddffbac98e486c1f841ae930e448
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-16 19:04:56 +0000

    auto-updates: obs-studio,

--- pkgs/obs-studio/metadata.nix
+++ pkgs/obs-studio/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/GeorgesStavracas/obs-studio";
   branch = "feaneron/egl-wayland";
-  rev = "54a06467bb0d206a182e56f1c8f59e5fd2e811ce";
-  sha256 = "0vifg50d42bqjiybz87079xjvhgh0773rfpwa7gfp6y7klc9zn1m";
+  rev = "1ef1cbd99ac7830675f06912687c656b6cfc4d7c";
+  sha256 = "1mi97n9lm2s1ia03vk8a3ds9w10npiidc58z8hncm4rsagw6z9iq";
   revdate = "2020-08-20 15:39:31Z";
 }
