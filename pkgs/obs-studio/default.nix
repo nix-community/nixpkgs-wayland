0109ab9bbcbdfd3f044cbf63587e0bc5d27c9338
Cole Mickens <cole.mickens@gmail.com>
2020-09-16 21:13:10 -0700

    obs: try to add v4l2sink patch

--- pkgs/obs-studio/default.nix
+++ pkgs/obs-studio/default.nix
@@ -51,7 +51,7 @@ in mkDerivation rec {
 
   patches = (fetchpatch {
     url = "https://github.com/obsproject/obs-studio/pull/3182/commits/51c38080f2bced5a448f556188d0d1dec2551efc.patch";
-    sha256 = "1dvkqfw1l1rpghfi8m5xpk6k5k3k9j5h39md8z00ggzvl743hkqp";
+    sha256 = "sha256-tpSYcLS5l0h4MmgAE9WayGbW2KBlbLsTDQi9dOf3T/U=";
   });
 
   nativeBuildInputs = [ cmake pkgconfig ];
