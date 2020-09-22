936700d59b3815e8d580fe51803e035ca5c9c6e0
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-22 21:05:07 +0000

    auto-updates: wayvnc,

--- pkgs/wayvnc/metadata.nix
+++ pkgs/wayvnc/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/any1/wayvnc";
   branch = "master";
-  rev = "8038e655971144af29962cdbf76a7b17be6dfb26";
-  sha256 = "0waj07qh262a67q8xgi3h2ifkrj2wx9nzblffgzsfnf3xcz6548g";
+  rev = "4a098e27f99a5299201d59234cf8d0165f13549c";
+  sha256 = "0xa9hw33hrlnq82iblrm5nlh0ahk6sn0n2qba1gs3nlspr7y2ycz";
   revdate = "2020-08-19 20:14:00Z";
 }
