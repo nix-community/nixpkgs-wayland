4df4043e34d793be69bbafaab7960259a8958f9f
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-22 01:06:49 +0000

    auto-updates: i3status-rust, nwg-launchers,

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -1,8 +1,8 @@
 {
   repo_git = "https://github.com/greshake/i3status-rust";
   branch = "master";
-  rev = "0b2e2213ebbc02a4b64d62cf1cb6bd7e8070dda5";
-  sha256 = "sha256-BDDGWWRnmw6A84kmD//Z9B3troha1+kXBG6foEHU0Vg=";
-  cargoSha256 = "sha256-rzDIGSyNjSaaZjLM3P1p3o8Gdhasnj8BWIa4vj4iLyk=";
+  rev = "3979cc961fec3af6784fee75676dd53fc3628343";
+  sha256 = "1n7kksqdw0q6hdqh9xykr2cc7miv9fycn8gp7zj0g3zmaw6a5zgl";
+  cargoSha256 = "02vc2ryh8zgl4v1qjk61f65h3vg1n06z8m4dnjnbx28qnp746f8r";
   revdate = "2020-08-14 16:00:22Z";
 }
--- pkgs/nwg-launchers/metadata.nix
+++ pkgs/nwg-launchers/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/nwg-piotr/nwg-launchers";
   branch = "master";
-  rev = "4a181e39431d7020089588848a25e14fb003c916";
-  sha256 = "09a78rhk7b6z2rwj3fs2avpzjlnpj3jkld2cbl4ixwdqslcxr3ly";
+  rev = "8fbeadb911a9ae2f66da17a9f7659845749b0653";
+  sha256 = "0r0wj4w3jj3l56z1lx6ypkzz4fsgx4vzqbvs95661l8q362pndzw";
   revdate = "2020-05-09 19:42:15Z";
 }
