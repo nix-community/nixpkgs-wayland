aa2c4aeb17d337ea2a1d22e5a5feb970d33aae54
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-09-22 08:05:23 +0000

    auto-updates: emacs,

--- pkgs/emacs/metadata.nix
+++ pkgs/emacs/metadata.nix
@@ -2,7 +2,7 @@
   repo_git = "https://github.com/masm11/emacs";
   upattr = "emacs-pgtk";
   branch = "pgtk";
-  rev = "f427182affacc6faba8fd393b4df0d748de2747d";
-  sha256 = "0pqxfdizkb0ib19kiphjmj6vzbvnd777h0anvz8v42x2xvd06vsk";
+  rev = "0b06fbaeb8f9cad185a452e44943a411d9bdedda";
+  sha256 = "0jx4qzbfgzdlzy65wbb77byhqya7i027wlvnd12p50pkc6aqn1qx";
   revdate = "2020-08-18 05:03:18Z";
 }
