2558783689ca7e4f6aa8d4819e0829b2d01e1b3a
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-11-08 14:41:39 +0000

    auto-updates: emacs, wlroots,

--- pkgs/emacs/metadata.nix
+++ pkgs/emacs/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/masm11/emacs";
   upattr = "emacs-pgtk";
   branch = "pgtk";
-  rev = "bef27f591a23fb0da071b79bf78c453c7f220a40";
-  sha256 = "0g6xniadmw4jwkgshl1j2sb5gv3f138qfnqaih6l46plc5lswml1";
+  rev = "9c7750758271139c55313e6e153c94d30123a6cc";
+  sha256 = "1vkmrbvzpmhv3qfkd846awhcikjl7v7znmq66bm9zz74h7vx5v10";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "372a52ecc08aff003b3024e852e848024b699a15";
-  sha256 = "0m039hq51r87qc7x72jp0d9jcimcmiisqivy63k6f4sbsx911ajf";
+  rev = "9595f95452a1d7420bbb190dc29c3806c29ca41e";
+  sha256 = "0alcm6vwcqjkykc66x6na47aqxnknw20gsix55kzndy8v45hshkf";
 }
