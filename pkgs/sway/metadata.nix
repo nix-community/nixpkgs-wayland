2ab8c0fb3edfea17bd0275ff225985c0916a2986
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-11-08 17:06:29 +0000

    auto-updates: sway, wlroots,

--- pkgs/sway/metadata.nix
+++ pkgs/sway/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/swaywm/sway";
   branch = "master";
   upattr = "sway-unwrapped";
-  rev = "0df5753aed57c6b5e3a5f64ff410cadf8d96c35c";
-  sha256 = "0a3qk6jhzakzz0rf39p5lhgk7gww4lys90h8h1hzwgxzqylv873d";
+  rev = "5ae4f65045c04df7ad2d87142b4064803d97a73e";
+  sha256 = "0fc5vm70qzznnipjzhwry0x0awiwyj3hwrg5lkh36zhk661vbxss";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "9595f95452a1d7420bbb190dc29c3806c29ca41e";
-  sha256 = "0alcm6vwcqjkykc66x6na47aqxnknw20gsix55kzndy8v45hshkf";
+  rev = "238d1c078fb03338e9f271d98f7bf6b1fc399285";
+  sha256 = "01j38lmgs2c6fq68v8b75pkilia2wsgzgp46ivfbi9hhx47kgcfn";
 }
