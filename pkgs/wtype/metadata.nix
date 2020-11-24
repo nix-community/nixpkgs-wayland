6db3fe245bb0e216452b0080b5d3ab78ef41e491
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-11-24 12:08:50 +0000

    auto-updates: wtype,

--- pkgs/wtype/metadata.nix
+++ pkgs/wtype/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/atx/wtype";
   branch = "master";
-  rev = "bc262ff93e559c2f0b6da70557d3bec63173d520";
-  sha256 = "1sxczrvyp4iadanxvw3c229dcwbjbbgyv4smdfnzf3hf5b1hsakv";
+  rev = "21528f2ac055b84d6314c5c4a3159862486066c1";
+  sha256 = "19g7rzwxvp7w2z85dlj144yd4maibfvglx17hs9w58wixdbqh456";
 }
