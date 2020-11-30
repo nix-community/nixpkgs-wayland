aafac875d55e0db5f30aeaedc6f157e197f193c8
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-11-30 10:08:58 +0000

    auto-updates: wayfire, wlfreerdp,

--- pkgs/wayfire/metadata.nix
+++ pkgs/wayfire/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/WayfireWM/wayfire";
   branch = "master";
-  rev = "c180c29dc33f52c96a12c74243025a37c01c13c3";
-  sha256 = "0axv8srngya7hzdfz2yhy9zyxcqah6pcdyqs1ycs97rrinjsj2yb";
+  rev = "aaff4711430ad41d20576ba8f34b0cea43fa0891";
+  sha256 = "1ds47hkhffa3hkl9cknksw8wpq6rsva7r49hgxdwdfnzn0hnaiqr";
 }
--- pkgs/wlfreerdp/metadata.nix
+++ pkgs/wlfreerdp/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/FreeRDP/FreeRDP";
   branch = "master";
-  rev = "2b676bf1e7de111cd63c7f77cbbbf1d299b3a8fd";
-  sha256 = "1vr351zwixzlx8wi1lv1l55v05zqsvl20yj1fr4qbk1vyk0n0hy3";
+  rev = "a2308507aa7f6e086e7c21e4f81d599dbb4b73d6";
+  sha256 = "15j5ag57jnkxxaba4x3r8ara378d2754cw5vksrl97vv4q06wcy9";
 }
