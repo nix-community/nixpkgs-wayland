8e4f2b5f39722fe5e8b30a52974e72fd3fe63f85
ishan <ishan365.ia@gmail.com>
2020-12-04 19:37:25 +0530

    fix the sha256

--- pkgs/wcm/metadata.nix
+++ pkgs/wcm/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/WayfireWM/wcm";
   branch = "master";
   rev = "5d935e0727ab976c85690669083addc9c21ee298";
-  sha256 = "0000000000000000000000000000000000000000000000000000";
+  sha256 = "sha256-Nrz/Em80+C8uTUf0+Ny/WLcMKFVRKAzXaeIp3m7NsnU=";
 }
