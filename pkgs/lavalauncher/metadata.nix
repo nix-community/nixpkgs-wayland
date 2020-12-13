024c012ac5e37043a7bfc59a3e2eeaa7f90b2ab3
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-13 12:12:16 +0000

    auto-updates: lavalauncher, wlroots,

--- pkgs/lavalauncher/metadata.nix
+++ pkgs/lavalauncher/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~leon_plickat/lavalauncher";
   branch = "master";
-  rev = "c017879c2fa8bc609807e06c9f81c3fd762add72";
-  sha256 = "03x4iapdk5yzmb62ybzvv82piw1x17wi39s2rb9f6bswjx825cwq";
+  rev = "1058077aac0f514eaf53cff0382a2b7056fc9ed1";
+  sha256 = "0qbiqya1267dzh11d4yw6pkbwm1h11zm04b9zcak0mr2pjiiyzpc";
 }
--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "4c363a564f7ce1ef722417e8b8a0607c1004c828";
-  sha256 = "1545cb72qns3ax0qcpr68i4r6wcn6sxg4n1wwrvxpncn42321vfv";
+  rev = "525fa6ada067084bc63e3002ba12e07499623514";
+  sha256 = "18b3jnfjwmwjavwblvr29s4ml0718pjhphj4kwnb8lrmli8jzbr7";
 }
