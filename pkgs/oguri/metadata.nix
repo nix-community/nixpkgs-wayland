3cee0ad86e318ccb95a4f00b879bf69b35e18b62
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-19 17:07:11 +0000

    auto-updates: oguri,

--- pkgs/oguri/metadata.nix
+++ pkgs/oguri/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/vilhalmer/oguri";
   branch = "master";
-  rev = "b822c12b52571bb0ebabed934d4a7a78d1426619";
-  sha256 = "0n2ijl7s2i6vz26bhgg7zhjq44nm457ylpx0wh8rpw2nhzfw3990";
+  rev = "6937fee10a9b0ef3ad8f94f606c0e0d9e7dec564";
+  sha256 = "110dr5hncnisskl1cpw20kb7dsz29vs03l65v7fk6g6qiyj6ywxi";
 }
