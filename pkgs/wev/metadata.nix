aa225cc99dc051fcd08727aa533d7032d0853e78
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:02:13 +0000

    auto-update(378018): wev: 54de46d120396ead4dcbce0b52cf506c200380f5 => 54de46d120396ead4dcbce0b52cf506c200380f5

--- pkgs/wev/metadata.nix
+++ pkgs/wev/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://git.sr.ht/~sircmpwn/wev";
   branch = "master";
   rev = "54de46d120396ead4dcbce0b52cf506c200380f5";
-  sha256 = "0v5vlahf3j464vvggfm8s2dnk3hcdx59dnshlbkcn9dh8cv9gy7y";
+  sha256 = "sha256-/viXNkOwJcvmolDblkpvDI5pm9Couvf2JobI4aCiu2w=";
 }
