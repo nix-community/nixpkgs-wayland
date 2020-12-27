c41f8bba034c1d2ed73729037470941147482bd3
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:02:10 +0000

    auto-update(378018): wdisplays: dd7e1e22ee04cf37ecc8b48cdb0c103de18e8761 => dd7e1e22ee04cf37ecc8b48cdb0c103de18e8761

--- pkgs/wdisplays/metadata.nix
+++ pkgs/wdisplays/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/cyclopsian/wdisplays";
   branch = "master";
   rev = "dd7e1e22ee04cf37ecc8b48cdb0c103de18e8761";
-  sha256 = "1dyaqhlfv4s5w9mgyl2li2ca7ya5wx2j8942vkkra8z0rbs3m3qi";
+  sha256 = "sha256-EY869MrgI5Xn3IIkJEXnRfmjmIhUUP9q4kWT7SjEyrc=";
 }
