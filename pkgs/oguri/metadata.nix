ffb4e831d3321686d7cd4a50b88377f56d423896
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:00:57 +0000

    auto-update(378018): oguri: 6937fee10a9b0ef3ad8f94f606c0e0d9e7dec564 => 6937fee10a9b0ef3ad8f94f606c0e0d9e7dec564

--- pkgs/oguri/metadata.nix
+++ pkgs/oguri/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/vilhalmer/oguri";
   branch = "master";
   rev = "6937fee10a9b0ef3ad8f94f606c0e0d9e7dec564";
-  sha256 = "110dr5hncnisskl1cpw20kb7dsz29vs03l65v7fk6g6qiyj6ywxi";
+  sha256 = "sha256-sXNvpI/YPDPd2cXQAfRO4ut21gSCXxbo1DpaZmHJDYQ=";
 }
