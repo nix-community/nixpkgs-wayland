69cfafd714c44df3fb666b5758603b6d05bece1c
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 03:59:16 +0000

    auto-update(378018): kanshi: f88ba3a1dd3aa9099f291c365e628d2ffbf0cac6 => f88ba3a1dd3aa9099f291c365e628d2ffbf0cac6

--- pkgs/kanshi/metadata.nix
+++ pkgs/kanshi/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/emersion/kanshi";
   branch = "master";
   rev = "f88ba3a1dd3aa9099f291c365e628d2ffbf0cac6";
-  sha256 = "15yzka9a2rh4fli02hg3p7vc4npdb13j4ff3lr7gqv00dgqj2d3q";
+  sha256 = "sha256-eDQh8WsAbPxOpsM5IkdY7VrC9rnjQQEidQRmoZKa35c=";
 }
