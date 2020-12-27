bef26981fb65b9e6077c48c6992aead7db107c5f
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:04:03 +0000

    auto-update(378018): wlr-randr: 988a802f5b4050dd3db1138a0cf3464789131dfb => 988a802f5b4050dd3db1138a0cf3464789131dfb

--- pkgs/wlr-randr/metadata.nix
+++ pkgs/wlr-randr/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/emersion/wlr-randr";
   branch = "master";
   rev = "988a802f5b4050dd3db1138a0cf3464789131dfb";
-  sha256 = "10c8zzp78s5bw34vvjhilipa28bsdx3jbyhnxgp8f8kawh3cvgsc";
+  sha256 = "sha256-TL/NBuRqIofu6xb6JUdveiGhbqQRyr3J4KtodO7/iIE=";
 }
