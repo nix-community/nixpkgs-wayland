e4ad507c6e48c827e7827b18cdbd8b4ff6643d79
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 03:56:24 +0000

    auto-update(378018): cage: 0d85c1652e676806a5c065bdc88e83f0b33a0a5e => 0d85c1652e676806a5c065bdc88e83f0b33a0a5e

--- pkgs/cage/metadata.nix
+++ pkgs/cage/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/Hjdskes/cage";
   branch = "master";
   rev = "0d85c1652e676806a5c065bdc88e83f0b33a0a5e";
-  sha256 = "1z2wd3xi5r061cxifkpk12rqnkb5fvn7abz2qnqk19rvxqiqya4z";
+  sha256 = "sha256-nyiPI+47pzCxxeIvdex2ZU2LswjzThc7CwbkEvtoXPw=";
 }
