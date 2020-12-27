d22650eada32ad9c2c8e3394b4b1a5d0834ac4bc
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:02:36 +0000

    auto-update(378018): wlay: 1e316ca92d38856c517af4a0a7d88c8589bd2131 => 1e316ca92d38856c517af4a0a7d88c8589bd2131

--- pkgs/wlay/metadata.nix
+++ pkgs/wlay/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/atx/wlay";
   branch = "master";
   rev = "1e316ca92d38856c517af4a0a7d88c8589bd2131";
-  sha256 = "1p19zr9qfd94wrjf53gcqn8cc9axjrcgkv5jvlhblhqal2vfnzp7";
+  sha256 = "sha256-537rtqAKQ7og3bLs+ViWXSXGkMXsjeJk5iQ1h1P+Kdw=";
 }
