e7b439aff2052a9698a2cf62e5fd953cf1b49dfc
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 03:57:18 +0000

    auto-update(378018): gebaar-libinput: 8c3f67db473896fd8d369d7f8492a8c8e83b44a1 => 8c3f67db473896fd8d369d7f8492a8c8e83b44a1

--- pkgs/gebaar-libinput/metadata.nix
+++ pkgs/gebaar-libinput/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/Osleg/gebaar-libinput-fork";
   branch = "master";
   rev = "8c3f67db473896fd8d369d7f8492a8c8e83b44a1";
-  sha256 = "KrWJBS/j1wD/2qyhlyThhEgRGwgLYIgGEBQsAU1r5hU=";
+  sha256 = "sha256-KrWJBS/j1wD/2qyhlyThhEgRGwgLYIgGEBQsAU1r5hU=";
 }
