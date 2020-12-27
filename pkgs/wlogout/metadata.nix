fcd5034498f9f926723b27694519d0a7b79407aa
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:03:52 +0000

    auto-update(378018): wlogout: 0a4289e02c6c4337756d4df7ff31605f8c8795c3 => 0a4289e02c6c4337756d4df7ff31605f8c8795c3

--- pkgs/wlogout/metadata.nix
+++ pkgs/wlogout/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/ArtsyMacaw/wlogout";
   branch = "master";
   rev = "0a4289e02c6c4337756d4df7ff31605f8c8795c3";
-  sha256 = "1swhzkqkzli59c89pvrakfvicd00x7ga860c3x2pbb4y3xziqfvi";
+  sha256 = "sha256-cTscfx+erHVFHwwYpN7pADQWt5sq75sQSyXSP/H8kOs=";
 }
