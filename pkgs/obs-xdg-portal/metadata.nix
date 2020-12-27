44ed9bd0cd0928962b0c9340849850e9369b5ab8
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:00:51 +0000

    auto-update(378018): obs-xdg-portal: 066acfd9f18a1b75e4be5b0176ada71c7628c3d7 => 066acfd9f18a1b75e4be5b0176ada71c7628c3d7

--- pkgs/obs-xdg-portal/metadata.nix
+++ pkgs/obs-xdg-portal/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://gitlab.gnome.org/feaneron/obs-xdg-portal.git";
   branch = "master";
   rev = "066acfd9f18a1b75e4be5b0176ada71c7628c3d7";
-  sha256 = "0myqcqigd0964cl0kzg2nkbavqjxr58w9nhdxhy4wrcq1szq6y1g";
+  sha256 = "sha256-L3iDvw6YZU487A3axFHJXeKt1rTi/QkoIyaB9iJm2Fc=";
 }
