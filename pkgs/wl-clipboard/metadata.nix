bd8577ba99e151e747b81997de9f8e43f37547c6
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:04:20 +0000

    auto-update(378018): wl-clipboard: aa4633b894c3c5ae6053026529ec9288566060a6 => aa4633b894c3c5ae6053026529ec9288566060a6

--- pkgs/wl-clipboard/metadata.nix
+++ pkgs/wl-clipboard/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/bugaevc/wl-clipboard";
   branch = "master";
   rev = "aa4633b894c3c5ae6053026529ec9288566060a6";
-  sha256 = "0inxlj2hgx6i1v2bccgycixpak4xkfjygn4065g445h99dymhbvc";
+  sha256 = "sha256-bC9YfUsJFkJeMYDY56WbnUx1e2T+MbbEDtH0B4Wk3UY=";
 }
