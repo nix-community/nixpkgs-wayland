d3ff9fa759e847d2a99dbceed27385dc64fa13f5
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-27 04:04:29 +0000

    auto-update(378018): wl-gammactl: 611846ce8f5f6e7fe1afb100416cceb8af54afb2 => 611846ce8f5f6e7fe1afb100416cceb8af54afb2

--- pkgs/wl-gammactl/metadata.nix
+++ pkgs/wl-gammactl/metadata.nix
@@ -2,5 +2,5 @@
   repo_git = "https://github.com/mischw/wl-gammactl";
   branch = "master";
   rev = "611846ce8f5f6e7fe1afb100416cceb8af54afb2";
-  sha256 = "0s54hq6l2552bwm0c7v0higlj5bpiwf12nc6wb130a06jgrbgxz2";
+  sha256 = "sha256-4ve38pMGKDDC4oZZERyPdxVJX4RgHwYqX6IUQQ2GpGg=";
 }
