8a9ed9dd97f069dab87bdb7523c906c20a8592e0
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-28 20:07:05 +0000

    auto-update(379434): wtype: 1c20416a958279c903ccd0ee6da301e5554b5e15 => fb8cb526470308d36b28c33620fcb74916f952e8

--- pkgs/wtype/metadata.nix
+++ pkgs/wtype/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/atx/wtype";
   branch = "master";
-  rev = "1c20416a958279c903ccd0ee6da301e5554b5e15";
-  sha256 = "sha256-5+xnAkKwpt1Liw00D8w2vyQy6WbFzujCw+QZIAS046c=";
+  rev = "fb8cb526470308d36b28c33620fcb74916f952e8";
+  sha256 = "sha256-+wJPUXTmSsmgPXnwdjZ+9crlWIJn7uOznnmRlgWDkrw=";
 }
