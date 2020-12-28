725dfaae3b90b111ea78841e47ca9c76c2189f0d
Cole Botkens <cole.mickens+colebot@gmail.com>
2020-12-28 16:04:49 +0000

    auto-update(379303): obs-studio-dmabuf: ce2a0a7597f74aaacf0311fa78523fea151c4a72 => efdd59b43953bc066f201dd67cab48cbcf71b92d

--- pkgs/obs-studio-dmabuf/metadata.nix
+++ pkgs/obs-studio-dmabuf/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/GeorgesStavracas/obs-studio";
   branch = "feaneron/egl-dmabuf";
-  rev = "ce2a0a7597f74aaacf0311fa78523fea151c4a72";
-  sha256 = "sha256-DZSxqMQT9xa0nahy6208mPpoZC20tJ/zsY6SEfcpTPs=";
+  rev = "efdd59b43953bc066f201dd67cab48cbcf71b92d";
+  sha256 = "sha256-GxNY/rWY9VJ8DCQD2mPAUkmWUshdvp8LzZ0rYWH0ViM=";
 }
