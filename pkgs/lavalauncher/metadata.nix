6d0bcc3363a0618430c13c65f16c2d90a3c001b5
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-01-04 22:03:49 +0000

    auto-update(386557): lavalauncher: b3ec10f17f5c3c64c545dac8408fb9e7d40c27f4 => 5e11cecb73ad415d1b8838b73cf61fb995fe8286

--- pkgs/lavalauncher/metadata.nix
+++ pkgs/lavalauncher/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~leon_plickat/lavalauncher";
   branch = "master";
-  rev = "b3ec10f17f5c3c64c545dac8408fb9e7d40c27f4";
-  sha256 = "sha256-Qt9iX96mJ+oWFL953Cq8MKfRn9Qvvqd78mZ3mN3T9AI=";
+  rev = "5e11cecb73ad415d1b8838b73cf61fb995fe8286";
+  sha256 = "sha256-RKrqRuCmRruT36ZaWi+nGow7Jtw2VgyZg6n/eAHiGa4=";
 }
