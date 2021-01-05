78e31c10639cbfe56233f9500a586a48606285a9
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-01-05 19:19:05 +0000

    auto-update(387673): obs-wlrobs: f2fd7d8c14de => 02e7fd0062af

--- pkgs/obs-wlrobs/metadata.nix
+++ pkgs/obs-wlrobs/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wlrobs";
   branch = "default";
-  rev = "f2fd7d8c14de";
-  sha256 = "sha256-pjTc4sGyxUOJWrWYZ5DrEUJDlfoFyZoo9bXwMc7+p+Y=";
+  rev = "02e7fd0062af";
+  sha256 = "sha256-8U+TzyS/jV2LUCdrx7w8aquBt2bcPPEoFFpMSEDNfaQ=";
 }
