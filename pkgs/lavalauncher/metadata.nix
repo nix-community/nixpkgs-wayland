8595ccaa16a496ab73620fc9eff7ab491af00ef3
Cole Mickens <cole.mickens@gmail.com>
2021-01-08 08:02:00 +0000

    auto-update(manual): lavalauncher: 9194d5cb76afef5d359d8d5dac61d7e0799d8343 => 0f447ead6fd380e6ce07bfc3fa4fb6b04eb8e9aa

--- pkgs/lavalauncher/metadata.nix
+++ pkgs/lavalauncher/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~leon_plickat/lavalauncher";
   branch = "master";
-  rev = "9194d5cb76afef5d359d8d5dac61d7e0799d8343";
-  sha256 = "sha256-6CiGKvHUnp2ykQquskgGgqaXqbnOVJsx/YmBEY1o0ko=";
+  rev = "0f447ead6fd380e6ce07bfc3fa4fb6b04eb8e9aa";
+  sha256 = "sha256-uQk1HZbiv5uHMm8zLhRT8dibTJtJ8qP8ezGhCRKYWIo=";
 }
