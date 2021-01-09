4f4341a026ffa648a8ab41654f6280db4f182e06
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-01-09 23:53:54 +0000

    auto-update(391745): xdg-desktop-portal-wlr: 727f13f34da2ef154aabcf70ad2c2e82ca223504 => cf3e02695e3d54de142b7b1b9740fb2776be1fc4

--- pkgs/xdg-desktop-portal-wlr/metadata.nix
+++ pkgs/xdg-desktop-portal-wlr/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/xdg-desktop-portal-wlr";
   branch = "master";
-  rev = "727f13f34da2ef154aabcf70ad2c2e82ca223504";
-  sha256 = "sha256-fYjWmMA4v/YKzyL9xFJWwukYRgawiVBOAaZzGXxE6VY=";
+  rev = "cf3e02695e3d54de142b7b1b9740fb2776be1fc4";
+  sha256 = "sha256-6ILnPoE+PyIrEWHjvIgaXfrE+A4Ekbbjd2PD8mkfcAQ=";
 }
