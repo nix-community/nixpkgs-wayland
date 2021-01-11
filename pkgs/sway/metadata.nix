a70eabe36ab37c7275c50211e6019411af680e7b
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-01-11 11:42:28 +0000

    auto-update(392915): sway: 661cdac2d648f1f2abcaa2304c0b19d997a06088 => a7414885d2a1ed4ab09869492547e2eaa09b7beb

--- pkgs/sway/metadata.nix
+++ pkgs/sway/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/swaywm/sway";
   branch = "master";
   upattr = "sway-unwrapped";
-  rev = "661cdac2d648f1f2abcaa2304c0b19d997a06088";
-  sha256 = "sha256-2jaA5/HEiiCUhULyZGbdvA/kohVY/JWtjaSeCszmNak=";
+  rev = "a7414885d2a1ed4ab09869492547e2eaa09b7beb";
+  sha256 = "sha256-iuZkC722hX3P/biwKXaULEvFqJ1nQ/bILKNwTJOhTlI=";
 }
