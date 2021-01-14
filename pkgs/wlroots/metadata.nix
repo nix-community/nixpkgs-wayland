2c1b36311146a9e802e54a4ef9c83f6952da8374
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-01-14 12:16:09 +0000

    auto-update(395698): wlroots: 02a086599c069768987bc9c6d9a8f79b6bfd8811 => a6a0568316233fc1988cd81c564e9b913ab476d0

--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/swaywm/wlroots";
   branch = "master";
-  rev = "02a086599c069768987bc9c6d9a8f79b6bfd8811";
-  sha256 = "sha256-1kyayEKeLO3lW6aqnStk95EsAWx5gpBza7T/cJebFp0=";
+  rev = "a6a0568316233fc1988cd81c564e9b913ab476d0";
+  sha256 = "sha256-gZqenxW4DEVI/Li5sWwsrnLkY2m4CR4VpwIXlZh5M4w=";
 }
