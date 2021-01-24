15eb7468bcee6ae9c2e03c7cc78e19bab9d322b1
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-01-24 06:05:30 +0000

    auto-update(405506): lavalauncher: ac017c09f37f59045f0a942ca7360b0362f7200a => 500096914af677bd75bffd6751bbc6b0951fe53e

--- pkgs/lavalauncher/metadata.nix
+++ pkgs/lavalauncher/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~leon_plickat/lavalauncher";
   branch = "master";
-  rev = "ac017c09f37f59045f0a942ca7360b0362f7200a";
-  sha256 = "sha256-WF4gsrK7xCnv+782vLh85gaF/zwX8yZSjxlGgavWZBw=";
+  rev = "500096914af677bd75bffd6751bbc6b0951fe53e";
+  sha256 = "sha256-fDUuWesaoFRavdvbZ6Qmbw5k5uE+dD6jZfZuTvOfdN0=";
 }
