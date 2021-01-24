9f59530faa9948f84883bf6cd8b81461b68e4e75
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-01-24 06:04:58 +0000

    auto-update(405506): i3status-rust: a920c577669042bbf8d720c5af26355c166f6390 => 244bab0d027a21da369e95199673c02349f57b15

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/greshake/i3status-rust";
   branch = "master";
-  rev = "a920c577669042bbf8d720c5af26355c166f6390";
-  sha256 = "sha256-i74dUz0jN+avKKU190/yNU5F9oKl05XM47F4w+/rzew=";
-  cargoSha256 = "sha256-qkrbbVAlce4Y5JnxDc9j/XqHr2izA6XHDpTiUSnJUd8=";
+  rev = "244bab0d027a21da369e95199673c02349f57b15";
+  sha256 = "sha256-/nS/JTz2LinHhmJOYNPPaQXYCMdoTaLUkdMm4clLDdE=";
+  cargoSha256 = "sha256-prm29hkKVt25mFaiZc7FXUSPmbrHjmErl32C4IMRoB8=";
 }
