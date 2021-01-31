0745090cf84e94ba43b3baa2270765826ed0e253
Cole Mickens <cole.mickens@gmail.com>
2021-01-31 23:34:24 +0000

    flake.nix: make unstableSmallPkgs work again

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           config.allowUnfree = true;
         };
       pkgs_ = genAttrs (builtins.attrNames inputs) (inp: genAttrs supportedSystems (sys: pkgsFor inputs."${inp}" sys []));
-      fullPkgs_ = genAttrs supportedSystems (sys: pkgsFor inputs.nixpkgs sys [ inputs.self.overlay ]);
+      fullPkgs_ = genAttrs (builtins.attrNames inputs) (inp: genAttrs supportedSystems (sys: pkgsFor inputs."${inp}" sys [inputs.self.overlay]));
     in
     rec {
       overlay = final: prev:
@@ -102,24 +102,24 @@
           waylandPkgs // { inherit waylandPkgs; };
 
       packages = forAllSystems (system:
-        fullPkgs_.${system}.waylandPkgs
+        fullPkgs_.nixpkgs.${system}.waylandPkgs
       );
 
       unstableSmallPkgs = forAllSystems (system: with fullPkgs_.unstableSmall.${system};
           linkFarmFromDrvs "wayland-packages-unstable-small"
             (builtins.attrValues waylandPkgs));
 
-      unstablePkgs = forAllSystems (system: with fullPkgs_.${system};
+      unstablePkgs = forAllSystems (system: with fullPkgs_.nixpkgs.${system};
           linkFarmFromDrvs "wayland-packages-unstable"
             (builtins.attrValues waylandPkgs));
 
       defaultPackage = unstablePkgs;
 
       devShell = forAllSystems (system:
-        fullPkgs_.${system}.mkShell {
+        fullPkgs_.nixpkgs.${system}.mkShell {
           nativeBuildInputs = []
             ++ (with pkgs_.cachix.${system}; [ cachix ])
-            ++ (with fullPkgs_.${system}; [
+            ++ (with fullPkgs_.nixpkgs.${system}; [
                 nixUnstable nix-prefetch nix-build-uncached
                 bash cacert curl git jq mercurial openssh ripgrep parallel
             ])
