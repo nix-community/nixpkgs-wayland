18c35eeba4a0c789349c197eeda2aaeccc2e863a
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-02-02 08:04:32 +0000

    auto-update(414239): waybar: 7d78a3aeef58a03698f4564260fad0f7cd57d35f => f2e9bb54f0a495fa53ea594d6846b903f1c231f6

--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/Alexays/Waybar";
   branch = "master";
-  rev = "7d78a3aeef58a03698f4564260fad0f7cd57d35f";
-  sha256 = "sha256-vxdzQmyvfVd/Zo8tjAtKX969SSkAlgQIaPjgBm2ruv4=";
+  rev = "f2e9bb54f0a495fa53ea594d6846b903f1c231f6";
+  sha256 = "sha256-MqeAGMQwzcwd2mLsO9C/14f6e8v0vjubQ/cXv13QoZc=";
 }
