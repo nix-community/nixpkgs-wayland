6ea4e4546d4f5795e3890f40e4d1e0fc55cd5154
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-02-03 00:06:10 +0000

    auto-update(415072): swayidle: 068942751ba459ef3b9ba0ec8eddf9f6f212c4d7 => 7dafac758c6071146a711f317ecfba77f8261c6a

--- pkgs/swayidle/metadata.nix
+++ pkgs/swayidle/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/swaywm/swayidle";
   branch = "master";
-  rev = "068942751ba459ef3b9ba0ec8eddf9f6f212c4d7";
-  sha256 = "sha256-c3fLdyLHqgZFRd/BZtRwCcktulXBOeJFuzVqdHOwgtY=";
+  rev = "7dafac758c6071146a711f317ecfba77f8261c6a";
+  sha256 = "sha256-YaoYjmVhjxkto3j0PzSJneIoW7uHsQBodDtw7VV6wGs=";
 }
