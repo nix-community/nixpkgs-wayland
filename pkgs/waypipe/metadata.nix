aebe3e48cffba56c9c4520685eec304ce510d832
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-02-08 08:16:41 +0000

    auto-update(419523): waypipe: fcb9809b1d0ec6b8bde348e8be2c56c4be6a1098 => 72169ea20b2cc3fcc51ae08c9131e02464caac6f

--- pkgs/waypipe/metadata.nix
+++ pkgs/waypipe/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://gitlab.freedesktop.org/mstoeckl/waypipe.git";
   branch = "master";
-  rev = "fcb9809b1d0ec6b8bde348e8be2c56c4be6a1098";
-  sha256 = "sha256-YMUOIRJnhC2d+Z6NHLbMHXpepZvmlMBuCXvxs8oxVLE=";
+  rev = "72169ea20b2cc3fcc51ae08c9131e02464caac6f";
+  sha256 = "sha256-aREsZ3ubkCdWE9hxj0orFDI/vdkVFqW8giMxu+lBPzw=";
 }
