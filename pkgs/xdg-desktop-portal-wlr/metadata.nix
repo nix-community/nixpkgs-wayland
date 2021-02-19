4ab5dc3162f6b90fe73765d1d62604d24d3aae2b
Cole Mickens <cole.mickens@gmail.com>
2021-02-18 16:37:11 -0800

    auto-update(manual): xdg-desktop-portal-wlr: cf3e02695e3d54de142b7b1b9740fb2776be1fc4 => e103e120e20e83acec5b66ce4cd33a0eb86df0b0

--- pkgs/xdg-desktop-portal-wlr/metadata.nix
+++ pkgs/xdg-desktop-portal-wlr/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/xdg-desktop-portal-wlr";
   branch = "master";
-  rev = "cf3e02695e3d54de142b7b1b9740fb2776be1fc4";
-  sha256 = "sha256-6ILnPoE+PyIrEWHjvIgaXfrE+A4Ekbbjd2PD8mkfcAQ=";
+  rev = "e103e120e20e83acec5b66ce4cd33a0eb86df0b0";
+  sha256 = "sha256-3WnWHn/YVrgEKI05tM5jnzxzBU3HUIh+EbyHFYcHX+4=";
 }
