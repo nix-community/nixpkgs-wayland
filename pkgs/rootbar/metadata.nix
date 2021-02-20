9cd75ff5a334db542d3306d1bc0c0b86b3c8ecf8
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-02-20 10:04:33 +0000

    auto-update(431960): rootbar: 250d8a3e4f67 => 25212aec3af5

--- pkgs/rootbar/metadata.nix
+++ pkgs/rootbar/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/rootbar";
   branch = "default";
-  rev = "250d8a3e4f67";
-  sha256 = "sha256-J4CZuDjyiz6Mq8ocBIXvl7I0xX9PEQPfrN9KKMDqadA=";
+  rev = "25212aec3af5";
+  sha256 = "sha256-Lp8ulmYCtwcJRAZMalzVa+AmoJt4mHF0Vs2Fs5wTRUE=";
 }
