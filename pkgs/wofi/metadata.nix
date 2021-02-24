278d2a4b3a0188fbf065fec04618488e95b665ae
Cole Mickens <cole.mickens@gmail.com>
2021-02-24 15:52:53 -0800

    auto-update(manual): wofi: 85352f091b72 => bae1c99a465f

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wofi";
   branch = "default";
-  rev = "85352f091b72";
-  sha256 = "sha256-mwjhnJ6TUAix22pBpPMkF0YvR74xOl6DM0JtvXJZ5zQ=";
+  rev = "bae1c99a465f";
+  sha256 = "sha256-JIUiZ6rr0aBEFaTHvoL/BJkiCRChmT+PohGOthVbuZw=";
 }
