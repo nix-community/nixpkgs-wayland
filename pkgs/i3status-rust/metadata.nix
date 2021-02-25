4c07d2700aa0f7959263a7475579464f850d68cc
Cole Mickens <cole.mickens@gmail.com>
2021-02-25 14:52:59 -0800

    i3status-rust: skip for a bit

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -4,4 +4,5 @@
   rev = "ef23e86da403fcbca6e3c003a415afd963f053d8";
   sha256 = "sha256-eFNfai4mitAzVy3R0rgwKRzZucvko5W0OBSk5mYwyW4=";
   cargoSha256 = "sha256-1iOgB/UK0EWC7ZgLdwgtjBccUSyFC+vTHzEkKSO951w=";
+  skip = true;
 }
