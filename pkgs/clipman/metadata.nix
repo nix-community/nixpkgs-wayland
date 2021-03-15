e10f8c55c6322fd0990c6395e56f0794bfe4f0e7
Cole Mickens <cole.mickens@gmail.com>
2021-03-15 03:05:30 -0700

    clipman fixup

--- pkgs/clipman/metadata.nix
+++ pkgs/clipman/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/yory8/clipman";
   branch = "master";
-  rev = "e77051c01d4cc39421e4984c620ec6bc5379cb6a";
-  sha256 = "sha256-6zJlSZOmcVrq8B+rqlbLK6KLpS34GvDqpX33gOWS+2o=";
+  rev = "cf88ab521ad987b232a60e088a9b9b435a0532d2";
+  sha256 = "sha256-aPo649E6lCGFBi1ps8WP7ybtHcG7qcb02d7jtyJ7eak=";
   vendorSha256 = "sha256-Z/sVCJz/igPDdeczC6pemLub6X6z4ZGlBwBmRsEnXKI=";
 }
