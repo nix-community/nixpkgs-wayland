ca4e7f0ddce87243ac678ec46ac8cd0e23a97beb
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-03-20 20:15:38 +0000

    auto-update(465151): obs-studio: 4ed7cd463d896fb27b67d6625ffba50f3b1178e4 => fc9716f0dd29c0df4b2170b69c14af387199bd61

--- pkgs/obs-studio/metadata.nix
+++ pkgs/obs-studio/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/obsproject/obs-studio";
   branch = "master";
-  rev = "4ed7cd463d896fb27b67d6625ffba50f3b1178e4";
-  sha256 = "sha256-3F2XdYv4uFJcIkcuCDwjHKzTKZOns3B1tPLC4tmIkI8=";
+  rev = "fc9716f0dd29c0df4b2170b69c14af387199bd61";
+  sha256 = "sha256-MCvEaYtFI0dYZQ+yzuiKSjFq1Wr39UhbwfwIhIfPsrs=";
 }
