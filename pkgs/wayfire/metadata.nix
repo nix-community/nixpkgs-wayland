bffc6d0c6b22f9c0d8068ce71dd317ab2a411b05
Cole Mickens <cole.mickens@gmail.com>
2021-03-29 04:50:06 +0000

    auto-update(manual): wayfire: 15191ef60f346950580034fe25fa7e7009a397b6 => 30f13ab31cad83b9e3773c26cd64fa885e7a0d9b

--- pkgs/wayfire/metadata.nix
+++ pkgs/wayfire/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/WayfireWM/wayfire";
   branch = "master";
-  rev = "15191ef60f346950580034fe25fa7e7009a397b6";
-  sha256 = "sha256-P/Q7ZXl+pOLKDZa9wmdlUJuUJ7WnbWo7C46sJecRXNA=";
+  rev = "30f13ab31cad83b9e3773c26cd64fa885e7a0d9b";
+  sha256 = "sha256-6QqLAxhIwA+h5kj02l5KcJ68td29GzyM/bSoZc1XAjw=";
 }
