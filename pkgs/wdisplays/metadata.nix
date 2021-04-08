7c44c2a0a65a1e678f27f1eb4e0d95d46f6e2248
Nicolas Berbiche <nicolas@normie.dev>
2021-04-08 17:32:06 -0400

    wdisplays: update upstream branch to fix-global-version
    wdisplays failed to launch

--- pkgs/wdisplays/metadata.nix
+++ pkgs/wdisplays/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/wdisplays";
-  branch = "master";
-  rev = "dd7e1e22ee04cf37ecc8b48cdb0c103de18e8761";
-  sha256 = "sha256-EY869MrgI5Xn3IIkJEXnRfmjmIhUUP9q4kWT7SjEyrc=";
+  branch = "fix-global-version";
+  rev = "5198a9c94b40ff157c284df413be5402f1b75118";
+  sha256 = "sha256-m+65yf0Y3YXwxDLztNUEoobPNzm58DZEzqZEJHQjP+0=";
 }
