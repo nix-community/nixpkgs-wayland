3414ad475ae8d0dfe528068d05f7107a1022d2a6
Cole Mickens <cole.mickens@gmail.com>
2021-04-12 09:04:05 +0000

    auto-update(manual): wayvnc: 81192ac74df316e0faf7bc2f985741f701db753d => 7a32cae4acfcd35e27d06bb728cf6e2b85f7c094

--- pkgs/wayvnc/metadata.nix
+++ pkgs/wayvnc/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/any1/wayvnc";
   branch = "master";
-  rev = "81192ac74df316e0faf7bc2f985741f701db753d";
-  sha256 = "sha256-Yamky/FX2dgvRkVdMFb30wTwc1ukU72JCla9RfUx9Q8=";
+  rev = "7a32cae4acfcd35e27d06bb728cf6e2b85f7c094";
+  sha256 = "sha256-dhwNq9AFlzKvU6kGhogIK/1LtdiGiiJ4KZuJt3WIj4U=";
 }
