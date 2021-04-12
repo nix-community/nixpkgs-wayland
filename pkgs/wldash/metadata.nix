8e965e34c7679935f9644ae800fe4d70cb0bca16
Cole Mickens <cole.mickens@gmail.com>
2021-04-12 09:33:34 +0000

    pkgs: update rust cargoSha256 hashes

--- pkgs/wldash/metadata.nix
+++ pkgs/wldash/metadata.nix
@@ -3,5 +3,5 @@
   branch = "master";
   rev = "56f6b1668dda6034205bf9f3d5b735f271a8c8b3";
   sha256 = "sha256-kTgXBnzOAxFg35Cn28XUSNmBwSUJ6Ll2E5GgATZh2PI=";
-  cargoSha256 = "sha256-79eFDtOQeVv8b+14cPYdx4pQI6jEXVjWw9T5W+5Vyig=";
+  cargoSha256 = "sha256-p1KDYDHKtzN2e3IZa2q4z1ihzW+LIL96h8zoIZTX4E0=";
 }
