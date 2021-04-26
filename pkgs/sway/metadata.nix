975987ee522e792987501e7cd2f6ccf6e6c99fc0
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-04-26 08:05:07 +0000

    auto-update(495747): sway: 80128d23ba9f0a6a92284b2c6077e304f35e7a76 => 4e6f51525308a8883bf998a360a192edc0822cdd

--- pkgs/sway/metadata.nix
+++ pkgs/sway/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/swaywm/sway";
   branch = "master";
   upattr = "sway-unwrapped";
-  rev = "80128d23ba9f0a6a92284b2c6077e304f35e7a76";
-  sha256 = "sha256-7xtlC8dT99PY8fLZIhAuxeBbO+VyRXfadVMZZ+wFX0c=";
+  rev = "4e6f51525308a8883bf998a360a192edc0822cdd";
+  sha256 = "sha256-dkZJjQAZn/QKJRTSEqrvQglD7cNB+MI+n/udEoblyUs=";
 }
