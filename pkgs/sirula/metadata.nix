54bbb747e5d2f082aca8da7b3a5f081bbd548853
Cole Mickens <cole.mickens@gmail.com>
2021-04-29 05:20:34 +0000

    sirula: react to nixpkgs update

--- pkgs/sirula/metadata.nix
+++ pkgs/sirula/metadata.nix
@@ -3,5 +3,5 @@
   branch = "master";
   rev = "225f2f3c3e252c0d2a53e151f5b0a5e5b1ee3d22";
   sha256 = "sha256-U4J7cQtao7lrghMlW9h1BQN4XBzI63OH4g0FhEn/p40=";
-  cargoSha256 = "sha256-1t3SBx75gkLoXVQvgt7rxbPbDF5Cs7inb05cFfgN/Ew=";
+  cargoSha256 = "sha256-fQD5J+3PPDRoCV2uWQk4I834wVXRBN2n2jNj8xYfU+w=";
 }
