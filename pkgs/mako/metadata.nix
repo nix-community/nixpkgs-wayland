ccb190a57dd0724145f9fa5a155ba22e912ea7b2
Cole Mickens <cole.mickens@gmail.com>
2021-05-03 21:37:15 +0000

    auto-update(manual): mako: e5b5d56aaaf8390c97a17c208f950803538e7294 => 25017fde836d3809c1e214939ccbacdd95cb2443

--- pkgs/mako/metadata.nix
+++ pkgs/mako/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/mako";
   branch = "master";
-  rev = "e5b5d56aaaf8390c97a17c208f950803538e7294";
-  sha256 = "sha256-CNTdPTJnve7c8fQbRrxS5BBHCjyZY0ZBQsyUslsUhcY=";
+  rev = "25017fde836d3809c1e214939ccbacdd95cb2443";
+  sha256 = "sha256-p4eSxNW2GAgp7oXRBHOnbbphYRBGRuydP4gazQxkWOk=";
 }
