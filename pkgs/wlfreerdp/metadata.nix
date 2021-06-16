d42484a44546379ca79d77215937a07718ed5b25
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-06-16 09:48:55 +0000

    auto-update(manual): wlfreerdp: 3a75228242401de96eaf66222e77a72a40dc3b40 => 670cf8512ba751054b0e09d5c41e890953fbbb62

--- pkgs/wlfreerdp/metadata.nix
+++ pkgs/wlfreerdp/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/FreeRDP/FreeRDP";
   branch = "master";
-  rev = "3a75228242401de96eaf66222e77a72a40dc3b40";
-  sha256 = "sha256-uLLSSqeeIoWmcP1g/hhrvf4ywMEYmsFV30ExGljiQ2I=";
+  rev = "670cf8512ba751054b0e09d5c41e890953fbbb62";
+  sha256 = "sha256-UPjgwM+0fwC7uDbGFUGBN7xmToOOUDBe9aue36hkKGE=";
 }
