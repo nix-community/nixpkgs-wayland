37548845a187a1ada286014af926ffe0c87c5065
Cole Mickens <cole.mickens@gmail.com>
2021-07-06 12:06:58 -0700

    auto-update(manual): grim: 0e677488ad59c87202e85ab8b75d866c17b447f3 => c7ef385b55e086e0cf8273299b9190ae06c6c6af

--- pkgs/grim/metadata.nix
+++ pkgs/grim/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/grim";
   branch = "master";
-  rev = "0e677488ad59c87202e85ab8b75d866c17b447f3";
-  sha256 = "sha256-nYbHuooJasTfSv6fjJvFUlCi8qMbHxbg81B6MneLOu4=";
+  rev = "c7ef385b55e086e0cf8273299b9190ae06c6c6af";
+  sha256 = "sha256-fZXtdzG1tQfhgYff78UFQ6JlHjzLHIVMBEqsCyEfzDc=";
 }
