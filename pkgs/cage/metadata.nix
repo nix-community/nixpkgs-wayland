c2977024c0ac0a7439d64f258e5230caf389cd03
Cole Mickens <cole.mickens@gmail.com>
2021-07-06 12:06:30 -0700

    auto-update(manual): cage: efaf76e9ab2032ed6eedee7260c0857fac34b5b1 => 646b3e80b45a011676eb4190652b8c506d56e7a4

--- pkgs/cage/metadata.nix
+++ pkgs/cage/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/Hjdskes/cage";
   branch = "master";
-  rev = "efaf76e9ab2032ed6eedee7260c0857fac34b5b1";
-  sha256 = "sha256-MgzBjmLT5hDgVR+XgWcauaMzLSDXkNyYNLYlKbN/opI=";
+  rev = "646b3e80b45a011676eb4190652b8c506d56e7a4";
+  sha256 = "sha256-JnPt8Q7SADuTPYu6aOYbfIDmG3U63NRHRcRDG/szqW4=";
 }
