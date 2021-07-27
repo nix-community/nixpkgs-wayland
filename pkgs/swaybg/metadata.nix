b97c1aae55824f35f3b0f90f2a5b727e6bea8164
Cole Mickens <cole.mickens@gmail.com>
2021-07-26 17:53:09 -0700

    auto-update(manual): swaybg: ccbd046b3347734d98d49ce3272f54187148b5a7 => 96cf26d605d491d0cbb9e931c0bf8dcd28258e52

--- pkgs/swaybg/metadata.nix
+++ pkgs/swaybg/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/swaywm/swaybg";
   branch = "master";
-  rev = "ccbd046b3347734d98d49ce3272f54187148b5a7";
-  sha256 = "sha256-5YZ5umuTpKpUAXU60p/zLlPM2MYmwUz5QQSjsNPouAc=";
+  rev = "96cf26d605d491d0cbb9e931c0bf8dcd28258e52";
+  sha256 = "sha256-5hTgK0aTkkoEXpMeViKHWFnxyZF9Mdo1DcxwzRNGoJw=";
 }
