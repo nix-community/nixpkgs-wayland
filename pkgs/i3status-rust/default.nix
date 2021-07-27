0b702642a0af92b6980222415e8da06e71124b54
Cole Mickens <cole.mickens@gmail.com>
2021-07-26 17:50:20 -0700

    i3status-rust: fix f48e4c9a7f83b22a8754a6554ec6...

--- pkgs/i3status-rust/default.nix
+++ pkgs/i3status-rust/default.nix
@@ -4,12 +4,18 @@ let
   metadata = import ./metadata.nix;
   ignore = [ "i3status-rust" ];
   args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
-in
-(i3status-rust.override args).overrideAttrs(old: {
-  version = metadata.rev;
-  src = fetchFromGitHub {
+  newsrc = fetchFromGitHub {
     owner = "greshake";
     repo = "i3status-rust";
     inherit (metadata) rev sha256;
   };
+in
+(i3status-rust.override args).overrideAttrs(old: {
+  version = metadata.rev;
+  src = newsrc;
+
+  cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+    src = newsrc;
+    outputHash = metadata.cargoSha256;
+  });
 })
--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/greshake/i3status-rust";
   branch = "master";
-  rev = "00e808444c752a93914431c5d2dced00e7c5f0c3";
-  sha256 = "sha256-9PXvQrh0gmn/G+b7sbQffQkPnUKu1eVrvUoJlRBsOEM=";
-  cargoSha256 = "sha256-X8q/+dO/eIEyg0WJHI9adVDmsH0KV4KkmjcEgQcrE/E=";
+  rev = "7c89cd8d26246c5437b70b77df6f7471a75321a0";
+  sha256 = "sha256-D4wtuIGxtg0f0mM8MfS8fp+TpifMeIrEEWVY7gr3pfw=";
+  cargoSha256 = "0ksfnmaalqn1cv3xilafbhc2nk8jwi0fry74sav5rm15cd3w72pa";
 }
