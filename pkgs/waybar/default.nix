aa8e21b1f3ac1bc921565625b4d93c4d90d2f4db
Cole Mickens <cole.mickens@gmail.com>
2021-07-27 16:53:44 -0700

    waybar: add libevdev, libxkbcommon

--- pkgs/waybar/default.nix
+++ pkgs/waybar/default.nix
@@ -1,8 +1,9 @@
-args_@{ stdenv, lib, fetchFromGitHub, waybar, ...}:
+args_@{ stdenv, lib, fetchFromGitHub, waybar
+, libevdev, libxkbcommon, ...}:
 
 let
   metadata = import ./metadata.nix;
-  ignore = [ "waybar" ];
+  ignore = [ "waybar" "libevdev" "libxkbcommon" ];
   args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
 in
 (waybar.override args).overrideAttrs(old: {
@@ -13,6 +14,7 @@ in
     repo = "Waybar";
     inherit (metadata) rev sha256;
   };
+  buildInputs = old.buildInputs ++ [ libevdev libxkbcommon ];
   prePatch = ''
     sed -i "s|version: '0.9.7'|version: '-git-${metadata.rev}'|g" meson.build
   '';
