e1f12e8fba61aa4997e9b3aefe6570971e5daf71
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-08-11 14:44:45 +0000

    auto-update(manual): wlroots-eglstreams: 6649c4d7b306e5838d6898a4b9669c2e4eb2e0f4 => 9e3a62e1fe8030f4dcfe00d44bab3faaa281bf29

--- pkgs/wlroots-eglstreams/metadata.nix
+++ pkgs/wlroots-eglstreams/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/danvd/wlroots-eglstreams";
   branch = "master";
-  rev = "6649c4d7b306e5838d6898a4b9669c2e4eb2e0f4";
-  sha256 = "sha256-fsC1ERNuTPMpmVrmjLWhcrC+PKAQ9XvEZi/zH8ZJAEE=";
+  rev = "9e3a62e1fe8030f4dcfe00d44bab3faaa281bf29";
+  sha256 = "sha256-/HvmscO2lHZ+L0i+P5Ag4g1ravKYLLQReMCl7FOixPY=";
 }
