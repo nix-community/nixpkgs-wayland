c7fee4771958503ef3ce57135d7cdb36b33059c3
Cole Mickens <cole.mickens@gmail.com>
2021-09-13 14:19:15 -0700

    auto-update(manual): wl-gammactl: 611846ce8f5f6e7fe1afb100416cceb8af54afb2 => e2385950d97a3baf1b6e2f064dd419ccec179586

--- pkgs/wl-gammactl/metadata.nix
+++ pkgs/wl-gammactl/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/mischw/wl-gammactl";
   branch = "master";
-  rev = "611846ce8f5f6e7fe1afb100416cceb8af54afb2";
+  rev = "e2385950d97a3baf1b6e2f064dd419ccec179586";
   sha256 = "sha256-4ve38pMGKDDC4oZZERyPdxVJX4RgHwYqX6IUQQ2GpGg=";
 }
