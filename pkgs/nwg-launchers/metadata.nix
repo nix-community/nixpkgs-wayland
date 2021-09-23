c4032a6e7fcdeff38b0813ac738a0689ce672b13
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-09-23 05:35:40 +0000

    auto-update(manual): nwg-launchers: 51c990869e1aab99f59fb3b280bfaebbc8194bdd => 33e42b885643dc6d52448a0e596d33346e324cb2

--- pkgs/nwg-launchers/metadata.nix
+++ pkgs/nwg-launchers/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/nwg-piotr/nwg-launchers";
   branch = "master";
-  rev = "51c990869e1aab99f59fb3b280bfaebbc8194bdd";
-  sha256 = "sha256-fZ3llNNcitZ7UIThxGVpYLSURqV1tCk6c60xQRSYcx8=";
+  rev = "33e42b885643dc6d52448a0e596d33346e324cb2";
+  sha256 = "sha256-eCPfRYEkr/ZfiMT0fM0SJCQmWhRoJqZEahuaD48aZF8=";
 }
