a84dc694e58ec69ab4dd48cd868c233490359b29
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-09-28 15:24:38 +0000

    auto-update(manual): dunst: fc331998c3c9394481d59d576aa54c4040a537a5 => e973cf8b1c86abf687ab3bd7ea4a7285dafd3018

--- pkgs/dunst/metadata.nix
+++ pkgs/dunst/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/dunst-project/dunst";
   branch = "master";
-  rev = "fc331998c3c9394481d59d576aa54c4040a537a5";
-  sha256 = "sha256-HGZQTrqTjPnPPR6UnRkQPtiC9PIN0uQZcgALRz8Utg0=";
+  rev = "e973cf8b1c86abf687ab3bd7ea4a7285dafd3018";
+  sha256 = "sha256-qbmg+ZNsRt3q+dr8gZ17Oy+9M5/3HZqruksajRLcFsk=";
 }
