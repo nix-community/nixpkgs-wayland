a07632a8a422c0e0cbba6a85e2d535bfe9e56d31
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-10-07 11:00:42 +0000

    auto-update(manual): grim: af000f9900615c3583d3e752695454bba6e992e1 => e658b13d169e59c44effb49b0cf65c6234576c70

--- pkgs/grim/metadata.nix
+++ pkgs/grim/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/grim";
   branch = "master";
-  rev = "af000f9900615c3583d3e752695454bba6e992e1";
-  sha256 = "sha256-wUdvEe7eJzRrSanapM8X28n5MPOEdY6am9cP5uimshk=";
+  rev = "e658b13d169e59c44effb49b0cf65c6234576c70";
+  sha256 = "sha256-MgEMW3d7TmgHidjy9SuP1g50kZ27RZArM9KzqaMndtg=";
 }
