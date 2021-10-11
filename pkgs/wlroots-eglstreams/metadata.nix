87a48e05247d6c6b0ad16268b797c04db804b68b
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-10-11 09:13:19 +0000

    auto-update(manual): wlroots-eglstreams: 58f73211302263fa87b1063421d58b3bd595a2d3 => 8073eb71c234d3479098145a7053c2b3467f26f2

--- pkgs/wlroots-eglstreams/metadata.nix
+++ pkgs/wlroots-eglstreams/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/colemickens/wlroots-eglstreams";
   branch = "master";
-  rev = "58f73211302263fa87b1063421d58b3bd595a2d3";
-  sha256 = "sha256-j/w8COKo70Qod7gTW+yd/Qq23fiwP4SYqSQYDyjynj4=";
+  rev = "8073eb71c234d3479098145a7053c2b3467f26f2";
+  sha256 = "sha256-farXqIIRrZmZurAOwtl+x4AuGo02QuGavvK2QKZm6/Y=";
 }
