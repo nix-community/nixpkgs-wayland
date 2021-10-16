fecd3ab90ff170778a879bf1f602e72f2ed277ed
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-10-16 13:32:20 +0000

    auto-update(manual): aml: a1baa973e87b4c81681cdc22aa7cbbec35244d2a => 08b6b18d1935fcc2567368c16d8a31d1d2b17aa8

--- pkgs/aml/metadata.nix
+++ pkgs/aml/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/any1/aml";
   branch = "master";
-  rev = "a1baa973e87b4c81681cdc22aa7cbbec35244d2a";
-  sha256 = "sha256-KdIma517jVhaVQcva5J+kjlQnG12MCkSL6CXkbNPUtE=";
+  rev = "08b6b18d1935fcc2567368c16d8a31d1d2b17aa8";
+  sha256 = "sha256-HNuf/LN1FryUEKFLM0mB4/YkPg0zzTE9gSyQlocNIdU=";
 }
