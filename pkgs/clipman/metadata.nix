ab8372a55a8aebd5f97255c73af6333431aab4d4
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-11-06 09:08:52 +0000

    auto-update(manual): clipman: 5c6734de74a9927d244247c5e8e7086be19bcbe7 => 96cde32f61faaf8535d3734d29f1ef8e2f66672a

--- pkgs/clipman/metadata.nix
+++ pkgs/clipman/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/yory8/clipman";
   branch = "master";
-  rev = "5c6734de74a9927d244247c5e8e7086be19bcbe7";
-  sha256 = "sha256-wwqNOv4Koiodn/u0OnBJ6O2pT7lr0LWk1dB/5KEOABU=";
+  rev = "96cde32f61faaf8535d3734d29f1ef8e2f66672a";
+  sha256 = "sha256-BJXnj/xnooOedasU4tbZIU2+qKFgzbY/EbSOy7kylIA=";
   vendorSha256 = "sha256-Z/sVCJz/igPDdeczC6pemLub6X6z4ZGlBwBmRsEnXKI=";
 }
