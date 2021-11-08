d745df6a8bb6a46425310da1d52a8ececfceac23
Cole Mickens <cole.mickens@gmail.com>
2021-11-08 15:53:32 -0800

    i3status-rust: fix cargoSha256

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -3,5 +3,5 @@
   branch = "master";
   rev = "b431f02cb9362639e6e2ec93086a628e91666adc";
   sha256 = "sha256-mW1rNDK24pAk6JTfSl/HBFbMYm4Taj7nybw4Y148vsY=";
-  cargoSha256 = "sha256-y4NE6fiNvrDStIMVZA2UL1qLfsbQJaeW51BH0K4bNZc=";
+  cargoSha256 = "sha256-6V7UHttXYL47IdyouzhGV/PpjhT9Nwl9nv3M0p+qvIY=";
 }
