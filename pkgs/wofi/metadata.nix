67bd0f3d1509f33b1db49967f2f1eae5afa5f870
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-11-11 06:52:38 +0000

    auto-update(manual): wofi: bae1c99a465f => 188e5c56df52

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wofi";
   branch = "default";
-  rev = "bae1c99a465f";
-  sha256 = "sha256-JIUiZ6rr0aBEFaTHvoL/BJkiCRChmT+PohGOthVbuZw=";
+  rev = "188e5c56df52";
+  sha256 = "sha256-BKMMS6KNh+GB9wK0FIvYjW3OCeWVTtMt8Pv3Zks2lfA=";
 }
