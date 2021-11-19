3e3c9221104b1f73debd299c82f80d41dc6e6bf8
Cole Mickens <cole.mickens@gmail.com>
2021-11-18 18:00:57 -0800

    wlroots-eglstreams: remove

--- pkgs/wlroots-eglstreams/metadata.nix
+++
@@ -1,6 +0,0 @@
-{
-  repo_git = "https://github.com/colemickens/wlroots-eglstreams";
-  branch = "master";
-  rev = "fe5751dec79489e018c2970c2bad219fcec9823b";
-  sha256 = "sha256-Ei+ZuF2EbMX6EVPKeHGOPPgg4WSuCW0+RZZs8Bcmj5o=";
-}
