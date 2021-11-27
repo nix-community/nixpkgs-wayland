69365cb144c1e28802a949a5bcbcdd8106d36c76
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-11-27 08:35:26 +0000

    auto-update(manual): slurp: fa0d21f7594f276aff6fe5d77bc4ef59084358c8 => afe7f0e392c749df017dbcdd39e393102cfbe408

--- pkgs/slurp/metadata.nix
+++ pkgs/slurp/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/slurp";
   branch = "master";
-  rev = "fa0d21f7594f276aff6fe5d77bc4ef59084358c8";
-  sha256 = "sha256-9t41eoZMgG1XWNfMqQiV8TR8SdWpYsJvN9n5KIsnh0I=";
+  rev = "afe7f0e392c749df017dbcdd39e393102cfbe408";
+  sha256 = "sha256-pVabTg3Lqtz4pPcpVtgegMZlqTvWNEWu5aZKjb5/dIo=";
 }
