95941191fbb28fe10d5923eb58d6f3e0a45cc9c6
Cole Mickens <cole.mickens@gmail.com>
2021-11-27 03:24:03 +0000

    auto-update(manual): wlroots: 83d78f9fd415a1110e888b3c5f9cc9fb79627217 => 254ab890e787ccee1e33fb420781c9696761ce3c

--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://gitlab.freedesktop.org/wlroots/wlroots";
   branch = "master";
-  rev = "83d78f9fd415a1110e888b3c5f9cc9fb79627217";
-  sha256 = "sha256-oKqWH6LP0ZH+Ro6hpFF2q795aG4jt4uwb2/29qYr3II=";
+  rev = "254ab890e787ccee1e33fb420781c9696761ce3c";
+  sha256 = "sha256-xU9tXZFDZfhU39QxDcc6KtPtLcYniR6affyakFOaCVs=";
 }
