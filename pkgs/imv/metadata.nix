cc92e66708e833e8c486483b0824d6a48b7ff9d0
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-11-30 18:12:57 +0000

    auto-update(manual): imv: f58e81288b2138d1b532cf11fb95bbcbac362a4e => 1bb925e1d5d30a0900d434150f0e84c81d63ae56

--- pkgs/imv/metadata.nix
+++ pkgs/imv/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/eXeC64/imv";
   branch = "master";
-  rev = "f58e81288b2138d1b532cf11fb95bbcbac362a4e";
-  sha256 = "sha256-LC3piH9f+OD743fGmmCdaudR+TGC4b+wArjB4giUKTY=";
+  rev = "1bb925e1d5d30a0900d434150f0e84c81d63ae56";
+  sha256 = "sha256-sPd0pilF0xDmvA2RPTeNuChgdtQl0EnfjIPnZkJuGp4=";
 }
