880a7d2a9c458c61a335786a58a4a145423ce451
Cole Mickens <cole.mickens@gmail.com>
2021-12-13 11:10:38 -0800

    auto-update(manual): wofi: 188e5c56df52 => 2cd009548707

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wofi";
   branch = "default";
-  rev = "188e5c56df52";
-  sha256 = "sha256-BKMMS6KNh+GB9wK0FIvYjW3OCeWVTtMt8Pv3Zks2lfA=";
+  rev = "2cd009548707";
+  sha256 = "sha256-nUWiKTAXDbfNXWhbqlC1WApZ34ognkAdJHXujSP9ML8=";
 }
