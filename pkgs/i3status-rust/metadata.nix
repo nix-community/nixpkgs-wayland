afce0b198800e9165c7ea9cd6cf56938148af08e
Cole Mickens <cole.mickens@gmail.com>
2021-12-13 22:32:14 -0800

    i3status-rust: metadata: refresh for upstream rust

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -3,5 +3,5 @@
   branch = "master";
   rev = "f3cda7be0c4573e5e6c0dcbb00b22da4df8fa07b";
   sha256 = "sha256-E3BSN9RR2+AStx1UjHtTCanedlG4CTiRsnrrIbPHXK4=";
-  cargoSha256 = "sha256-Ta+sI0hJ6QT9dMVkfKrbPoFh3qmSuLY3/votE2sqGJA=";
+  cargoSha256 = "sha256-NJCbgR1ahs2V+J44nB+ZcTTZmSz2/SAzGKR04bJ2jwI=";
 }
