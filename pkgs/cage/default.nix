d59e8834874316337b9e10491be62476910b2db7
Artturin <Artturin@artturin.com>
2022-01-06 01:21:01 +0200

    cage: fix build and switch to a override

--- pkgs/cage/default.nix
+++ pkgs/cage/default.nix
@@ -1,38 +1,16 @@
-{ stdenv, lib, fetchFromGitHub, fetchpatch
-, pkg-config, meson, ninja, scdoc
-, wayland, wayland-protocols
-, wlroots, pixman, libxkbcommon, udev, libGL, libX11
-}:
+args_@{ lib, fetchFromGitHub, cage, ...}:
 
 let
   metadata = import ./metadata.nix;
+  ignore = [ "cage" ];
+  args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
 in
-stdenv.mkDerivation rec {
-  name = "cage-${version}";
-  version = metadata.rev;
-
+(cage.override args).overrideAttrs(old: {
+  pname = "cage";
+  version = "${metadata.rev}";
   src = fetchFromGitHub {
     owner = "Hjdskes";
     repo = "cage";
-    rev = metadata.rev;
-    sha256 = metadata.sha256;
+    inherit (metadata) rev sha256;
   };
-
-  nativeBuildInputs = [ pkg-config meson ninja ];
-  buildInputs = [
-    scdoc
-    wayland wayland-protocols
-    wlroots pixman libxkbcommon udev libGL libX11
-  ];
-  mesonFlags = [ "-Dxwayland=true" ];
-
-  enableParallelBuilding = true;
-
-  meta = with lib; {
-    description = "A Wayland kiosk";
-    homepage    = "https://www.hjdskes.nl/projects/cage/";
-    license     = licenses.mit;
-    platforms   = platforms.linux;
-    maintainers = with maintainers; [ colemickens ];
-  };
-}
+})
