7dd54b5f3113a69e315472ed7540abc1013498d3
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-01-09 03:01:55 +0000

    auto-update(manual): sway-unwrapped: 1bf1d84b7535c3c132240ed7b18414dc6cfe7e8a => e2b4c573d6506250c77f01512bc07c72996cd363

--- pkgs/sway-unwrapped/metadata.nix
+++ pkgs/sway-unwrapped/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/swaywm/sway";
   branch = "master";
   upattr = "sway-unwrapped";
-  rev = "1bf1d84b7535c3c132240ed7b18414dc6cfe7e8a";
-  sha256 = "sha256-h1jYjiyfuQYMQ2KyGP7nv3pZ8GHFGRbzYEAkvl0NEx8=";
+  rev = "e2b4c573d6506250c77f01512bc07c72996cd363";
+  sha256 = "sha256-IgdjDFddZWRcejzQrxvVb9IKLlitC8Fhg2qceBKOnno=";
 }
