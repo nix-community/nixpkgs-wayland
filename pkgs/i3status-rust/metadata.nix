3164fb29394475b38bf784095145e77d010b02d3
Florian Franzen <Florian.Franzen@gmail.com>
2022-01-14 15:03:29 +0100

    i3status-rust: update cargo vendor hash as well

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://github.com/greshake/i3status-rust";
   branch = "master";
   rev = "87e9c2ef53f8b878813ec228b35b70fb224fc29d";
-  sha256 = "sha256-AaPPxKxKAHlsxEBpIXYz10s+qjpj1TOfsRIh5j3pp7A=";
-  cargoSha256 = "sha256-ztjVmpytvRyPkws3PGX2BehOGa2//9xSS8rEOajepR0=";
+  sha256 = "AaPPxKxKAHlsxEBpIXYz10s+qjpj1TOfsRIh5j3pp7A=";
+  cargoSha256 = "3/v9E63EPQl3LA2l/YHLx+KdiLYRBfFGC+bags1VhlQ=";
 }
