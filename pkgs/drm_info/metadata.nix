1168718fa2cf5e639369aa0388294941fba874ce
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-01-17 01:07:24 +0000

    auto-update(manual): drm_info: d41993496add03014cfcbca742cf1079efd1e0f0 => de9472b8c77c22d2d9cd2937335371fc02104895

--- pkgs/drm_info/metadata.nix
+++ pkgs/drm_info/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/ascent12/drm_info";
   branch = "master";
-  rev = "d41993496add03014cfcbca742cf1079efd1e0f0";
-  sha256 = "sha256-yO8IMU3liCWHBDXRdsifQLAZlhlXSDhBQ8Zr5R9RCH0=";
+  rev = "de9472b8c77c22d2d9cd2937335371fc02104895";
+  sha256 = "sha256-snA7e4c4rryfg+2IGZWE2Hna4TkvMgkVlPtvqR7Usgc=";
 }
