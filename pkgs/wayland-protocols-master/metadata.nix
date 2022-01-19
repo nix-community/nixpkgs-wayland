a7a359959b2996c23d8b59db7e0d78a32803f453
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-01-19 09:43:44 +0000

    auto-update(manual): wayland-protocols-master: c2ceb5f14d0af04c248e03c125d2ed85980cf806 => 344048614ad4a3887e431f63b6f12208edce51f9

--- pkgs/wayland-protocols-master/metadata.nix
+++ pkgs/wayland-protocols-master/metadata.nix
@@ -2,6 +2,6 @@
   repo_git = "https://gitlab.freedesktop.org/wayland/wayland-protocols";
   branch = "main";
   upattr = "wayland-protocols-master";
-  rev = "c2ceb5f14d0af04c248e03c125d2ed85980cf806";
-  sha256 = "sha256-Dg+P1J/DvnN3b5b6+WWvQ+3LMQbvKv24mSifMaz3tEc=";
+  rev = "344048614ad4a3887e431f63b6f12208edce51f9";
+  sha256 = "sha256-OV3kvqd5bG1nY81tx4MpSs2rBJbmf2lwsF58UAkkcSc=";
 }
