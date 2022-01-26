125bd4f1bdea53b0c59882c86d7f520b91fafb80
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-01-26 00:00:53 +0000

    auto-update(manual): nwg-panel: 8bb7922b48e1229505da6dd6711242fe0b17f510 => 114f52d2524a67173bb719568a23d8c772e3fe7f

--- pkgs/nwg-panel/metadata.nix
+++ pkgs/nwg-panel/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/nwg-piotr/nwg-panel";
   branch = "master";
-  rev = "8bb7922b48e1229505da6dd6711242fe0b17f510";
-  sha256 = "sha256-YW1yU2etd5BNjjoty/z/J2UJ23mrwR8w1nl2W1sF2S0=";
+  rev = "114f52d2524a67173bb719568a23d8c772e3fe7f";
+  sha256 = "sha256-O/EuCcwueUT5Bx0QmTyd+rY+SaBMD7Qp0dscv9ftApU=";
 }
