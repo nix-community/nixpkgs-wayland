d883281534e5f2a5876cb7af279f2590e426f027
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2022-02-03 11:11:18 +0100

    i3status-rust: update the cargoSha256

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -3,5 +3,5 @@
   branch = "master";
   rev = "a5c1bc6d95594841e0e5ad5a2ffc97a214df0427";
   sha256 = "sha256-eVu2sEV1RHoXkY4eSQnIB1kj1WBW8azN+0t2MFI557o=";
-  cargoSha256 = "sha256-KIU0YWxlYL5GVOVAQZddg3PvE3qCnlukPmWgAU9sCO0=";
+  cargoSha256 = "sha256-tNwf2ShnzoSrb1R/g0hOGwQMulWYXyVCILU3Jb+Sfpg=";
 }
