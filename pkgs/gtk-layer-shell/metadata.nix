6cd175ed07e9a9f192e3015c05c3faf539071f46
Cole Mickens <cole.mickens@gmail.com>
2022-02-11 10:26:07 -0800

    auto-update(manual): gtk-layer-shell: beb30f2289d2729b4bd87d96e6e93bee2f8909c4 => ca37ef1baa623302dda5fcea200d04e0d9a10578

--- pkgs/gtk-layer-shell/metadata.nix
+++ pkgs/gtk-layer-shell/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/wmww/gtk-layer-shell";
   branch = "master";
-  rev = "beb30f2289d2729b4bd87d96e6e93bee2f8909c4";
-  sha256 = "sha256-0S1WBpxXpWoMOecJQS6FKEXRZdw4E5hrjURPyhkxiMc=";
+  rev = "ca37ef1baa623302dda5fcea200d04e0d9a10578";
+  sha256 = "sha256-Ht1qqzVVl0jfpzHPuLgaVVyFIMEiYHjzjh7UqmrkXbU=";
 }
