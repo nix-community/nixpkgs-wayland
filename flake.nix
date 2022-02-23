b607944f6c5bc850aeb59c9b65fa6e7d1ee8ac99
Cole Mickens <cole.mickens@gmail.com>
2022-02-23 04:19:54 -0800

    flakes-compat update

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     cachix = { url = "github:nixos/nixpkgs/nixos-21.05"; };
+    flake-compat = { url = github:edolstra/flake-compat; flake = false; };
   };
 
   outputs = inputs:
@@ -37,70 +38,12 @@
 
       overlay = final: prev:
         let
-          _wayland-protocols-master = prev.callPackage ./pkgs/wayland-protocols-master {
-            wayland-protocols = prev.wayland-protocols;
-          };
-
-          _fcftNewer = prev.callPackage ./pkgs-temp/fcft {
-            fcft = prev.fcft;
-          };
-
-          _mesonNewer = prev.callPackage ./pkgs-temp/meson {};
-          _wlroots = prev.callPackage ./pkgs/wlroots {
-            meson = _mesonNewer;
-            wayland-protocols = _wayland-protocols-master;
-            wayland = _waylandNewer;
-          };
-          _waylandNewer = prev.wayland.overrideAttrs(old: {
-            version = "1.20.0";
-            src = prev.fetchFromGitLab {
-              domain = "gitlab.freedesktop.org";
-              owner = "wayland";
-              repo = "wayland";
-              rev = "1.20.0";
-              sha256 = "sha256-N+riSb3F3vcyUlNdlSnOUrKdDtBcGn9rUyCAi3nel6E=";
-            };
-            patches = [
-              (prev.writeText "patch.diff" ''
-                  From 378623b0e39b12bb04d3a3a1e08e64b31bd7d99d Mon Sep 17 00:00:00 2001
-                  From: Florian Klink <flokli@flokli.de>
-                  Date: Fri, 27 Nov 2020 10:22:20 +0100
-                  Subject: [PATCH] add placeholder for @nm@
-
-                  ---
-                  egl/meson.build | 2 +-
-                  1 file changed, 1 insertion(+), 1 deletion(-)
-
-                  diff --git a/egl/meson.build b/egl/meson.build
-                  index dee9b1d..e477546 100644
-                  --- a/egl/meson.build
-                  +++ b/egl/meson.build
-                  @@ -11,7 +11,7 @@ wayland_egl = library(
-
-                   executable('wayland-egl-abi-check', 'wayland-egl-abi-check.c')
-
-                  -nm_path = find_program('nm').path()
-                  +nm_path = find_program('${prev.stdenv.cc.targetPrefix}nm').path()
-
-                   test(
-                   	'wayland-egl symbols check',
-                  --
-                  2.29.2
-                '')
-            ];
-          });
           waylandPkgs = rec {
             # wlroots-related
             cage             = prev.callPackage ./pkgs/cage {
-              wayland = _waylandNewer;
-              meson = _mesonNewer;
               wlroots = prev.wlroots;
             };
             drm_info         = prev.callPackage ./pkgs/drm_info {};
-            foot             = prev.callPackage ./pkgs/foot {
-              inherit foot;
-              fcft = _fcftNewer;
-            };
             gebaar-libinput  = prev.callPackage ./pkgs/gebaar-libinput {};
             glpaper          = prev.callPackage ./pkgs/glpaper {};
             grim             = prev.callPackage ./pkgs/grim {};
@@ -109,23 +52,14 @@
               imv = prev.imv;
             };
             lavalauncher     = prev.callPackage ./pkgs/lavalauncher {};
-            mako             = prev.callPackage ./pkgs/mako {
-              meson = _mesonNewer;
-            };
+            mako             = prev.callPackage ./pkgs/mako {};
             oguri            = prev.callPackage ./pkgs/oguri {};
             rootbar          = prev.callPackage ./pkgs/rootbar {};
-            sirula           = prev.callPackage ./pkgs/sirula {};
             slurp            = prev.callPackage ./pkgs/slurp {};
-            sway-unwrapped   = prev.callPackage ./pkgs/sway-unwrapped {
-              meson = _mesonNewer;
-              wayland = _waylandNewer;
-              wayland-protocols =_wayland-protocols-master;
-            };
+            sway-unwrapped   = prev.callPackage ./pkgs/sway-unwrapped {};
             swaybg           = prev.callPackage ./pkgs/swaybg {};
             swayidle         = prev.callPackage ./pkgs/swayidle {};
-            swaylock         = prev.callPackage ./pkgs/swaylock {
-              wayland-protocols =_wayland-protocols-master;
-            };
+            swaylock         = prev.callPackage ./pkgs/swaylock {};
             waybar           = prev.callPackage ./pkgs/waybar {
               waybar = prev.waybar;
             };
@@ -145,16 +79,14 @@
             wlogout          = prev.callPackage ./pkgs/wlogout {
               wlogout = prev.wlogout;
             };
-            wlroots          = _wlroots;
+            wlroots          = prev.callPackage ./pkgs/wlroots {};
             wlr-randr        = prev.callPackage ./pkgs/wlr-randr {};
             wlsunset         = prev.callPackage ./pkgs/wlsunset {};
             wofi             = prev.callPackage ./pkgs/wofi {};
             wtype            = prev.callPackage ./pkgs/wtype {};
-            xdg-desktop-portal-wlr = prev.callPackage ./pkgs/xdg-desktop-portal-wlr {};
-
             wshowkeys        = prev.callPackage ./pkgs/wshowkeys {};
 
-            wayland-protocols-master = _wayland-protocols-master;
+            xdg-desktop-portal-wlr = prev.callPackage ./pkgs/xdg-desktop-portal-wlr {};
 
             # misc
             aml = prev.callPackage ./pkgs/aml {};
@@ -162,30 +94,25 @@
             dunst = prev.callPackage ./pkgs/dunst {
               dunst = prev.dunst;
             };
+            foot             = prev.callPackage ./pkgs/foot {
+              inherit foot;
+            };
             gtk-layer-shell = prev.callPackage ./pkgs/gtk-layer-shell {};
             i3status-rust    = prev.callPackage ./pkgs/i3status-rust {
               i3status-rust = prev.i3status-rust;
             };
             neatvnc = prev.callPackage ./pkgs/neatvnc {};
-
             nwg-launchers = prev.callPackage ./pkgs/nwg-launchers {};
             nwg-panel = prev.callPackage ./pkgs/nwg-panel {
               nwg-panel = prev.nwg-panel;
             };
-
-            obs-studio = prev.libsForQt5.callPackage ./pkgs/obs-studio {
-              ffmpeg = prev.ffmpeg_4;
-              vlc = prev.vlc;
-            };
+            sirula           = prev.callPackage ./pkgs/sirula {};
             wlfreerdp = prev.callPackage ./pkgs/wlfreerdp {
               inherit (prev) libpulseaudio;
               inherit (prev.gst_all_1) gstreamer gst-plugins-base gst-plugins-good;
             };
             # wayfire stuff
-            wayfire          = prev.callPackage ./pkgs/wayfire {
-              meson = _mesonNewer;
-              wayland-protocols = _wayland-protocols-master;
-            };
+            wayfire          = prev.callPackage ./pkgs/wayfire {};
 
             libvncserver_master = prev.callPackage ./pkgs/libvncserver_master {
               libvncserver = prev.libvncserver;
--- packages.nix
+++ packages.nix
@@ -1,9 +1,13 @@
 let
-  flake = (import (fetchTarball {
-    url="https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
-    sha256="0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
-  }) {
-    src = ./.;
-  });
+  flake = (import
+    (
+      let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
+    )
+    { src = ./.; }
+  );
 in
   flake.defaultNix.packages."${builtins.currentSystem}"
