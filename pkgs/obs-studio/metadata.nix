a673ef6b7bf3ab652a21fe313d8444c9c556fe5e
Cole Mickens <cole.mickens@gmail.com>
2022-02-23 14:23:40 -0800

    obs-studio: drop

--- pkgs/obs-studio/metadata.nix
+++
@@ -1,7 +0,0 @@
-{
-  repo_git = "https://github.com/obsproject/obs-studio";
-  branch = "master";
-  rev = "a79b6d6f79ebda82fdcf00a0dab270c0b6bed2ef";
-  sha256 = "sha256-wEwnt1BAh8eSkFiuOlzVy4V8HD5EnvQnmtap5kPhdr0=";
-  skip = true;
-}
