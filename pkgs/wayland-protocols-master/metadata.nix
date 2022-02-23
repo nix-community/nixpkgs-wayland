4a6134b33ca9e926dfdc865750ceca991105ac50
Cole Mickens <cole.mickens@gmail.com>
2022-02-23 14:24:14 -0800

    wayland-protocols-master: drop

--- pkgs/wayland-protocols-master/metadata.nix
+++
@@ -1,7 +0,0 @@
-{
-  repo_git = "https://gitlab.freedesktop.org/wayland/wayland-protocols";
-  branch = "main";
-  upattr = "wayland-protocols-master";
-  rev = "7e2d9e381e32c99a1d9f054ab460398c9141f8e5";
-  sha256 = "sha256-A/T7H2G14msvqlS6A4vOQi5NdSVJwQscpisDb5F3uWU=";
-}
