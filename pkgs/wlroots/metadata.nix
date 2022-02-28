8daa341f60c69a7c6752d9f8024a103b424c1aa9
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-02-28 22:10:52 +0000

    auto-update(manual): wlroots: 17a289e36e920ebf18af6e9215388ee6c20029c7 => c84744321edf3e5f083feb01addd82eae0969e31

--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://gitlab.freedesktop.org/wlroots/wlroots";
   branch = "master";
-  rev = "17a289e36e920ebf18af6e9215388ee6c20029c7";
-  sha256 = "sha256-F82IFjhu93SSR6nnE/hRwMtIfrX6JsMuAZzqkhWXG14=";
+  rev = "c84744321edf3e5f083feb01addd82eae0969e31";
+  sha256 = "sha256-o3czsM5rrDYRhqq/9clv/uligmm67FHdtj6PZBibIV0=";
 }
