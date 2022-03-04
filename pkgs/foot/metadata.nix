dbec966ee5c15d6692e76410f803d8085e8cf4e3
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-03-04 19:32:15 +0000

    auto-update(manual): foot: cc8e86fd2d7c2e2527e0c77237ff815888008ac4 => 6e536e7ed8f1de2615fa323123ddb2b06a45a179

--- pkgs/foot/metadata.nix
+++ pkgs/foot/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://codeberg.org/dnkl/foot";
   branch = "master";
-  rev = "cc8e86fd2d7c2e2527e0c77237ff815888008ac4";
-  sha256 = "sha256-1dl7KbQlZgV2oAcMFO8Ca2fxyq2ag0hDfmNBYodBJBc=";
+  rev = "6e536e7ed8f1de2615fa323123ddb2b06a45a179";
+  sha256 = "sha256-chac+rKB8lioFpYxVEDCAotL4ym1DA+wgstZg6Bh3qw=";
 }
