599ef38efc6b707086a6998fd4d292f34c774f25
Cole Mickens <cole.mickens@gmail.com>
2022-03-22 00:31:24 -0700

    i3status-rs: fixup again

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -3,5 +3,5 @@
   branch = "master";
   rev = "06272a149411090cb1f0281edcad460beadd6817";
   sha256 = "sha256-DSYcME3HSBLM/eQziur8EhWRf6TJ9KfnqE/sv9bZmCY=";
-  cargoSha256 = "sha256-DkTpob4x7e5oqG/q6T7Vx3g+Xq3tfsLcEHa8RLt4Dzw=";
+  cargoSha256 = "sha256-2rJ9ZZHK1acJu6r6EjuoolLJZMdBsbKgN1DNvGM7deA=";
 }
