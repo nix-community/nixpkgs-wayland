70b4e3423d5d29a2aaa58b9a22432c3edb7b2bae
Cole Mickens <cole.mickens@gmail.com>
2022-04-10 12:15:48 -0700

    wlvncc: add ffmpeg

--- pkgs/wlvncc/default.nix
+++ pkgs/wlvncc/default.nix
@@ -6,6 +6,7 @@
 , pixman, libuv, libglvnd
 , neatvnc, libX11, libdrm
 , aml, mesa
+, ffmpeg
 }:
 
 let
@@ -30,6 +31,7 @@ stdenv.mkDerivation rec {
     pixman libuv libglvnd
     neatvnc libX11 libdrm
     aml mesa
+    ffmpeg
   ];
 
   enableParallelBuilding = true;
