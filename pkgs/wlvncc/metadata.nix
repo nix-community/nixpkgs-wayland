93c79ae0ec9b1053910b9495a1d652383d2be265
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-04-10 19:19:50 +0000

    auto-update(manual): wlvncc: ec51388fec0d78b84634035b6de0789657f8da2e => 5b4694e1fd74eff16d453fb24e58818b72753a7f

--- pkgs/wlvncc/metadata.nix
+++ pkgs/wlvncc/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/any1/wlvncc";
   branch = "master";
-  rev = "ec51388fec0d78b84634035b6de0789657f8da2e";
-  sha256 = "sha256-A1javnUYbBy27Zj0nnDcWyR2dkqvewF7lnc6Uqq6SZE=";
+  rev = "5b4694e1fd74eff16d453fb24e58818b72753a7f";
+  sha256 = "sha256-nRSawn6PGhaRJRJ3moV+jYS+MlO4ezum+B+XiuPemjw=";
 }
