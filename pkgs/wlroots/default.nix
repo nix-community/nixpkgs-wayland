dc0456b0d4569432d0bf3e77ea87b07dadaa71e8
Artturin <Artturin@artturin.com>
2022-05-01 00:21:53 +0300

    wlroots: convert to override

--- pkgs/wlroots/default.nix
+++ pkgs/wlroots/default.nix
@@ -1,71 +1,13 @@
-{ stdenv, lib, fetchFromGitLab, fetchpatch, meson, ninja, pkg-config
-, wayland, wayland-protocols, libinput, libxkbcommon, pixman
-, xcbutilwm, libX11, libcap, xcbutilimage, xcbutilerrors, mesa
-, libglvnd
-, libpng, ffmpeg_4
-, seatd
-, libuuid
-, xorg # ?
-, libdrm
-, enableXWayland ? true, xwayland ? null
-, vulkan-headers, vulkan-loader, glslang
-}:
+args_@{ lib, fetchFromGitLab, wlroots, ... }:
 
 let
   metadata = import ./metadata.nix;
-  pname = "wlroots";
-  version = metadata.rev;
-in stdenv.mkDerivation rec {
-  inherit pname version;
-
+  ignore = [ "wlroots" ];
+  args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
+in
+(wlroots.override args).overrideAttrs(old: {
+  version = "${metadata.rev}";
   src = fetchFromGitLab {
-    domain = "gitlab.freedesktop.org";
-    owner = "wlroots";
-    repo = "wlroots";
-    rev = metadata.rev;
-    sha256 = metadata.sha256;
+    inherit (metadata) domain owner repo rev sha256;
   };
-
-  # $out for the library, $bin for rootston, and $examples for the example
-  # programs (in examples) AND rootston
-  outputs = [ "out" "examples" ];
-
-  nativeBuildInputs = [ meson ninja pkg-config xwayland ];
-
-  buildInputs = [
-    wayland wayland-protocols libinput libxkbcommon pixman
-    xcbutilwm libX11 libcap xcbutilimage xcbutilerrors mesa
-    libpng ffmpeg_4
-    libglvnd
-    seatd
-    libuuid
-    xorg.xcbutilrenderutil
-    vulkan-headers vulkan-loader glslang
-    libdrm
-  ] ++ lib.optional enableXWayland xwayland;
-
-
-  mesonFlags =
-    lib.optional (!enableXWayland) "-Dxwayland=disabled"
-  ;
-
-  postInstall = ''
-    # Install ALL example programs to $examples:
-    # screencopy dmabuf-capture input-inhibitor layer-shell idle-inhibit idle
-    # screenshot output-layout multi-pointer rotation tablet touch pointer
-    # simple
-    mkdir -p $examples/bin
-    cd ./examples
-    for binary in $(find . -executable -type f -printf '%P\n' | grep -vE '\.so'); do
-      cp "$binary" "$examples/bin/wlroots-$binary"
-    done
-  '';
-
-  meta = with lib; {
-    description = "A modular Wayland compositor library";
-    inherit (src.meta) homepage;
-    license     = licenses.mit;
-    platforms   = platforms.linux;
-    maintainers = with maintainers; [ primeos ];
-  };
-}
+})
