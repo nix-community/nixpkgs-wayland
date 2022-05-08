d8cc118c8aed2c05e34034c4feb897b318de59fa
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-05-08 16:29:07 +0000

    auto-update(manual): wlsunset: 09c5e9a8dd147775d03f8a0e58d4e306c7902f67 => cee422b440b63a1ee3913872674e95486c37f84e

--- pkgs/wlsunset/metadata.nix
+++ pkgs/wlsunset/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~kennylevinsen/wlsunset";
   branch = "master";
-  rev = "09c5e9a8dd147775d03f8a0e58d4e306c7902f67";
-  sha256 = "sha256-tU4r0FVZgn7pKwK7iWSVqF2gGvpfVvoZ9rZEfk+MW6M=";
+  rev = "cee422b440b63a1ee3913872674e95486c37f84e";
+  sha256 = "sha256-JxcZQ67eCvp9edE2EulIF8dAEGbyNyKaca9njE3AIBg=";
 }
