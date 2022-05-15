272c213dce193f315cf9282a9921b1164d959fab
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-05-15 05:19:28 +0000

    auto-update(manual): wofi: af279df8f63a => f11d83bdf12e

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wofi";
   branch = "default";
-  rev = "af279df8f63a";
-  sha256 = "sha256-dp9zfxkfMKAV0v1/oFqutyzLEcKkB96L7SAB8a2FIO0=";
+  rev = "f11d83bdf12e";
+  sha256 = "sha256-Xh+pRRmTR/ZMwAFqnY8G+7IUJBzJR1qw5g+3bEw7ntQ=";
 }
