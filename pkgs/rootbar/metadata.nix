0f526110a798a0e07993c4022daa61765295da04
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-05-15 05:41:24 +0000

    auto-update(manual): rootbar: bfc10a2f5495 => 96c051136342

--- pkgs/rootbar/metadata.nix
+++ pkgs/rootbar/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/rootbar";
   branch = "default";
-  rev = "bfc10a2f5495";
-  sha256 = "sha256-5yDlZyKNbHF87iuLh1+2Qf27qerTHmadWBq/65MqOwE=";
+  rev = "96c051136342";
+  sha256 = "sha256-rnXnycRqAttus/Xa4+1G3NySjmGDtoHwIHVzCnvncng=";
 }
