9347a8abae1cb4501c810f8954a1a27c1744e540
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-05-15 05:19:09 +0000

    auto-update(manual): obs-wlrobs: a11ffa2fc5ed => 3eb154e5fe63

--- pkgs/obs-wlrobs/metadata.nix
+++ pkgs/obs-wlrobs/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/wlrobs";
   branch = "default";
-  rev = "a11ffa2fc5ed";
-  sha256 = "sha256-Cl6a+Uk0NYpdEgU9zgN6Gn+sI6paEmDEG6OOw0DeBxs=";
+  rev = "3eb154e5fe63";
+  sha256 = "sha256-5b8fU31UjBQ2bEIKWfwRRZc/sMOxxmwrbWDOvO3ubQE=";
 }
