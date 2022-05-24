1db716a67e77415a147f660cd9aa474efaccbf52
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-05-24 14:40:48 +0000

    auto-update(manual): mako: 55104d36575230d57af007cd9e7d53cb3a92a96e => 0d95a1653616454e894f27edc329a9f3a7f96dc2

--- pkgs/mako/metadata.nix
+++ pkgs/mako/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/mako";
   branch = "master";
-  rev = "55104d36575230d57af007cd9e7d53cb3a92a96e";
-  sha256 = "sha256-n5KsA/PmH+wE/6RHP4ACG9b7xmF48AqjEPnuIptofLQ=";
+  rev = "0d95a1653616454e894f27edc329a9f3a7f96dc2";
+  sha256 = "sha256-06UDXVsC7jDd/Lq7dSu3ZnkGW9rLiGHSJLoG9SZi1HY=";
 }
