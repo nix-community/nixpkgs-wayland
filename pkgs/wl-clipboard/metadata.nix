89fab3e7b7ee2533329e8ffa2343c975753e123a
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-06-13 14:16:35 +0000

    auto-update(manual): wl-clipboard: c206d79821bce27dda15ef102536b60ea3182427 => 89cd9f9e430a6cd8b82fa65a9c05e530e2430ffd

--- pkgs/wl-clipboard/metadata.nix
+++ pkgs/wl-clipboard/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/bugaevc/wl-clipboard";
   branch = "master";
-  rev = "c206d79821bce27dda15ef102536b60ea3182427";
-  sha256 = "sha256-lqtLHLsSChWcYWsfFigj0Xveo9doAr7G31fRSaxm0Lw=";
+  rev = "89cd9f9e430a6cd8b82fa65a9c05e530e2430ffd";
+  sha256 = "sha256-7or+a8yV8rcOkfKS/lKwNMsN40Ao2OVXHoTMPLIEuy0=";
 }
