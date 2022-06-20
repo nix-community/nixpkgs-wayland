fc966b50cc0ed5448d6255dac1bbf6d11e79477c
Cole Mickens <cole.mickens@gmail.com>
2022-06-19 22:57:56 -0700

    auto-update(manual): wlroots: d89285f7835d87cb873e2c5de1219ebdaae5d929 => 1d581656c756fd17a6cf55474d32586f6769663f

--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wlroots";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "d89285f7835d87cb873e2c5de1219ebdaae5d929";
-  sha256 = "sha256-Q7rbZxBEWyGX5e+fQ4rPZLhnksQFCOoG1EPHBkecnts=";
+  rev = "1d581656c756fd17a6cf55474d32586f6769663f";
+  sha256 = "sha256-e4Yj5VZzIOsAMMkLErJzMwtCQlfhV+Xu55XhMUVCUrs=";
 }
