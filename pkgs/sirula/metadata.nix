32ee0438dc06c1ff7636bb8b41863b9ab79d6ab4
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-07-08 14:42:23 +0000

    auto-update(manual): sirula: a63956e75a7e218cd100b0a4b2c8fd9f8814bc17 => 70b5ce1936c819f4e5c8674e90145e5e7badcfa8

--- pkgs/sirula/metadata.nix
+++ pkgs/sirula/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/DorianRudolph/sirula";
   branch = "master";
-  rev = "a63956e75a7e218cd100b0a4b2c8fd9f8814bc17";
-  sha256 = "sha256-DrFxV5xIDnbvWUpwMZUqFma3sW6tKGC7UIKTnnXSosU=";
-  cargoSha256 = "sha256-xLP8RgFptK2F1PDOD061NLV+Ge5/O1EufJ4Xqlvd9iY=";
+  rev = "70b5ce1936c819f4e5c8674e90145e5e7badcfa8";
+  sha256 = "sha256-jw3SBgm14AbyVYXOBJBfZbtsAJCwNcRVw0zdUSbuBfk=";
+  cargoSha256 = "sha256-3ULEvkU8hYMeNylV/g98acnCG2vObAB+2G7FVH7t2WE=";
 }
