45a67849fe6667571306f5051495bc27c048fb42
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-08-19 10:20:21 +0000

    auto-update(manual): cage: 46778e6d6c2c327943c16b00e2d600933beec999 => a81ab701d73d0f43c9bb9747e5fdf3c5e226139c

--- pkgs/cage/metadata.nix
+++ pkgs/cage/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/Hjdskes/cage";
   branch = "master";
-  rev = "46778e6d6c2c327943c16b00e2d600933beec999";
-  sha256 = "sha256-eTbu+8C3mB+2664OnULG8xXkvJdWYWIjqofn5ag8JCA=";
+  rev = "a81ab701d73d0f43c9bb9747e5fdf3c5e226139c";
+  sha256 = "sha256-tiIrTfANVWk07zoNYWd1W4KqVJFDVAMi4m3ECJks1XU=";
 }
