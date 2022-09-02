d275f86969e2668d633d55fa7210afca8dd2dae7
Cole Mickens <cole.mickens@gmail.com>
2022-09-02 08:58:36 -0700

    waybar: add libjack2

--- pkgs/waybar/default.nix
+++ pkgs/waybar/default.nix
@@ -1,11 +1,12 @@
 args_@{ stdenv, lib, fetchFromGitHub, waybar
 , libevdev, libxkbcommon, catch2
+, libjack2
 , upower, glib
 , ...}:
 
 let
   metadata = import ./metadata.nix;
-  ignore = [ "waybar" "libevdev" "libxkbcommon" "catch2" "upower" "glib" ];
+  ignore = [ "waybar" "libevdev" "libxkbcommon" "catch2" "upower" "glib" "libjack2" ];
   args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
 in
 (waybar.override args).overrideAttrs(old: {
@@ -16,7 +17,7 @@ in
     repo = "Waybar";
     inherit (metadata) rev sha256;
   };
-  buildInputs = old.buildInputs ++ [ libevdev libxkbcommon catch2 upower glib ];
+  buildInputs = old.buildInputs ++ [ libevdev libxkbcommon catch2 upower glib libjack2 ];
   prePatch = ''
     sed -i "s|version: '0.9.7'|version: '-git-${metadata.rev}'|g" meson.build
   '';
