3999b6f39253bfd122c4807fd5259858261cd154
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-09-10 01:44:46 +0000

    auto-update(manual): glpaper: f89e60b7941f => 9ba380656131

--- pkgs/glpaper/metadata.nix
+++ pkgs/glpaper/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_hg = "https://hg.sr.ht/~scoopta/glpaper/";
   branch = "default";
-  rev = "f89e60b7941f";
-  sha256 = "sha256-E7FKjt3NL0aAEibfaq+YS2IVvpjNjInA+Rs8SU63/3M=";
+  rev = "9ba380656131";
+  sha256 = "sha256-//IKW2BfpqwcR2WAPShs0kzn5nkV/yyABPvOYm3zMPU=";
 }
