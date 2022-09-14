115c0b42fb4e940e4192ece703278a7b0dd7efca
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-09-14 08:21:58 +0000

    auto-update(manual): kanshi: d83229664e9c9f8724ac5936a345fa973befefe0 => eddaeac28ffdc1a263f11c793b99132827a7561f

--- pkgs/kanshi/metadata.nix
+++ pkgs/kanshi/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~emersion/kanshi";
   branch = "master";
-  rev = "d83229664e9c9f8724ac5936a345fa973befefe0";
-  sha256 = "sha256-j+q3Wcg2tZjedNJI6on8fz+zTYKKDvq4CZQ7ZJBO6cU=";
+  rev = "eddaeac28ffdc1a263f11c793b99132827a7561f";
+  sha256 = "sha256-1zn0rKzmbfj+i4dEmFN6wDnZh09haGJNc07N2Z+Ehms=";
 }
