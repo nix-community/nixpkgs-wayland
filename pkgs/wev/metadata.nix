f5ea785ff992dae37451230763a38fffe458f4f7
Cole Mickens <cole.mickens@gmail.com>
2022-10-10 15:08:24 -0700

    auto-update(manual): wev: 54de46d120396ead4dcbce0b52cf506c200380f5 => 83de8e931ab04ce3322a58b359d8effa7901b21c

--- pkgs/wev/metadata.nix
+++ pkgs/wev/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~sircmpwn/wev";
   branch = "master";
-  rev = "54de46d120396ead4dcbce0b52cf506c200380f5";
-  sha256 = "sha256-/viXNkOwJcvmolDblkpvDI5pm9Couvf2JobI4aCiu2w=";
+  rev = "83de8e931ab04ce3322a58b359d8effa7901b21c";
+  sha256 = "sha256-lNFgjRXO/ZbcXJF06DykPoJJ6/a8ZfVA6g95i+rNdWs=";
 }
