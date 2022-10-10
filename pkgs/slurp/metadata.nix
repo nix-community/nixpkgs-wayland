5f89c5a371faec5a14868b095907b72f3b37fc2c
Cole Mickens <cole.mickens@gmail.com>
2022-10-10 15:07:59 -0700

    auto-update(manual): slurp: b38e40fc4ea313ae56f091bd4ca4de954d99776d => 956c9c91ae80e7c487097ca712839e7fc80dd9ae

--- pkgs/slurp/metadata.nix
+++ pkgs/slurp/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/emersion/slurp";
   branch = "master";
-  rev = "b38e40fc4ea313ae56f091bd4ca4de954d99776d";
-  sha256 = "sha256-lgD9Hm83knLqHPYbGFqAJK025NxK4a5C5L+IImLBDLI=";
+  rev = "956c9c91ae80e7c487097ca712839e7fc80dd9ae";
+  sha256 = "sha256-hq3sw3lkwNLwWNRXiVzH4LrND7jxjrpaOvlgJnZ/rJQ=";
 }
