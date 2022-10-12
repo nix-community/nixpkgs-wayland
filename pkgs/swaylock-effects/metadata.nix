d6e53b1b05ccd1ac16792e1cf122e3c5dda246c4
Cole Botkens <cole.mickens+colebot@gmail.com>
2022-10-12 21:46:51 +0000

    auto-update(manual): swaylock-effects: a8fc557b86e70f2f7a30ca9ff9b3124f89e7f204 => b2ea29de5b7ca5c83aa2495360142e046516e00d

--- pkgs/swaylock-effects/metadata.nix
+++ pkgs/swaylock-effects/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/mortie/swaylock-effects";
   branch = "master";
-  rev = "a8fc557b86e70f2f7a30ca9ff9b3124f89e7f204";
-  sha256 = "sha256-GN+cxzC11Dk1nN9wVWIyv+rCrg4yaHnCePRYS1c4JTk=";
+  rev = "b2ea29de5b7ca5c83aa2495360142e046516e00d";
+  sha256 = "sha256-qNXDykXz7Pc7pu35SUkOIIsLs63wNrwNt9LaMLXU+ic=";
 }
