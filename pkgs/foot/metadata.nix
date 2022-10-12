5b04d46b2c016e01df42ab2515b6972b698a13a3
Osama Rebach <osamarebach@gmail.com>
2022-10-12 18:26:38 +0100

    foot: generate pgo data for xtgettcap

--- pkgs/foot/metadata.nix
+++ pkgs/foot/metadata.nix
@@ -2,6 +2,6 @@
   skip = true;
   repo_git = "https://codeberg.org/dnkl/foot";
   branch = "master";
-  rev = "37218be64853dccd5574a7f5de7193c0e1a7b5e6";
-  sha256 = "sha256-0lNMwgXE5DxY+NPJo39oagkzfm9qOTTvFCD/ZJ2fUBI=";
+  rev = "3949e3427169f20f050d5df8ae824fa5a8d75fd6";
+  sha256 = "sha256-X80UWv+HdkHEZW7kpLH+iE9oFmTv1epaogd76kK9YoI=";
 }
