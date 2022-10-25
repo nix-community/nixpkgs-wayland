4012a163dd98ded08c26b858f69a95c52e87064f
Cole Mickens <cole.mickens@gmail.com>
2022-10-25 15:46:59 -0700

    auto-update(manual): wayprompt: 58020dea3a7c3c16d4b9e47e375f9cc1b8c3f7cc => ff0004a7a2a91ef841162369b2ed2e25a2dff9ce

--- pkgs/wayprompt/metadata.nix
+++ pkgs/wayprompt/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://git.sr.ht/~leon_plickat/wayprompt";
   branch = "master";
-  rev = "58020dea3a7c3c16d4b9e47e375f9cc1b8c3f7cc";
-  sha256 = "sha256-ERoaaCP6xT7Jn9pS5g9y/FRFcw4n7lS0hvr4pftYduo=";
+  rev = "ff0004a7a2a91ef841162369b2ed2e25a2dff9ce";
+  sha256 = "sha256-wXgCU3FM/D5348aIAurj3raiCThPFGUDoHPXImRYxM8=";
 }
