f0e3a16ee80be6cb0e3f0251a653a59602e5e052
Artturin <Artturin@artturin.com>
2022-10-25 21:52:50 +0300

    convert some packages to use githubTemplate

--- templates/github-template.nix
+++ templates/github-template.nix
@@ -1,19 +1,29 @@
 args_@{ attrName
 , prev
+, extra
 , ...
 }:
 
 let
   inherit (prev) lib;
   metadata = import ../pkgs//${attrName}/metadata.nix;
-  ignore = [ "attrName" "prev" ];
+  ignore = [ "attrName" "prev" "extra" ];
   args = lib.filterAttrs (n: _: (!builtins.elem n ignore)) args_;
   overridenAttr = ((lib.attrByPath [ attrName ] (throw "attr ${attrName} does not exist in nixpkgs") prev).override args);
 in
-overridenAttr.overrideAttrs (_: {
-  version = metadata.rev;
-  src = prev.fetchFromGitHub {
-    inherit (metadata) owner repo rev sha256;
-  };
-})
+overridenAttr.overrideAttrs (old: (
+  {
+    version = metadata.rev;
+    src = prev.fetchFromGitHub {
+      inherit (metadata) owner repo rev sha256;
+    };
+  } // lib.optionalAttrs (extra ? nativeBuildInputs)
+    {
+      nativeBuildInputs = extra.nativeBuildInputs ++ old.nativeBuildInputs;
+    } // lib.optionalAttrs (extra ? patches)
+    {
+      patches = extra.patches;
+    }
+)
+)
 
