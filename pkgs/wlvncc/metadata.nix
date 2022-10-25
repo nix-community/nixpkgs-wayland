c6459c8b6f3e7338080b72e3f0493ef5576ad13e
Cole Mickens <cole.mickens@gmail.com>
2022-10-25 15:47:22 -0700

    auto-update(manual): wlvncc: 087a78e551d7aba4bf1ac4db0483e232ba5869c0 => ed52288aca86af60093bf7e4369464927ff339fc

--- pkgs/wlvncc/metadata.nix
+++ pkgs/wlvncc/metadata.nix
@@ -1,6 +1,6 @@
 {
   repo_git = "https://github.com/any1/wlvncc";
   branch = "master";
-  rev = "087a78e551d7aba4bf1ac4db0483e232ba5869c0";
-  sha256 = "sha256-RTf5QH37ahn0gDFU4pg7jNQ8wPR+Oxb2bc6b4LEuV3g=";
+  rev = "ed52288aca86af60093bf7e4369464927ff339fc";
+  sha256 = "sha256-rJWK31XPBkb5CBcYDjmmGwRAZIdLYDb3G2bpNue7f1U=";
 }
