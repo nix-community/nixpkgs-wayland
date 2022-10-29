56eb2515cf1d8eac44ae1be43da0b55169ffb632
Cole Mickens <cole.mickens@gmail.com>
2022-10-29 09:35:58 -0700

    auto-update(manual): waypipe: 23fef70904df87e9ebcb65de57ae49c14888eeaa => 0d911a31bcfbed53fa0277437d728e6ae3b0406c

--- pkgs/waypipe/metadata.nix
+++ pkgs/waypipe/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "waypipe";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "23fef70904df87e9ebcb65de57ae49c14888eeaa";
-  sha256 = "sha256-S0q3AGESYoS28hKtuVHg3MoMcw8OnB6IGOEi5eIOJoU=";
+  rev = "0d911a31bcfbed53fa0277437d728e6ae3b0406c";
+  sha256 = "sha256-Pbeshm2V5NfucT8zyi+0gen4bHrOpoDYAq7LI6SVzr4=";
 }
