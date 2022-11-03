d046e92ca78da53cdc3ae7e16b84220a06f038e6
Cole Mickens <cole.mickens@gmail.com>
2022-11-02 17:07:11 -0700

    salut: skip for now

--- pkgs/salut/metadata.nix
+++ pkgs/salut/metadata.nix
@@ -3,6 +3,7 @@ rec {
   repo = "salut";
   repo_git = "https://gitlab.com/${owner}/${repo}";
   branch = "master";
+  skip = true;
   rev = "2c2d8e325adae3b134dbe6275673b8235cae3663";
   sha256 = "sha256-WV7zremeW9vw0Fssk9Vj2dKVYnHhfqEIxoMGTs2+mhU=";
   cargoSha256 = "sha256-axuPX9mRo8ZTNLT6TURajC1N4dcrDtsNqnZzRJzGSmQ=";
