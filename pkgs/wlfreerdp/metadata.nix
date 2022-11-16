7d87532f037d8d9ce67517eec123650883ce9a78
Cole Mickens <cole.mickens@gmail.com>
2022-11-16 15:44:28 -0800

    auto-update: wlfreerdp: ebc8cd1d4a314494dfc9c73d12507f28efb5b3f1 => b1583d56c07ca530dd130bd83f889174b5f227ae

--- pkgs/wlfreerdp/metadata.nix
+++ pkgs/wlfreerdp/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "FreeRDP";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "ebc8cd1d4a314494dfc9c73d12507f28efb5b3f1";
-  sha256 = "sha256-2ZTNNFC3PMjj9kGl4oYzUKZ1EvuyDQSzVYgg3fkIABQ=";
+  rev = "b1583d56c07ca530dd130bd83f889174b5f227ae";
+  sha256 = "sha256-BPILk+3Io5lmixBWxPkrL0kD6dqbfghTj0ML0HHOy7I=";
 }
