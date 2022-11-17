b13be49557e62cd2f2acc402826eec76512d7998
Cole Mickens <cole.mickens@gmail.com>
2022-11-17 14:44:09 -0800

    wlroots: add hwdata

--- pkgs/wlroots/default.nix
+++ pkgs/wlroots/default.nix
@@ -1,12 +1,15 @@
-args_@{ lib, fetchFromGitLab, wlroots, ... }:
+args_@{ lib, fetchFromGitLab, wlroots
+, hwdata
+, ... }:
 
 let
   metadata = import ./metadata.nix;
-  ignore = [ "wlroots" ];
+  ignore = [ "wlroots" "hwdata" ];
   args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
 in
 (wlroots.override args).overrideAttrs (old: {
   version = "${metadata.rev}";
+  buildInputs = old.buildInputs ++ [ hwdata ];
   src = fetchFromGitLab {
     inherit (metadata) domain owner repo rev sha256;
   };
