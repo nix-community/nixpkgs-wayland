f5d63a557cc6085a784db0f94ad171a36bc4f382
Artturin <Artturin@artturin.com>
2022-12-18 10:13:04 +0200

    add new-wayland-protocols
    often there's a issue with us not having a new enough wayland-protocols
    
    maybe we should add it as a package? but under the name
    new-wayland-protocols so we don't rebuild all of its dependencies

--- flake.nix
+++ flake.nix
@@ -136,6 +136,13 @@
             (s: { attrName = s; }));
 
           waylandPkgs = genPackagesGH // rec {
+            new-wayland-protocols = prev.wayland-protocols.overrideAttrs (_: _: rec {
+              version = "1.31";
+              src = prev.fetchurl {
+                url = "https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/${version}/downloads/wayland-protocols-${version}.tar.xz";
+                hash = "sha256-oH+nIu2HZ27AINhncUvJovJMRk2nORLzlwbu71IZ4jg=";
+              };
+            });
             # wlroots-related
             salut = prev.callPackage ./pkgs/salut { };
             wayprompt = prev.callPackage ./pkgs/wayprompt { };
@@ -158,6 +165,7 @@
             wldash = prev.callPackage ./pkgs/wldash { };
             wlroots = prev.callPackage ./pkgs/wlroots {
               inherit (prev) wlroots;
+              wayland-protocols = final.new-wayland-protocols;
             };
 
             # misc
