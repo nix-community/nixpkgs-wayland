e9b78940481e316618eb23891f28c09cb628b008
Cole Mickens <cole.mickens@gmail.com>
2022-12-17 23:03:50 -0800

    auto-update: xdg-desktop-portal-wlr: c83b3cc51411237aa2071de5303ae5719d926032 => 0a8bfa14607022a080f8b6eabc4757d1918d3cd9

--- pkgs/xdg-desktop-portal-wlr/metadata.nix
+++ pkgs/xdg-desktop-portal-wlr/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "xdg-desktop-portal-wlr";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "c83b3cc51411237aa2071de5303ae5719d926032";
-  sha256 = "sha256-9WdZDUZ+K6gW7c0Ly0oFh90X2FtdeXfUjTJEH23Zxkw=";
+  rev = "0a8bfa14607022a080f8b6eabc4757d1918d3cd9";
+  sha256 = "sha256-UIYqVPdVysVljhZ1JiC1AaWnLvjXbPRQVaCgkfIwmM8=";
 }
