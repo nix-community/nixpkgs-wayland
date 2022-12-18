d6dd67d18cd378ef9e0c75def63934fd763ca0fa
Cole Mickens <cole.mickens@gmail.com>
2022-12-17 23:03:06 -0800

    auto-update: slurp: 956c9c91ae80e7c487097ca712839e7fc80dd9ae => 78aa986f5163e8b014eaf95d4d143f09c4a63da9

--- pkgs/slurp/metadata.nix
+++ pkgs/slurp/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "slurp";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "956c9c91ae80e7c487097ca712839e7fc80dd9ae";
-  sha256 = "sha256-hq3sw3lkwNLwWNRXiVzH4LrND7jxjrpaOvlgJnZ/rJQ=";
+  rev = "78aa986f5163e8b014eaf95d4d143f09c4a63da9";
+  sha256 = "sha256-XVhHvnm5Ym+S3xWn+9HnQFrnGV1JRGMW/GtY/gjTg/0=";
 }
