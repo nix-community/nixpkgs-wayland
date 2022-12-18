3640c71a2cd5dbac0d360a7b83843dcc8a9dea4a
Cole Mickens <cole.mickens@gmail.com>
2022-12-17 23:21:44 -0800

    waybar: cfef78a5 -> e00e36981

--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "Waybar";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "cfef78a5bc8df6a29e06ec128f7a6075425db402";
-  sha256 = "sha256-XMpl1soXKFe1NOux4HRBrmMrANgcnjGeGsUUKOzOFkE=";
+  rev = "e00e36981e5a9297d72c10df1c267f060dab6533";
+  sha256 = "sha256-aq4eHpAC4gqi83Hux7oWZibaarxLVdzvHhpyXz3OSuM=";
 }
