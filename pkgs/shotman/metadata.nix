a4f28d2975704a9bce0755f4416e035241b72a75
Cole Mickens <cole.mickens@gmail.com>
2022-12-17 23:03:02 -0800

    auto-update: shotman: d496d601c8e341c59d8eed541f20cbadef668847 => 1fe408d851686a0dc158c68ccd97d14e4b8355c8

--- pkgs/shotman/metadata.nix
+++ pkgs/shotman/metadata.nix
@@ -5,7 +5,7 @@ rec {
   repo = "shotman";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "d496d601c8e341c59d8eed541f20cbadef668847";
-  sha256 = "sha256-k120m6lxifNEdvgpkehKzCAjnAOj0aCNCxlTBEBFvWY=";
-  cargoSha256 = "sha256-AAOdWl7VGfaP5enTg1vX+l+gnezvZkG6O1WkvUA08EI=";
+  rev = "1fe408d851686a0dc158c68ccd97d14e4b8355c8";
+  sha256 = "sha256-71qSuvLfVN0fdFgphOKkLCxY/SVsV0i9oqKoQ4RZPbM=";
+  cargoSha256 = "sha256-pccAQzlbIYQpXGTxymW6WnyCvJ6t8/WjdKzvKRTdjOo=";
 }
