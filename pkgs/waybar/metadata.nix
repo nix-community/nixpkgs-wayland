1584ac67a5dd0e829cfe128b74f16a24d21b0013
Cole Mickens <cole.mickens@gmail.com>
2023-01-07 12:51:30 -0800

    auto-update: waybar: 1d6cfe7ce6015bcdaff1c2ca565e5e1f2796e10b => a92223c31657e0df2e0e0588a89bf14906c86a27

--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "Waybar";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "1d6cfe7ce6015bcdaff1c2ca565e5e1f2796e10b";
-  sha256 = "sha256-DUZ/MPRMX/TmbJHaB150Wjlm8D1kewBEoi+ZQLTsCRM=";
+  rev = "a92223c31657e0df2e0e0588a89bf14906c86a27";
+  sha256 = "sha256-vIgyb9vpsoBQ9wXjhQBWZ6LFHvApTMh2jPTLNygFcVs=";
 }
