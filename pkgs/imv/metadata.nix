dfaf4425ed71cfcdc31fb2d4251ad025f0e2139d
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-01-18 22:36:03 +0000

    auto-update: imv: 4448fb6104d67e3dfff3e71babe257992fce5560 => 885e17397ac503de84723d4f0b1c97b1258548ab

--- pkgs/imv/metadata.nix
+++ pkgs/imv/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "imv";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "4448fb6104d67e3dfff3e71babe257992fce5560";
-  sha256 = "sha256-4RVfVqt4Oh4SPtkJRnkx1QIS0HW84A9v5T2u1D3lYzk=";
+  rev = "885e17397ac503de84723d4f0b1c97b1258548ab";
+  sha256 = "sha256-LLEEbriHzZhAOQivqHqdr6g7lh4uj++ytlme8AfRjf4=";
 }
