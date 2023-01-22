8c1b8e8ac43e6a493fa56c235f521003608070a8
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-01-22 18:54:19 +0000

    auto-update: aml: f4efd0edec8d39b176e32f2478a19b3512eeed1c => b83f3576ce4187d9285f06e9066ef43a691464d4

--- pkgs/aml/metadata.nix
+++ pkgs/aml/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "aml";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "f4efd0edec8d39b176e32f2478a19b3512eeed1c";
-  sha256 = "sha256-wXJDTe7UGS2tsgFtHzdgky+C1z87fF1sNujDdR4ebgQ=";
+  rev = "b83f3576ce4187d9285f06e9066ef43a691464d4";
+  sha256 = "sha256-BX+MRqvnwwLPhz22m0gfJ2EkW31KQEi/YTgOCMcQk2Q=";
 }
