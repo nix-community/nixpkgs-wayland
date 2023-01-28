976adb64096c2ae28e44daee7837b437bef285e2
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-01-28 22:12:09 +0000

    auto-update: swaylock: 6213fa0a7cac6da1a49f340d4999b9f6f9e82a5c => ac3b49b6571ceda3f8db11a98bfe320106996280

--- pkgs/swaylock/metadata.nix
+++ pkgs/swaylock/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "swaylock";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "6213fa0a7cac6da1a49f340d4999b9f6f9e82a5c";
-  sha256 = "sha256-a0cEUidGsqf5TOCVirZCler3x9OJlZB+ypoTz2luCMI=";
+  rev = "ac3b49b6571ceda3f8db11a98bfe320106996280";
+  sha256 = "sha256-Zpxc9h0S4JXPZgxugtvXfOexatyhcfiujM/KGGBSv8U=";
 }
