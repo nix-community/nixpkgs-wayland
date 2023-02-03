8f62034c03e8690d30dfaf2de4e1e9c27431a392
Cole Mickens <cole.mickens@gmail.com>
2023-02-03 10:06:11 -0600

    auto-update: freerdp: 0c496681f5ac8b08f77121388d413eff298edb57 => 2eda0aa2ea7c3ff3c5c9f7cfa8cf65146be9564f

--- pkgs/freerdp/metadata.nix
+++ pkgs/freerdp/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "FreeRDP";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "0c496681f5ac8b08f77121388d413eff298edb57";
-  sha256 = "sha256-+5mMd+vLuoofHr+VP/goOLYAasi0BXfrtdM+UudlkfA=";
+  rev = "2eda0aa2ea7c3ff3c5c9f7cfa8cf65146be9564f";
+  sha256 = "sha256-XONmVvsOT8Bbt6hhNL8l0Pau0Dzqg99FwfCfw8nIkH4=";
 }
