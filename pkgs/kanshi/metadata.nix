3bf7499ef7c9f50eba44f11c064e040e66f8b3c8
Cole Mickens <cole.mickens@gmail.com>
2023-02-03 22:22:52 -0600

    auto-update: kanshi: 6bf46e97df34c0e47fb6e92374a62e598dd7082b => d044f2a888202fd772ceee8117367b4ecdad7bb3

--- pkgs/kanshi/metadata.nix
+++ pkgs/kanshi/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "kanshi";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "6bf46e97df34c0e47fb6e92374a62e598dd7082b";
-  sha256 = "sha256-hV8FyP5RBFZYNSutc8J5W1PrfhegM24tPwz3W3WZKBk=";
+  rev = "d044f2a888202fd772ceee8117367b4ecdad7bb3";
+  sha256 = "sha256-VErGRHZG29MHT98rWV8r2OT3iG7px36nRikJyf+8OCQ=";
 }
