fc3267b75cded14f95744996f82fae5c33fef67d
Cole Mickens <cole.mickens@gmail.com>
2023-02-04 15:14:27 -0600

    auto-update: wayprompt: 1b60c61d39d210f7e025e7ca57fb563ffd4d1839 => 3d737328fe70726ae1233e491a8ae51db4029093

--- pkgs/wayprompt/metadata.nix
+++ pkgs/wayprompt/metadata.nix
@@ -5,7 +5,7 @@ rec {
   repo = "wayprompt";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "1b60c61d39d210f7e025e7ca57fb563ffd4d1839";
-  sha256 = "sha256-MJWO803lzX0uczAVZf0KxBqfhFYt/zme5JuAhukVuDA=";
+  rev = "3d737328fe70726ae1233e491a8ae51db4029093";
+  sha256 = "sha256-/CMjBcdJ7Def1TfZZRQ2vQSI+/otV2NEh4BIB/lEbC4=";
   fetchSubmodules = true;
 }
