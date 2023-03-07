d9a91e7c7b80b0683c676c1444ee1dc35faa1c1f
Artturin <Artturin@artturin.com>
2023-03-07 21:38:29 +0200

    wlroots: unblock

--- pkgs/wlroots/default.nix
+++ pkgs/wlroots/default.nix
@@ -1,15 +1,15 @@
-args_@{ lib, fetchFromGitLab, wlroots
+args_@{ lib, fetchFromGitLab, wlroots, libdisplay-info
 , hwdata
 , ... }:
 
 let
   metadata = import ./metadata.nix;
-  ignore = [ "wlroots" "hwdata" ];
+  ignore = [ "wlroots" "hwdata" "libdisplay-info" ];
   args = lib.filterAttrs (n: v: (!builtins.elem n ignore)) args_;
 in
 (wlroots.override args).overrideAttrs (old: {
   version = "${metadata.rev}";
-  buildInputs = old.buildInputs ++ [ hwdata ];
+  buildInputs = old.buildInputs ++ [ hwdata libdisplay-info ];
   src = fetchFromGitLab {
     inherit (metadata) domain owner repo rev sha256;
   };
