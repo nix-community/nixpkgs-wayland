3aa8e64ccc3e18f07fac6c10e7dceaf33d7a68c0
Artturin <Artturin@artturin.com>
2023-04-18 19:36:37 +0300

    wlroots skip for now

--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -6,4 +6,6 @@ rec {
   branch = "master";
   rev = "40dde59475bef1eaa7ac70786b700fb3549bb366";
   sha256 = "sha256-NBNKfc5MpFGY/Pph0cXlpGKAXPnz7r5ImWZV9UwvXvA=";
+  skip = true; #        > Dependency wayland-server found: NO found 1.21.0 but need: '>=1.22'
+  # depends on https://github.com/NixOS/nixpkgs/pull/226283
 }
