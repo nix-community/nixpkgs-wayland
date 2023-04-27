78c9f9c0081b33054c6216734a573b2bfe0d9fc4
Artturin <Artturin@artturin.com>
2023-04-27 12:24:36 +0300

    sway-unwrapped: remove conflicting patch

--- flake.nix
+++ flake.nix
@@ -80,6 +80,14 @@
               replaceInput = {
                 wlroots_0_16 = final.wlroots;
               };
+              replace.patches = let
+                conflicting-patch = (prev.fetchpatch {
+                  name = "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch";
+                  url = "https://github.com/swaywm/sway/commit/dee032d0a0ecd958c902b88302dc59703d703c7f.diff";
+                  hash = "sha256-dx+7MpEiAkxTBnJcsT3/1BO8rYRfNLecXmpAvhqGMD0=";
+                });
+              in
+                lib.remove conflicting-patch prev.sway-unwrapped.patches;
             }
             {
               attrName = "cage";
