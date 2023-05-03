e0158588c3317516ca5ddb963419e946c328ee77
Cole Mickens <cole.mickens@gmail.com>
2023-05-02 21:26:52 -0600

    sway-unwrapped: unskip

--- pkgs/sway-unwrapped/metadata.nix
+++ pkgs/sway-unwrapped/metadata.nix
@@ -7,5 +7,4 @@ rec {
   branch = "master";
   rev = "dadf3e9b7865e7a907a6d95cbb94a2e1cd8cf67f";
   sha256 = "sha256-ma4kBTd5+KD6lAAM+DrlXbCEq5YKXxIUDuVOwvTcBKg=";
-  skip = true; # wlroots is skipped
 }
