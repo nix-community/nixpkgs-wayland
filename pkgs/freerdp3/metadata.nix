7554b5d82e68d0d07862920428b996ecd9633fe7
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-05-12 12:29:43 +0000

    auto-update: freerdp3: 52786d0f12e02383acb830409628ea47fd59ecc0 => 4a006322afb7d861554fa40beeccaf61b6769216

--- pkgs/freerdp3/metadata.nix
+++ pkgs/freerdp3/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "FreeRDP";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "52786d0f12e02383acb830409628ea47fd59ecc0";
-  sha256 = "sha256-IyAoxfRTj12pwpufATvbuW5D6gVT1YmKPEv/RDnWI1s=";
+  rev = "4a006322afb7d861554fa40beeccaf61b6769216";
+  sha256 = "sha256-bzxghHGVFWhk4Tnqlbc+N2FhIoJGeuRHyhEENinwcKw=";
 }
