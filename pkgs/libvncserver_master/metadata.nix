c254d310633e0caa0a954e974577ee18e0b47c63
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-05-20 10:14:40 +0000

    auto-update: libvncserver_master: f9dabe8a843c1cada59baad4871618029e7be924 => d62002e46fb4a4a16573ff9b8a37d114443aa059

--- pkgs/libvncserver_master/metadata.nix
+++ pkgs/libvncserver_master/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "libvncserver";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "f9dabe8a843c1cada59baad4871618029e7be924";
-  sha256 = "sha256-DyU8nDjvWIw4wn/0Fr+jkt+SZspD7qTVuLtrNiTXx6A=";
+  rev = "d62002e46fb4a4a16573ff9b8a37d114443aa059";
+  sha256 = "sha256-DUSXNUMIbH2wsQuoVxdENaTI0rEbx7iBIKV3rOzaygw=";
 }
