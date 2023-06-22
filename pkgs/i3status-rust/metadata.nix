ca43222ce915655d4314699200b0e5ef1ea6eeb8
Artturin <Artturin@artturin.com>
2023-06-22 17:33:31 +0300

    skip i3status-rust

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -6,4 +6,6 @@ rec {
   branch = "master";
   rev = "d6dfd0d122f0532f44eae82aa5774fae244d1f19";
   sha256 = "sha256-ir0t9CSRCt1PmTd75plq14RnxZM84VSbq72Ka3W6R04=";
+  # error[E0658]: use of unstable library feature 'is_some_and'
+  skip = true;
 }
