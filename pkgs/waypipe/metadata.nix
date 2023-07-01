5ce4dddf7de5f490c1b0892b399501435b0f5ec4
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-07-01 21:14:15 +0000

    auto-update: waypipe: 033d0e7eb613f1ddfe104d07079a81b8d4e02dac => a55cd3b10c5ff7e99a2d454041687272251d2758

--- pkgs/waypipe/metadata.nix
+++ pkgs/waypipe/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "waypipe";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "033d0e7eb613f1ddfe104d07079a81b8d4e02dac";
-  sha256 = "sha256-1VLPnP4BmF9Zha0uVsPjA/WbF/oLfZmdDX57SzqrV5A=";
+  rev = "a55cd3b10c5ff7e99a2d454041687272251d2758";
+  sha256 = "sha256-g55v8PUBvBgkCoBpN3IcoZWAEaooPFnVXvrlyhgnu2o=";
 }
