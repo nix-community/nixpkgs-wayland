7277a255a5fdc74c83e95a39c6c6bce8791f5369
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-07-07 20:17:33 +0000

    auto-update: shotman: 0328ae53c87166097c0259dd840ee237c63a1b31 => a172c2fdd07fb696f59a7a64b7d3fd3179af1a96

--- pkgs/shotman/metadata.nix
+++ pkgs/shotman/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "shotman";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "0328ae53c87166097c0259dd840ee237c63a1b31";
-  sha256 = "sha256-7d6piFJ6TVlBO0ddV6Hi6hypaSgLJ70WLHKG7dE01fg=";
+  rev = "a172c2fdd07fb696f59a7a64b7d3fd3179af1a96";
+  sha256 = "sha256-BZGN446ie92Igrc2V+c8Kuj+RZOB/I2zHMERH6IsH8o=";
 }
