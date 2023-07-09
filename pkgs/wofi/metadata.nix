f39ecc7c68f31fe422eff2e650a7901eb865d3f4
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-07-09 10:17:09 +0000

    auto-update: wofi: 6e2e74b6c797 => 0136d7bfedf9

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wofi";
   repo_hg = "https://${domain}/${owner}/${repo}";
   branch = "default";
-  rev = "6e2e74b6c797";
-  sha256 = "sha256-UVtArldxRtvZQbO0ObK1m4f1EZb42e+dVY5adNwC0bU=";
+  rev = "0136d7bfedf9";
+  sha256 = "sha256-WwKikyZTRLu1mwE+qpLppm3QwYt33IUrObl1MPr2wW8=";
 }
