d8f8d40462fb287e7a1b341119c34e1d731fb58f
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-07-21 07:13:55 +0000

    auto-update: foot: 098f0aafd46cf512515b1d94e7cd3f2ce371faec => 15d7885c785fa0898eb72657494e508aaf40beb8

--- pkgs/foot/metadata.nix
+++ pkgs/foot/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "foot";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "098f0aafd46cf512515b1d94e7cd3f2ce371faec";
-  sha256 = "sha256-Pk7AP4h5FL6zHiPLrMKw73J9FLxiWb7PW44bjq/zFqs=";
+  rev = "15d7885c785fa0898eb72657494e508aaf40beb8";
+  sha256 = "sha256-lNKfO/nogTRFG7mnWew36A1Ekk4aT5A866QBCY7xyzk=";
 }
