30ab3103787bf29da9e78f00503a430230b9a038
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-07-26 13:18:50 +0000

    auto-update: wdisplays: 1a32d8bbe285e631f1b0365eafd09017cb38a789 => 6233901a887c2bce4989dbb936c2cb51e02b23a0

--- pkgs/wdisplays/metadata.nix
+++ pkgs/wdisplays/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wdisplays";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "1a32d8bbe285e631f1b0365eafd09017cb38a789";
-  sha256 = "sha256-dtvP930ChiDRT60xq6xBDU6k+zHnkrAkxkKz2FxlzRs=";
+  rev = "6233901a887c2bce4989dbb936c2cb51e02b23a0";
+  sha256 = "sha256-FZpUzUKyoJ+Di36wx/35PDjeZjFL/fDuT/2sO9XINBA=";
 }
