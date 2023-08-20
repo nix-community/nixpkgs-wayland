253d882e4723e65dfcb72446294a793170670a0b
Cole Mickens <cole.mickens@gmail.com>
2023-08-19 20:45:33 -0500

    swww: unskip

--- pkgs/swww/metadata.nix
+++ pkgs/swww/metadata.nix
@@ -3,7 +3,6 @@ rec {
   repo = "swww";
   repo_git = "https://github.com/${owner}/${repo}";
   branch = "main";
-  skip = true;
   rev = "25f0e8edaeeb8886b23ef9db81bb48c9efc22d86";
   sha256 = "sha256-Z90Zwjqcu0hbtJwKPUNV996mtdgX//ZWWm+Dya/JV9A=";
 }
