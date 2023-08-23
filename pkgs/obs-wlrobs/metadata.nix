8bc88b4fa5f4a16d52d0bc2c619eb921a800028c
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-08-23 04:17:56 +0000

    auto-update: obs-wlrobs: a48763b63f96 => f72d5cb3cbbd

--- pkgs/obs-wlrobs/metadata.nix
+++ pkgs/obs-wlrobs/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wlrobs";
   repo_hg = "https://${domain}/${owner}/${repo}";
   branch = "default";
-  rev = "a48763b63f96";
-  sha256 = "sha256-5yJlo6xr9buMGmjViVWJvjLfRtjOAl/ESg4DDJdIOp4=";
+  rev = "f72d5cb3cbbd";
+  sha256 = "sha256-hiM0d38SSUqbyisP3fAtKRLBDjVKZdU2U1xyXci7yNk=";
 }
