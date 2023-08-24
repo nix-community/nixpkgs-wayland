25adbc09f7d0a5422ef006330230992180f55b47
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-08-24 08:18:41 +0000

    auto-update: cage: 121e3ac8b28db191e02645af7d8bab9d289ccd8b => 7ec7e3df2bfc2069219285ccca676327480fa471

--- pkgs/cage/metadata.nix
+++ pkgs/cage/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "cage";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "121e3ac8b28db191e02645af7d8bab9d289ccd8b";
-  sha256 = "sha256-q49MVO1oy2dHXnRFR+DNnLHdf50qB1zSs3OQgxxm7Kc=";
+  rev = "7ec7e3df2bfc2069219285ccca676327480fa471";
+  sha256 = "sha256-Vlf47Igc8leo9lh4OG0/W3tI1u22C/jDxcbn6/mPK5c=";
 }
