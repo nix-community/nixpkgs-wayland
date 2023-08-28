12f9c19bbc0a762bb19003ec83a204ab0a95919e
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-08-28 18:20:14 +0000

    auto-update: wf-recorder: 62bc883ee973ab045f3ff805fd67c420169d3cf1 => 0e7b2e27d70d312c6e70a16482ea59e4d4f506eb

--- pkgs/wf-recorder/metadata.nix
+++ pkgs/wf-recorder/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wf-recorder";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "62bc883ee973ab045f3ff805fd67c420169d3cf1";
-  sha256 = "sha256-qzpG4Im5bMxcHwILhGox8lnlp17gy9x0qlbvs7iL24k=";
+  rev = "0e7b2e27d70d312c6e70a16482ea59e4d4f506eb";
+  sha256 = "sha256-jdgBYo1ZFCl4Yksi/MbSqfNhWpSBcZYrMzy5Hytop6Q=";
 }
