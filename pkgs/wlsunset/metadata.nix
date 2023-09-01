a7b2ffb000242b48c4d9332e762ec46f67aa8aaf
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-09-01 14:15:05 +0000

    auto-update: wlsunset: c59bbbd6e3d3546f2903518824625e3289e774aa => b6cfe1761df05352c0ca7c3706476d5272d9521f

--- pkgs/wlsunset/metadata.nix
+++ pkgs/wlsunset/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wlsunset";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "c59bbbd6e3d3546f2903518824625e3289e774aa";
-  sha256 = "sha256-dMK/+/J/IRUj2VbOeyiWS8R7yLXQFXS2F72l8G+j1Qg=";
+  rev = "b6cfe1761df05352c0ca7c3706476d5272d9521f";
+  sha256 = "sha256-9oakhJssGyGSSrkae0WBN2dFmqWpA25WgU6S57zHEN8=";
 }
