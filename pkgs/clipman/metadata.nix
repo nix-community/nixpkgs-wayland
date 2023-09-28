d93a29c75d878bce117859e4f7710e1a6bafda4d
Artturin <Artturin@artturin.com>
2023-09-28 23:39:34 +0300

    skip clipman

--- pkgs/clipman/metadata.nix
+++ pkgs/clipman/metadata.nix
@@ -7,4 +7,6 @@ rec {
   rev = "8b30de8d3ae29bf88337121cf3bc30afbc2d06c4";
   sha256 = "sha256-BjiUxvcWk5a3pIhUJGtYBg5nh7xsc0xxN+YDDF3Fdw4=";
   vendorSha256 = "sha256-Z/sVCJz/igPDdeczC6pemLub6X6z4ZGlBwBmRsEnXKI=";
+  # upstream was removed
+  skip = true;
 }
