36fcf6e9155cb029fa9304880f941e823603f3ae
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-11-24 02:26:12 +0000

    auto-update: wl-screenrec: 161dec4dd0366c40be2d211315f0c60dd22b07ed => 82622bb72aa39146ce7b913f2349dde1fa69f6c0

--- pkgs/wl-screenrec/metadata.nix
+++ pkgs/wl-screenrec/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wl-screenrec";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "161dec4dd0366c40be2d211315f0c60dd22b07ed";
-  sha256 = "sha256-eAP2aOBdxQNm0GealN2bQaihcDzH+ahlWiu5HYIphfc=";
+  rev = "82622bb72aa39146ce7b913f2349dde1fa69f6c0";
+  sha256 = "sha256-FS0sBaEsHC6GYyoXQmNheNUVGbwysa3UXIto2Np/BNE=";
 }
