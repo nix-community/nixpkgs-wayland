954efc4fcce36a4389b7947821364e147b1e8f0b
Artturin <Artturin@artturin.com>
2023-12-06 21:45:35 +0200

    auto-update: libvncserver_master: bff96128a8c58e17cfced666706ca9da4be46429 => 784cccbb724517ee4e36d9938f93b9ee168a29e7

--- pkgs/libvncserver_master/metadata.nix
+++ pkgs/libvncserver_master/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "libvncserver";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "bff96128a8c58e17cfced666706ca9da4be46429";
-  sha256 = "sha256-+/FDeSE3Tg6QLItCZHS8KeBKWkxIynVbQwsrdOn8L5s=";
+  rev = "784cccbb724517ee4e36d9938f93b9ee168a29e7";
+  sha256 = "sha256-sBeBzJCaTXijS2khtZOnVj/YQqG+eIysjscBAhAW8gM=";
 }
