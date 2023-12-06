4ad414dc621c9365e78044974bd61d5f9d3fbaa3
Artturin <Artturin@artturin.com>
2023-12-06 21:45:29 +0200

    auto-update: i3status-rust: 6d936b9698902ad4bd35294cdb26147c1b23bb90 => 93a2d7db903a946d72cc590567e331c03e958926

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "i3status-rust";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "6d936b9698902ad4bd35294cdb26147c1b23bb90";
-  sha256 = "sha256-CldVak1BQ4VhRt24hHdog5O3crkQBZBkRWNT7uYUw4Y=";
+  rev = "93a2d7db903a946d72cc590567e331c03e958926";
+  sha256 = "sha256-07A7OECoyy9N5vIlI9SyfRAAl4GnUEllu/Jggro9Tt4=";
 }
