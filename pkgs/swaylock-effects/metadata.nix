4ae3309f41c794fd6109e638eb7fafe5358e3889
Artturin <Artturin@artturin.com>
2023-12-06 21:45:56 +0200

    auto-update: swaylock-effects: 7c5681ce96587ce3090c6698501faeccdfdc157d => 4f2234c6bca3ce80dce1dbf7407825a38d58103c

--- pkgs/swaylock-effects/metadata.nix
+++ pkgs/swaylock-effects/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "swaylock-effects";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "7c5681ce96587ce3090c6698501faeccdfdc157d";
-  sha256 = "sha256-09Kq90wIIF9lPjiY2anf9MSgi/EqeXKXW1mFmhxA/aM=";
+  rev = "4f2234c6bca3ce80dce1dbf7407825a38d58103c";
+  sha256 = "sha256-cuFM+cbUmGfI1EZu7zOsQUj4rA4Uc4nUXcvIfttf9zE=";
 }
