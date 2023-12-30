1df757bc9cc0031a3e271103bc62e28e8b7e114b
Cole Botkens <cole.mickens+colebot@gmail.com>
2023-12-30 17:12:57 +0000

    auto-update: swaylock-effects: 4f2234c6bca3ce80dce1dbf7407825a38d58103c => 496059a8565c2d5eed672c2e5bc5e1edd14b3de8

--- pkgs/swaylock-effects/metadata.nix
+++ pkgs/swaylock-effects/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "swaylock-effects";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "4f2234c6bca3ce80dce1dbf7407825a38d58103c";
-  sha256 = "sha256-cuFM+cbUmGfI1EZu7zOsQUj4rA4Uc4nUXcvIfttf9zE=";
+  rev = "496059a8565c2d5eed672c2e5bc5e1edd14b3de8";
+  sha256 = "sha256-8RDaa7IvMpG3GK6wd0EPO+lLeQlr5ioVa5oyR9wct1U=";
 }
