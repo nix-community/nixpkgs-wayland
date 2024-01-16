1ce3b947cee23ec9287064eae5ea0609665cd669
Artturin <Artturin@artturin.com>
2024-01-16 21:55:45 +0200

    fix broken hash
    the old hash was generated with submodules enabled and now submodules
    are disabled

--- pkgs/wl-gammactl/metadata.nix
+++ pkgs/wl-gammactl/metadata.nix
@@ -5,5 +5,5 @@ rec {
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
   rev = "e2385950d97a3baf1b6e2f064dd419ccec179586";
-  sha256 = "sha256-4ve38pMGKDDC4oZZERyPdxVJX4RgHwYqX6IUQQ2GpGg=";
+  sha256 = "sha256-8iMJK4O/sNIGPOBZQEfK47K6OjT6sxYFe19O2r/VSr8=";
 }
