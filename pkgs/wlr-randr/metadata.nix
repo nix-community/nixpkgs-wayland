738efed4ac78e059692b4dac4ce3aa622f367ba0
Artturin <Artturin@artturin.com>
2024-01-19 22:05:18 +0200

    auto-update: wlr-randr: d1c54cdce9b4da013f12f62c0428785c35d21649 => fdde0d1d1ce940febb9b6ea6b2faa97f4e81a788

--- pkgs/wlr-randr/metadata.nix
+++ pkgs/wlr-randr/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wlr-randr";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "d1c54cdce9b4da013f12f62c0428785c35d21649";
-  sha256 = "sha256-wLXXM5fGryIh/PcO7Nh3uUOH7Zd+yKxCV5wzJGc8UBI=";
+  rev = "fdde0d1d1ce940febb9b6ea6b2faa97f4e81a788";
+  sha256 = "sha256-Pr9XEQUtbG3Mo/QjFMoY+oJTQIAVW+aaEjYHZqJgbbg=";
 }
