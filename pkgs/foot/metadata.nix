9f3a5132022f5d3b5fdf8033e883fa0395f56222
Artturin <Artturin@artturin.com>
2024-01-19 19:26:19 +0200

    foot: manual update
    to make patch to fix build apply

--- pkgs/foot/metadata.nix
+++ pkgs/foot/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "foot";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "14472cdbd953d5f6891f54098e5d706e45bb0c68";
-  sha256 = "sha256-dSjANBbJ7cCRKWpZNyaqPByqxsuCUPhyBPlOCr9j0Wk=";
+  rev = "9da7152f834384a354710fcc8f99a6f83e5dfc12";
+  sha256 = "sha256-vxDocS4Ay3FdSkN56Dpgj+ZD83/18tv6WJPl/WWUHQU=";
 }
