cacb88940d375c0143a6bc3dfd5fc69ddf9513e0
Artturin <Artturin@artturin.com>
2024-01-19 22:03:58 +0200

    auto-update: swaybg: eabc06e7eb9e5cb08c8177f49753dedc505076b4 => 80ed4b020adfb0846f780faba95fc5cc9a770a18

--- pkgs/swaybg/metadata.nix
+++ pkgs/swaybg/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "swaybg";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "eabc06e7eb9e5cb08c8177f49753dedc505076b4";
-  sha256 = "sha256-HLxK++MIIFzkOoHYOEoSn05/5zEhJkn+djCihSet4w8=";
+  rev = "80ed4b020adfb0846f780faba95fc5cc9a770a18";
+  sha256 = "sha256-zfggXlN8Zly0iH5t9kBbcQyL5+NsRbHyaovNt/O9pw4=";
 }
