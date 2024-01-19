c37047c25d752c816de45b749ecdb09414b74442
Artturin <Artturin@artturin.com>
2024-01-19 22:05:11 +0200

    auto-update: wl-gammarelay-rs: ca0bb9fde7d0e0fecfbc9c453f119b6460b3d85d => 8fa994e97e1038d5d5fd55183dffd6c75face5c5

--- pkgs/wl-gammarelay-rs/metadata.nix
+++ pkgs/wl-gammarelay-rs/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wl-gammarelay-rs";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "ca0bb9fde7d0e0fecfbc9c453f119b6460b3d85d";
-  sha256 = "sha256-lJXW3yMWXI0m6kSol6UHgXpdO1wUzZ931GZiOamapiU=";
+  rev = "8fa994e97e1038d5d5fd55183dffd6c75face5c5";
+  sha256 = "sha256-WTBnvpTzARKH7jsfvnXG8dvFzGDuTCv0t1i+zCVzklw=";
 }
