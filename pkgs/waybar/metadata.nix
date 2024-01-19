8e2b4a8af9a5f0edfecd2755fdd578a1d88d4a16
Artturin <Artturin@artturin.com>
2024-01-19 22:04:12 +0200

    auto-update: waybar: 3e55d0d7f29bc91bf6ad908bf72c1f0fcb93ed1e => 6e12f8122347ae279ae0fa1923acd6b908fa769c

--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "Waybar";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "3e55d0d7f29bc91bf6ad908bf72c1f0fcb93ed1e";
-  sha256 = "sha256-uHS5iY8wibwYFY5t50KPNALA+7yXhoImyFC6Ndm8WP8=";
+  rev = "6e12f8122347ae279ae0fa1923acd6b908fa769c";
+  sha256 = "sha256-YG/4LeOPtc0u/bLbFQ4yCyLSatrzPfE3a9X1+k8Ttpc=";
 }
