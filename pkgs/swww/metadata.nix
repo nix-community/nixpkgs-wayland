75f79ac32cc3ec8d37b8c2f08b17275f2487384f
Artturin <Artturin@artturin.com>
2024-01-19 22:04:06 +0200

    auto-update: swww: 0908f36050d545a0eb97ca0cbfc40c47fc50d6ba => a4c5bdbf08f6ff1839aa76f162f540b822cabca3

--- pkgs/swww/metadata.nix
+++ pkgs/swww/metadata.nix
@@ -3,6 +3,6 @@ rec {
   repo = "swww";
   repo_git = "https://github.com/${owner}/${repo}";
   branch = "main";
-  rev = "0908f36050d545a0eb97ca0cbfc40c47fc50d6ba";
-  sha256 = "sha256-NRmlctWwiUVlbB457y3e2BpWNyJ7CHpEnAYazwYQZpk=";
+  rev = "a4c5bdbf08f6ff1839aa76f162f540b822cabca3";
+  sha256 = "sha256-huJnElxtHGmNd2I3zeDClPgfhfFPtb2y99FzR9i9JPc=";
 }
