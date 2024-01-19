d93ff536c8fecffaf2dc09db3b14b2c8fcbcab7c
Artturin <Artturin@artturin.com>
2024-01-19 22:05:06 +0200

    auto-update: wayvnc: 42494fbbe46314f0c31653e5228b83b481252109 => d819ca477cdf18ac1bbd88bf018e241eb01eeb79

--- pkgs/wayvnc/metadata.nix
+++ pkgs/wayvnc/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wayvnc";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "42494fbbe46314f0c31653e5228b83b481252109";
-  sha256 = "sha256-2rjrtWuBdYci8lIHl1ZD5nB/pU5huD1ecHUdzUf3cpo=";
+  rev = "d819ca477cdf18ac1bbd88bf018e241eb01eeb79";
+  sha256 = "sha256-z/1A1xyAe1kVly7EIvDDrD1zINFj9u/hNLdO0CFDShE=";
 }
