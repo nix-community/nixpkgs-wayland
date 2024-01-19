a934b80e96bedd0a735d672546bd191816b62ba9
Artturin <Artturin@artturin.com>
2024-01-19 22:05:33 +0200

    auto-update: wofi: 0cec6f4edd14 => 0ffe3fbb234c

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wofi";
   repo_hg = "https://${domain}/${owner}/${repo}";
   branch = "default";
-  rev = "0cec6f4edd14";
-  sha256 = "sha256-xp2HI4XlRIRIy4xaxS42wofKzFcEouvbkEJCMrQWn3Q=";
+  rev = "0ffe3fbb234c";
+  sha256 = "sha256-UWk2HIOm0ew/d22T19RDDYxoEUGLQBzeFVnN/DQefIg=";
 }
