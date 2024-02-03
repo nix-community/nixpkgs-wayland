95f3e5493c410535b799138053936c76d8e32c25
Artturin <Artturin@artturin.com>
2024-02-03 15:04:44 +0200

    kanshi: skip

--- pkgs/kanshi/metadata.nix
+++ pkgs/kanshi/metadata.nix
@@ -7,4 +7,5 @@ rec {
   branch = "master";
   rev = "cec99a56a32b2d7f9eebcc729aa2e16d15150c15";
   sha256 = "sha256-AniC5BWtj2QzCc2dwLPg+QNQUsne45No7uF2oIJiTOs=";
+  skip = true; # skip until https://git.sr.ht/~emersion/scfg is in nixpkgs
 }
