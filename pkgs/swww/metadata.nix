7285c9cae868a1b4c27e679683ce206285bdc0a9
Artturin <Artturin@artturin.com>
2024-02-09 14:59:36 +0200

    Revert "swww: skip"
    This reverts commit 2f507fb7fab7110705e0a9ee006a21c4f809d00c.
    
    fixed upstream

--- pkgs/swww/metadata.nix
+++ pkgs/swww/metadata.nix
@@ -5,10 +5,4 @@ rec {
   branch = "main";
   rev = "a4c5bdbf08f6ff1839aa76f162f540b822cabca3";
   sha256 = "sha256-huJnElxtHGmNd2I3zeDClPgfhfFPtb2y99FzR9i9JPc=";
-  # error: failed to select a version for the requirement `utils = "^0.8"`
-  # candidate versions found which didn't match: 0.8.2-master
-  # https://github.com/LGFae/swww/commit/a4c5bdbf08f6ff1839aa76f162f540b822cabca3
-  # https://github.com/LGFae/swww/commit/a69934937b81dba41533c473f9ce55a4fc41fb31
-  # :/
-  skip = true;
 }
