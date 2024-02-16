03725aa94a37410d01469434ce58673faf44bd66
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-02-16 17:13:36 +0000

    auto-update: wlr-randr: 742fe8c15b1f1e09a3717c256b649b01900275e7 => fe23a76deeb24bc4e4156a89bbeecf3a54c4e779

--- pkgs/wlr-randr/metadata.nix
+++ pkgs/wlr-randr/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wlr-randr";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "742fe8c15b1f1e09a3717c256b649b01900275e7";
-  sha256 = "sha256-M1V/j81wTWbTwV5+qF9LxVg4m+qLzgURUqaq2IETfIA=";
+  rev = "fe23a76deeb24bc4e4156a89bbeecf3a54c4e779";
+  sha256 = "sha256-P6s49YHc969nhVgy/j8Pr5HSdjhvJJGHJRwezNsFSmI=";
 }
