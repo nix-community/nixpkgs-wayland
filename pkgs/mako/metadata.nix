586f575cb864d685afeb31f719bee2b05121e5a8
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-02-16 15:16:20 +0000

    auto-update: mako: 04f55f8236574fdf506ab3177637c2ff67c5b3fd => 683d8826f29f678bc4752323329da0b3d7a502cd

--- pkgs/mako/metadata.nix
+++ pkgs/mako/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "mako";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "04f55f8236574fdf506ab3177637c2ff67c5b3fd";
-  sha256 = "sha256-P4DBXEm062hVDOy3H5HjcdRP8FJ6miAtVegM258IyDQ=";
+  rev = "683d8826f29f678bc4752323329da0b3d7a502cd";
+  sha256 = "sha256-LIl+aqD54Dq/qcuPDaXv0wN+tsAcU9jE0qVz+G17pn0=";
 }
