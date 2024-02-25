760a22855f216562613e73a3a645849c9270b184
Artturin <Artturin@artturin.com>
2024-02-25 05:25:04 +0200

    imv: skip

--- pkgs/imv/metadata.nix
+++ pkgs/imv/metadata.nix
@@ -7,4 +7,5 @@ rec {
   branch = "master";
   rev = "885e17397ac503de84723d4f0b1c97b1258548ab";
   sha256 = "sha256-LLEEbriHzZhAOQivqHqdr6g7lh4uj++ytlme8AfRjf4=";
+  skip = true; # meson.build:1:0: ERROR: Unknown options: "libjxl"
 }
