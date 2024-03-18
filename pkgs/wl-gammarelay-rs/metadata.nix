350f952699ed7b53814507d9c01e1f1dc4368bb3
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-03-18 09:16:37 +0000

    auto-update: wl-gammarelay-rs: dea260331727f8f90953b11905a9e4e50350c3b1 => a5b97e7e53d6d548960d0364b627077ded748095

--- pkgs/wl-gammarelay-rs/metadata.nix
+++ pkgs/wl-gammarelay-rs/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wl-gammarelay-rs";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "dea260331727f8f90953b11905a9e4e50350c3b1";
-  sha256 = "sha256-pGicxBBL8KzRbB21QGX9GplRO/j923mxvj8mLMFT2ig=";
+  rev = "a5b97e7e53d6d548960d0364b627077ded748095";
+  sha256 = "sha256-EggRcdHuw1yM0GaOJbzVf5zaHOuOgSoj0lyudU/NGLg=";
 }
