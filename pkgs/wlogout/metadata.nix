e959f11c081532dfe7670717d3446d4dec310857
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-04-19 20:16:18 +0000

    auto-update: wlogout: bc76e5d2433d234cce0e923fd386429759ef8f7a => c56ce5bbdffe66cb1fe1032b03292e15c3e24bb7

--- pkgs/wlogout/metadata.nix
+++ pkgs/wlogout/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wlogout";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "bc76e5d2433d234cce0e923fd386429759ef8f7a";
-  sha256 = "sha256-5ikvElaMYdg7jtSsn26FH1SvhEUhqgw9J6V9y/aPzIc=";
+  rev = "c56ce5bbdffe66cb1fe1032b03292e15c3e24bb7";
+  sha256 = "sha256-L92akm9p40Tlg2s6iVKRDng8IZiewT3Bn6GMcdm2aYQ=";
 }
