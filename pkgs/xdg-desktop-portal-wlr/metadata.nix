69d24e77148692819ccb87c1592b2317ae6830e7
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-04-23 13:17:48 +0000

    auto-update: xdg-desktop-portal-wlr: 7444be5834128ea026490bddde1537744da2a7e0 => 9895411671642c14bcb47cc9ae843a5684745f85

--- pkgs/xdg-desktop-portal-wlr/metadata.nix
+++ pkgs/xdg-desktop-portal-wlr/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "xdg-desktop-portal-wlr";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "7444be5834128ea026490bddde1537744da2a7e0";
-  sha256 = "sha256-Ohd8DkbYrt5QIbJpGYqYjiQUDvUmfa3qDHiFOvbULIo=";
+  rev = "9895411671642c14bcb47cc9ae843a5684745f85";
+  sha256 = "sha256-4JTRCcDJsavXU+rvPyuzQ3B/UWBb/Y/2Mdx8at49DSc=";
 }
