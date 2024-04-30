860fbfcbe27995af681fa3f97ce5dcf7372b0133
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-04-30 07:16:00 +0000

    auto-update: wbg: 966553a9dbdeda56be5a1e5442c9893aed7fcf85 => dd36cce8c47bb0e17a789cf2bd95a51e29b59e78

--- pkgs/wbg/metadata.nix
+++ pkgs/wbg/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wbg";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "966553a9dbdeda56be5a1e5442c9893aed7fcf85";
-  sha256 = "sha256-hsYDqf4DHwhQPkMhPZt+0vZ+9wgFNfRLJ6YQ5+kzREg=";
+  rev = "dd36cce8c47bb0e17a789cf2bd95a51e29b59e78";
+  sha256 = "sha256-KLBvPL6wOl7l1uBQQNox3SE2uSXPxFQytzBfXwdHS1s=";
 }
