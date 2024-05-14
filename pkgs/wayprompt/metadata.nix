ab219ef7330fbd1477b387fab8ced055d6d01b10
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-05-14 10:19:35 +0000

    auto-update: wayprompt: 8f1de1d9dc5c61df3e6597b8d24bb90f20de1a7f => 10be4e07d6442c3f25127c7189fb13db7923d6c9

--- pkgs/wayprompt/metadata.nix
+++ pkgs/wayprompt/metadata.nix
@@ -5,7 +5,7 @@ rec {
   repo = "wayprompt";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "8f1de1d9dc5c61df3e6597b8d24bb90f20de1a7f";
-  sha256 = "sha256-rmRE+GG093QOXEkioGdhBkR3jp2EAmPMhOQfV4z87BU=";
+  rev = "10be4e07d6442c3f25127c7189fb13db7923d6c9";
+  sha256 = "sha256-TSFdDepU1mmjcBrQ87E0qm9iIsEE2nImBH0jS+EyDfg=";
   fetchSubmodules = true;
 }
