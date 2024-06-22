a65153f6a4f277095ca707f863876062d452f071
Cole Mickens <cole.mickens@gmail.com>
2024-06-22 14:01:46 -0700

    auto-update: swww: 1bb137becc6ad1a638f7fd9c2b9a50b017af03ed => 90e205ea17ed0c103cfbd0472499d361e8aa0603

--- pkgs/swww/metadata.nix
+++ pkgs/swww/metadata.nix
@@ -3,6 +3,6 @@ rec {
   repo = "swww";
   repo_git = "https://github.com/${owner}/${repo}";
   branch = "main";
-  rev = "1bb137becc6ad1a638f7fd9c2b9a50b017af03ed";
-  sha256 = "sha256-UHP6hKZuWUJIKqJZxPXKwmrzEfAah87db9GGDL8zKaY=";
+  rev = "90e205ea17ed0c103cfbd0472499d361e8aa0603";
+  sha256 = "sha256-o+Rq5TtuEWg0Lyj3TyZ/Npmn57j7QXfQ5GjVYVISEGI=";
 }
