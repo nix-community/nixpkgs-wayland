8cfbe8d82bc07e19f226fc0aedc8e77cc5df2a35
Cole Mickens <cole.mickens@gmail.com>
2024-08-02 09:01:54 -0700

    auto-update: foot: 85b2fb1e32d499520e5c33f2357d58d35bcb0b60 => 62b0b65d4712abd9ecd21ec2fe684c87a868bcc1

--- pkgs/foot/metadata.nix
+++ pkgs/foot/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "foot";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "85b2fb1e32d499520e5c33f2357d58d35bcb0b60";
-  sha256 = "sha256-tEHSZPWJtyUSRdg9s1VLimUnXFzut6FfpmFQktdYR8A=";
+  rev = "62b0b65d4712abd9ecd21ec2fe684c87a868bcc1";
+  sha256 = "sha256-afcW0GhCYC4zuOMg4//RpnLAnSeZkDs4ssKd676BBWk=";
 }
