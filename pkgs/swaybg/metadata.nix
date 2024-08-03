42cd3b92c0e75ed39e76f6487c38d4eab76754f2
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-08-03 17:15:26 +0000

    auto-update: swaybg: 469d9d4d77a91d41cda517ea0a5ecca3ffd6eb20 => 1a70f3ab566f6d939a1e342158e830a38dfe31ac

--- pkgs/swaybg/metadata.nix
+++ pkgs/swaybg/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "swaybg";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "469d9d4d77a91d41cda517ea0a5ecca3ffd6eb20";
-  sha256 = "sha256-dNiYeBcVnHc1eQGYRpfQCATfDUrrkZIi/v7L8YfiNZc=";
+  rev = "1a70f3ab566f6d939a1e342158e830a38dfe31ac";
+  sha256 = "sha256-hZW5ewDPT1evM3eb3TpHu27Idh0xxp547dLEE5Qs2L0=";
 }
