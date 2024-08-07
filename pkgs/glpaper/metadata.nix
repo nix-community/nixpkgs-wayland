a7c4cb560775732e176a52eb28f73699d09034cd
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-08-07 02:37:54 +0000

    auto-update: glpaper: 14c158c49545 => af9827d20bfe

--- pkgs/glpaper/metadata.nix
+++ pkgs/glpaper/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "glpaper";
   repo_hg = "https://${domain}/${owner}/${repo}";
   branch = "default";
-  rev = "14c158c49545";
-  sha256 = "sha256-jYOUmyXwTffzwUmYEY1SQx2ClZiRiMZLanveE9zWOLI=";
+  rev = "af9827d20bfe";
+  sha256 = "sha256-zgvnWqsw243jZ9e6fG6L0hDfRRHwzmIdsxwnnWhimu0=";
 }
