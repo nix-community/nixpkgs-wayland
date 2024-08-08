e28b410ad60004cca039fc6de2dd65f485779833
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-08-08 21:17:14 +0000

    auto-update: swww: 8d347f6e2875c7dae73d15acc21216cd9c81bc6d => 81ae44954ebea9fb301d4bea1b5fdde8da323fb4

--- pkgs/swww/metadata.nix
+++ pkgs/swww/metadata.nix
@@ -3,6 +3,6 @@ rec {
   repo = "swww";
   repo_git = "https://github.com/${owner}/${repo}";
   branch = "main";
-  rev = "8d347f6e2875c7dae73d15acc21216cd9c81bc6d";
-  sha256 = "sha256-MNuUXUAb18eNMn4ha6dGxeWJq+5aeRvkB8/4Ci0rFWw=";
+  rev = "81ae44954ebea9fb301d4bea1b5fdde8da323fb4";
+  sha256 = "sha256-8YSZa9FcbrHNSxwJFXAQtQquLIypUhy8tRrxh++uVBc=";
 }
