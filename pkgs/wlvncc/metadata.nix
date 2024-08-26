79089ec18cc9d36fe8bd312cda3e85af722d9d9b
Artturin <Artturin@artturin.com>
2024-08-26 18:43:57 +0300

    auto-update: wlvncc: 0c1308fbb1e73e3498252ce2238549330d050d04 => 174ad8aef7978c242861a66c481a7ca25dc04e37

--- pkgs/wlvncc/metadata.nix
+++ pkgs/wlvncc/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wlvncc";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "0c1308fbb1e73e3498252ce2238549330d050d04";
-  sha256 = "sha256-W/E4Y0ZobtWaE4tt4VkwT2PfpJ/vsVoBHSPlfJT2oQc=";
+  rev = "174ad8aef7978c242861a66c481a7ca25dc04e37";
+  sha256 = "sha256-9dKck3aQIOcnScmfKbSb0pRcnH7f433xF/WYBdyn3qI=";
 }
