e2e9d9d0126adf4a7c8f767ca7393832bdcd78ac
Artturin <Artturin@artturin.com>
2024-08-26 18:43:41 +0300

    auto-update: wbg: 74ecbee9035b71c685413e99eae3798c858a2a83 => 38417d8172f6c9201495f6388d6d5f6334b19e02

--- pkgs/wbg/metadata.nix
+++ pkgs/wbg/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wbg";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "74ecbee9035b71c685413e99eae3798c858a2a83";
-  sha256 = "sha256-N0iy1DllsMjFVZwTL9fPjcbs1VSCBYNva5yqcqAt8RA=";
+  rev = "38417d8172f6c9201495f6388d6d5f6334b19e02";
+  sha256 = "sha256-ikwOVtR5cXZGd2GE/O4ej6cOQZomyEKkPcKe08EtPw0=";
 }
