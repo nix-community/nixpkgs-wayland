0a7b881082b9a1f1b22c2ee783847e2648c6bf4a
Artturin <Artturin@artturin.com>
2024-08-26 18:42:35 +0300

    auto-update: foot: cee0c5423ae1b306fc85960058adfd9574e4c91b => 01fa59b6b71cc756366905e6ef99cfd42944a292

--- pkgs/foot/metadata.nix
+++ pkgs/foot/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "foot";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "cee0c5423ae1b306fc85960058adfd9574e4c91b";
-  sha256 = "sha256-1C6jwmWsN0ac2B6Sh+TeThEPiIuMp4Fdfbqc1FEJCAA=";
+  rev = "01fa59b6b71cc756366905e6ef99cfd42944a292";
+  sha256 = "sha256-Lbow87UkXRWYpAGsB6KRS4AcRLF6TOE3XURzHuKZJKs=";
 }
