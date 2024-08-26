b9d97d3b13f4e5848f802d3f301c8e49af6980f2
Artturin <Artturin@artturin.com>
2024-08-26 18:42:33 +0300

    auto-update: eww: 4d55e9ad63d1fae887726dffcd25a32def23d34f => 057297b4a339a4f72a6adf4ab587ebde2c331fcb

--- pkgs/eww/metadata.nix
+++ pkgs/eww/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "eww";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "4d55e9ad63d1fae887726dffcd25a32def23d34f";
-  sha256 = "sha256-LTSFlW/46hl1u9SzqnvbtNxswCW05bhwOY6CzVEJC5o=";
+  rev = "057297b4a339a4f72a6adf4ab587ebde2c331fcb";
+  sha256 = "sha256-SMEo89FKzwXmQZAtiUwMmNNetwVsHI2FclkRhcpt170=";
 }
