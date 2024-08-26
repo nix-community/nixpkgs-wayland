4abda53b1a1542eb4b42a376bff2dbe6fe491f62
Artturin <Artturin@artturin.com>
2024-08-26 18:43:10 +0300

    auto-update: new-wayland-protocols: f4925c9313d26689918c1d1a138ec4819caeb77c => d1d185c61f68c9528a7559ff88d97368ce4cc077

--- pkgs/new-wayland-protocols/metadata.nix
+++ pkgs/new-wayland-protocols/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wayland-protocols";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "f4925c9313d26689918c1d1a138ec4819caeb77c";
-  sha256 = "sha256-URMkmlawIM2DLzEN8BfltnKCPjjec9CoxbyVWUT5MpA=";
+  rev = "d1d185c61f68c9528a7559ff88d97368ce4cc077";
+  sha256 = "sha256-kdfKQHxr1VOGMLmZD7BVPaoIM3KCrACLti0W1Gqpm08=";
 }
