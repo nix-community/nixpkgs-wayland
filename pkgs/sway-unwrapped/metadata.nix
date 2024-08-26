4ff4288fc0f406e97bfbd84a5a635af1ffecdc47
Artturin <Artturin@artturin.com>
2024-08-26 18:43:24 +0300

    auto-update: sway-unwrapped: b44015578a3d53cdd9436850202d4405696c1f52 => 77b9ddabe2a97c5d04c30929b0f8cbde3470fdd7

--- pkgs/sway-unwrapped/metadata.nix
+++ pkgs/sway-unwrapped/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "sway";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "b44015578a3d53cdd9436850202d4405696c1f52";
-  sha256 = "sha256-gTsZWtvyEMMgR4vj7Ef+nb+wcXkwGivGfnhnBIfPHOA=";
+  rev = "77b9ddabe2a97c5d04c30929b0f8cbde3470fdd7";
+  sha256 = "sha256-eHztQ+WODK4rB88pB3fyS54nmV5rixYQvNu+HA3JhvY=";
 }
