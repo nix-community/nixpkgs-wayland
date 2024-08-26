2bb02c493ee15c9757824edeafd4252721dec409
Artturin <Artturin@artturin.com>
2024-08-26 18:43:45 +0300

    auto-update: wf-recorder: 9978c3570436eb5358e4578421d83a7f3ffd2683 => a5485cf730cd9a4f64f2be18da65c7653f0fa37e

--- pkgs/wf-recorder/metadata.nix
+++ pkgs/wf-recorder/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wf-recorder";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "9978c3570436eb5358e4578421d83a7f3ffd2683";
-  sha256 = "sha256-Fo+F1sxVJSDun5/5L+4Gcj2Lvyui6TC0qekN4hvz2Jw=";
+  rev = "a5485cf730cd9a4f64f2be18da65c7653f0fa37e";
+  sha256 = "sha256-7/fQOkfAw5v3irD5blJOdq88j0VBrPVQQufdt9wsACk=";
 }
