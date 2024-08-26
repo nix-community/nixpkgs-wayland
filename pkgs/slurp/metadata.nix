8657eea67fd8db033f0e031ff62edb4edbc5dd60
Artturin <Artturin@artturin.com>
2024-08-26 18:43:17 +0300

    auto-update: slurp: 1d60b20db08f9262427159da51d02b5523085247 => 7a91fe68a67d37b98a904f41f535549a223efccb

--- pkgs/slurp/metadata.nix
+++ pkgs/slurp/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "slurp";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "1d60b20db08f9262427159da51d02b5523085247";
-  sha256 = "sha256-Cpkq/6lfMh1+VAh1woTbuC+7fVlSXUsH0gx02yWb6j8=";
+  rev = "7a91fe68a67d37b98a904f41f535549a223efccb";
+  sha256 = "sha256-UPAjvyOjBdAGsMM3YD2JdqlYIfWc8/o8gY58C7FuuoA=";
 }
