b8136c8b0958c4b7401a97c94983b03e87f812a9
Artturin <Artturin@artturin.com>
2024-08-26 18:42:54 +0300

    auto-update: i3status-rust: 04188c2ab8c5bae4315effea3ebb3cc3f154f753 => 433d2e0fbd9bfcb6de02c84f0eed3a24345d6181

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "i3status-rust";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "04188c2ab8c5bae4315effea3ebb3cc3f154f753";
-  sha256 = "sha256-+/dORZ5MDpOnDWCV/l7t9XpEzk0lehLme5UWtUsq6Z0=";
+  rev = "433d2e0fbd9bfcb6de02c84f0eed3a24345d6181";
+  sha256 = "sha256-X+4iLfiiT67yxuLrUMeCnNmGrsyH+dnKzIfKiVunl90=";
 }
