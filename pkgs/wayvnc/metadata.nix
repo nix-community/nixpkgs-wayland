048313828f04289121f9c5e5e4ab042be74db77f
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-09-07 21:18:36 +0000

    auto-update: wayvnc: 3cff6388b34c0d49bd370d85a28bf9ec23b49f7d => 5d67b9d72e178e8f850135a8a5aa9cd982102ff3

--- pkgs/wayvnc/metadata.nix
+++ pkgs/wayvnc/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wayvnc";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "3cff6388b34c0d49bd370d85a28bf9ec23b49f7d";
-  sha256 = "sha256-yw1ewXIXakQFrHWwekcjrDRW2sxIG82Ujav+6GCZtMI=";
+  rev = "5d67b9d72e178e8f850135a8a5aa9cd982102ff3";
+  sha256 = "sha256-ORtNnfCiJN9rcjUQ4DnpwYywLLJ6m2VYrssUoAjge4w=";
 }
