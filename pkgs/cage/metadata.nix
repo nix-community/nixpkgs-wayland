365b198d307763aa9f8b9f64a132ea64e2ea6ade
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-10-12 18:23:22 +0000

    auto-update: cage: f0651c7671afcf2d0f709c2e04f208dd5f3179f4 => e128a9f2511644529fa47701a7147d65d9920488

--- pkgs/cage/metadata.nix
+++ pkgs/cage/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "cage";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "f0651c7671afcf2d0f709c2e04f208dd5f3179f4";
-  sha256 = "sha256-n0uVIGRDm1SWoAFCIdaPSnWx0Ca7PbyYm7g9NKlorPg=";
+  rev = "e128a9f2511644529fa47701a7147d65d9920488";
+  sha256 = "sha256-2SFtz62z0EF8cpFTC6wGi125MD4a5mkXqP/C+7fH+3g=";
 }
