af2a275f50916c163d66179bb273db7863bb54db
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-11-07 22:35:34 +0000

    auto-update: new-wayland-protocols: 9dd051b4a0c6df82937d57d38ac7df389f16e6fa => fe3d5448be6cdefc49746c97c9361c1750ae89f6

--- pkgs/new-wayland-protocols/metadata.nix
+++ pkgs/new-wayland-protocols/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wayland-protocols";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "9dd051b4a0c6df82937d57d38ac7df389f16e6fa";
-  sha256 = "sha256-Jwlrw2B75eLu8tMsMY6kMz/aXD4dgbRZM+XRuyzriBM=";
+  rev = "fe3d5448be6cdefc49746c97c9361c1750ae89f6";
+  sha256 = "sha256-YcmueNNzBsWAnMs378f57F7KA1F/GtvB1v/cF4NBisQ=";
 }
