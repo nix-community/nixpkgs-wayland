8599ac4849febda560cb82e6359eeeac657cd3c7
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-12-04 18:30:13 +0000

    auto-update: waybar: 20ca48c3b863a8b11dc149161a3e6e33ce97eea5 => 3abac0abfd316a54772ae1146a28563bc4f47838

--- pkgs/waybar/metadata.nix
+++ pkgs/waybar/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "Waybar";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "20ca48c3b863a8b11dc149161a3e6e33ce97eea5";
-  sha256 = "sha256-5uRSlSdsfCudEFanhXJ8cRV+U18QZdu2cWYjIzeOvEs=";
+  rev = "3abac0abfd316a54772ae1146a28563bc4f47838";
+  sha256 = "sha256-8Yd1TokY0ZwJp4PZYU8KoxhFHUL1FddihPXWPQCwjiw=";
 }
