8dfb2712e7a1050ff1af5b02445b8d851cbfb3a7
Artturin <Artturin@artturin.com>
2024-12-04 20:06:27 +0200

    waypipe: skip

--- pkgs/waypipe/metadata.nix
+++ pkgs/waypipe/metadata.nix
@@ -7,4 +7,8 @@ rec {
   branch = "master";
   rev = "a04f6e3573f19ec7d7a7ef74b3fd1ee52400a2f7";
   sha256 = "sha256-WuPiXYzOCrkOX7Yf++70IXXQ5f4Sx0GjJsiuCktSz5E=";
+  # Porting to rust, let's stay on pre-rust for a while
+  # 2024-12-04
+  # https://gitlab.freedesktop.org/mstoeckl/waypipe/-/issues/107
+  skip = true;
 }
