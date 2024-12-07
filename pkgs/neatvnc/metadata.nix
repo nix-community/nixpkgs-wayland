98e4d443cc6cc78659a33074aae4dee9e4b37bd7
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-12-07 21:18:56 +0000

    auto-update: neatvnc: 51fa2b9901065fc4698ea4fd1184e1c5ee798f1d => 784a58315375947b8a0ec111bdde5088664dbca5

--- pkgs/neatvnc/metadata.nix
+++ pkgs/neatvnc/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "neatvnc";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "51fa2b9901065fc4698ea4fd1184e1c5ee798f1d";
-  sha256 = "sha256-yvrB87Kvk5131snCcY5i2ZF0BrJGjo5yNKEXj/jciCg=";
+  rev = "784a58315375947b8a0ec111bdde5088664dbca5";
+  sha256 = "sha256-1yjuir5NvOZte1s9pHOa+FKAaSY6IPQbRX61JuzLI44=";
 }
