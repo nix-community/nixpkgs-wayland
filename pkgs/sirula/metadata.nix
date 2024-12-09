5243a2987992a49c0d01515830430bbc45356ce4
Cole Botkens <cole.mickens+colebot@gmail.com>
2024-12-09 09:37:48 +0000

    auto-update: sirula: f76baeca529f9c9d6d1b4c89231b84b3662b656d => 69e757a523e8f4b26cc062e6addaa2dfcde180e0

--- pkgs/sirula/metadata.nix
+++ pkgs/sirula/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "sirula";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "f76baeca529f9c9d6d1b4c89231b84b3662b656d";
-  sha256 = "sha256-/SrC22MsVpY4gWMCmjFv8swYWC+DISQNc8+HgqTj8EE=";
+  rev = "69e757a523e8f4b26cc062e6addaa2dfcde180e0";
+  sha256 = "sha256-FX8hUmisejc7cHYMqSQLgIS80LiW2441/+gybf1nFkc=";
 }
