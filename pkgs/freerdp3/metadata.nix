8988b0603fed7e3d303718328cf4cd8dd137575e
Artturin <Artturin@artturin.com>
2024-12-22 21:33:31 +0200

    freerdp3: skip
    https://www.github.com/nix-community/nixpkgs-wayland/actions/runs/12456979244/job/34771036765

--- pkgs/freerdp3/metadata.nix
+++ pkgs/freerdp3/metadata.nix
@@ -6,4 +6,6 @@ rec {
   branch = "master";
   rev = "6111ac458c041e7d21d88910deb2f061317a743f";
   sha256 = "sha256-yOkJ1uT8CwnhsjAR2E9nD2F+FXFm3ZbZFVY4913rVI4=";
+  # Can't find SDL2 started in https://github.com/FreeRDP/FreeRDP/pull/10996 probably
+  skip = true;
 }
