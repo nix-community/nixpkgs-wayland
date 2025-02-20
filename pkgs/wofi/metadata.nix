3f3e0db2714274421b7ece1456dde76f53c010ce
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-02-20 13:02:18 +0000

    auto-update: wofi: d2f7f7f39ee4 => 572fa76abb6f

--- pkgs/wofi/metadata.nix
+++ pkgs/wofi/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wofi";
   repo_hg = "https://${domain}/${owner}/${repo}";
   branch = "default";
-  rev = "d2f7f7f39ee4";
-  sha256 = "sha256-QpWEPvVYZFAv1RKiDmAxsxLcOoAXQuuAlGbkVQmjXbI=";
+  rev = "572fa76abb6f";
+  sha256 = "sha256-LoToG+TuWOisul0Vgw6ENRSGI0rNqkUZXvmKaHJ9tfU=";
 }
