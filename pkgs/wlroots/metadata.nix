60536811e223f6d76e8e264885bb8e60bdc1ee1f
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-03-11 02:23:29 +0000

    auto-update: wlroots: 31f9d6bb97b61bb9cc0e185293a6770b90a14b7b => 50edd3a42dc5e6a7e225642ad192e5ed20bc7103

--- pkgs/wlroots/metadata.nix
+++ pkgs/wlroots/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wlroots";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "31f9d6bb97b61bb9cc0e185293a6770b90a14b7b";
-  sha256 = "sha256-+KJwD41mWD5/f17CjCQu+upGxQbUeEmNGW/H5EtDNv8=";
+  rev = "50edd3a42dc5e6a7e225642ad192e5ed20bc7103";
+  sha256 = "sha256-upreynSPecSY82eop+Vp9waWvibUwOLdcMbYcj6a7eY=";
 }
