2a300c12e53cc93a4c8b06e29407793f87b2c928
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-03-27 02:34:31 +0000

    auto-update: gtk-layer-shell: 5e2adead20e75bd642d3bab66e836a30422afd7b => 2898502a588d2db01c7c691ec03e7a6649989545

--- pkgs/gtk-layer-shell/metadata.nix
+++ pkgs/gtk-layer-shell/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "gtk-layer-shell";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "5e2adead20e75bd642d3bab66e836a30422afd7b";
-  sha256 = "sha256-J+Gx0LLl3HfxZzGxQ/lL3a6meibuiEzNH+APCzOaAPo=";
+  rev = "2898502a588d2db01c7c691ec03e7a6649989545";
+  sha256 = "sha256-9/pd4odCtFoIlJECHAHcpzdth1/ustaYHh7Nu4YJymo=";
 }
