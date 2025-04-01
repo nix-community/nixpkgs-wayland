d54e6755897779246c9ac5a79530737b110a5288
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-04-01 02:53:47 +0000

    auto-update: aml: c9d49a04612fc71fc634f1aa114ae02a679417c6 => f6086bca136ddad96d67121ecfece59899b91f6a

--- pkgs/aml/metadata.nix
+++ pkgs/aml/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "aml";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "c9d49a04612fc71fc634f1aa114ae02a679417c6";
-  sha256 = "sha256-9R6CvWtwpWVOdvfn9CWUIJtQFPPBf4OKuuha8orz1J0=";
+  rev = "f6086bca136ddad96d67121ecfece59899b91f6a";
+  sha256 = "sha256-sdAf1cAzIopRvK7wAXdiMo/fxwvBvYq5DGPEklofsbs=";
 }
