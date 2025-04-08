d9de146fe5c0ea2b9649c8dd4edffa455a15ebbe
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-04-08 13:30:01 +0000

    auto-update: wldash: 456d61de018b83357e9d9314ec08f230df8bf6a8 => d80a64444f9a963472e18d159af5d4f6af8d4431

--- pkgs/wldash/metadata.nix
+++ pkgs/wldash/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wldash";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "456d61de018b83357e9d9314ec08f230df8bf6a8";
-  sha256 = "sha256-1vGIJS4lJ6v8xm0q1Ko3WqtztewaKU0MbCRjRjyRmPQ=";
+  rev = "d80a64444f9a963472e18d159af5d4f6af8d4431";
+  sha256 = "sha256-a6v3JGx+8jjkxRxGVyrWe5c/5AHV7akgo4T79ZKbUvE=";
 }
