7fd78aaf58c52877946e477c6947a5fb59411de7
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-05-19 13:34:25 +0000

    auto-update: wev: 881979ca6544cd6570cd5d7442426c953012053f => faaada287d517fefe604b367a888b1cee30258c4

--- pkgs/wev/metadata.nix
+++ pkgs/wev/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "wev";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "881979ca6544cd6570cd5d7442426c953012053f";
-  sha256 = "sha256-A+32t2u2U9lqLllpvoEzj3mZKHnSLotyuHtyfDS5jK8=";
+  rev = "faaada287d517fefe604b367a888b1cee30258c4";
+  sha256 = "sha256-yECpYd9xWd9Xa+BpyFql9xYu4arknDdzhRrk5SSqmmQ=";
 }
