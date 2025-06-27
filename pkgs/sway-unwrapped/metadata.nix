1415668066397e878ea8772af45726076012b78f
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-06-27 02:56:18 +0000

    auto-update: sway-unwrapped: c2f08075ec00632293bbc63582c7f3ffd75441af => 3d6b9a28480a398e3af869d4051181f98a042022

--- pkgs/sway-unwrapped/metadata.nix
+++ pkgs/sway-unwrapped/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "sway";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "c2f08075ec00632293bbc63582c7f3ffd75441af";
-  sha256 = "sha256-kwDv9TP3oIxfk7x8zRFzSiecTKPqZwDg+AA9cdeDXCg=";
+  rev = "3d6b9a28480a398e3af869d4051181f98a042022";
+  sha256 = "sha256-Vq8wb+MqIpevbPwhvdEpwWB3XOth2VCnbEUBgUt5sBQ=";
 }
