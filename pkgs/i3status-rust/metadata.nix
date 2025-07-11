9450f0d05d56d9a8799fbe886b019c077dc8f541
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-07-11 13:40:03 +0000

    auto-update: i3status-rust: db4db28eda9bbf8b7fc61ddad65a00207be9368b => 0db54fd8668422bc371e5c2cbf4f2a81a604f1c4

--- pkgs/i3status-rust/metadata.nix
+++ pkgs/i3status-rust/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "i3status-rust";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "db4db28eda9bbf8b7fc61ddad65a00207be9368b";
-  sha256 = "sha256-E0HGF7oyffBWUT61fQZ+tjwDi7q9IhtV6DiF8TGeVsU=";
+  rev = "0db54fd8668422bc371e5c2cbf4f2a81a604f1c4";
+  sha256 = "sha256-lCJKfUMXCr5SwI4E+RS5QWvVP4YajEBDfJA4NrwH9RI=";
 }
