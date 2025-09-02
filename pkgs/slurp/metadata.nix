4b124253027502d411d1dd6534b19ab9f8c4ddc1
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-09-02 13:09:30 +0000

    auto-update: slurp: e4df2d2885de677332559d33dc9117891745c26d => 6460063cf4ceca2d85d4df1bd05b57c44c806a49

--- pkgs/slurp/metadata.nix
+++ pkgs/slurp/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "slurp";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "e4df2d2885de677332559d33dc9117891745c26d";
-  sha256 = "sha256-AcW6gcGdQ5XiVgXMnxlX6Mbe72ycdwM8bbQLDWwi+oc=";
+  rev = "6460063cf4ceca2d85d4df1bd05b57c44c806a49";
+  sha256 = "sha256-6F2zbgvTvmCarBRk9M8yhWa/QRmI6jCEUl35BmRawp8=";
 }
