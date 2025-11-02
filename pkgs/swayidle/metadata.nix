1433eaf52cfedc3bc6240bdb249892ff9543b6c3
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-11-02 02:26:01 +0000

    auto-update: swayidle: afcce4303d447f12b20e035fcb9ff09b4a0b948b => 95cf9efc20c8be7ce6206883097e169c843550a2

--- pkgs/swayidle/metadata.nix
+++ pkgs/swayidle/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "swayidle";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "afcce4303d447f12b20e035fcb9ff09b4a0b948b";
-  sha256 = "sha256-HdboDtyG6zBDJZpwalenB6doKdVobLW6PF7m66kEATA=";
+  rev = "95cf9efc20c8be7ce6206883097e169c843550a2";
+  sha256 = "sha256-fVYE2KJJt0x1cwFxlw17qnQj3BmP5gUThV+HtEFldw8=";
 }
