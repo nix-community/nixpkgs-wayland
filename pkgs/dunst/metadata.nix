bde4be1c03cabe6bea88ce9fe10ad6b0dd1fd1a0
Cole Botkens <cole.mickens+colebot@gmail.com>
2025-11-22 02:15:02 +0000

    auto-update: dunst: 1f505921a77936bef9865e465216c5da9052bf2f => e2593d337de639d5015806ad6f68261c74a2873a

--- pkgs/dunst/metadata.nix
+++ pkgs/dunst/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "dunst";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "1f505921a77936bef9865e465216c5da9052bf2f";
-  sha256 = "sha256-zO+ZDFpadf6Mn4cUKewZGLrOj5LOME+R1gb3+jVGQz0=";
+  rev = "e2593d337de639d5015806ad6f68261c74a2873a";
+  sha256 = "sha256-73l0ugji41AZOEBf8M+wiJtgRUi2wMYd6CtsjpDwaKw=";
 }
