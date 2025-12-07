417f4e01b2542e4b15944d4bf672e2a2deb8070d
Artturin <Artturin@artturin.com>
2025-12-07 16:39:29 +0200

    auto-update: imv: efc05208f04ac10f99e1b24cce5df9e36e48baa8 => 5de5c687e12917a27ea1dc4dbb6ddcbd852e92bf

--- pkgs/imv/metadata.nix
+++ pkgs/imv/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "imv";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "efc05208f04ac10f99e1b24cce5df9e36e48baa8";
-  sha256 = "sha256-2NPuNswX1SAIjqP+xFa0Jbwj/nlZHIfjdGt7vQowjj0=";
+  rev = "5de5c687e12917a27ea1dc4dbb6ddcbd852e92bf";
+  sha256 = "sha256-PFTVWK0uFRsvQz2JsaWjgeOPIzCVSCAwWJfXvnVnVOE=";
 }
