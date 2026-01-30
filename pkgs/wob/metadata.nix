0931043110c536957742e67379e7004e1664885d
Cole Botkens <cole.mickens+colebot@gmail.com>
2026-01-30 03:14:52 +0000

    auto-update: wob: 6958b155da0b10800c09fd23f4dca0ffb65cafd2 => b712016926e8a9f803e0de4d48a7412dc9374538

--- pkgs/wob/metadata.nix
+++ pkgs/wob/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wob";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "6958b155da0b10800c09fd23f4dca0ffb65cafd2";
-  sha256 = "sha256-5NgHBj8K0IO+8mogRCiIQbE788W1b3/mXO8c28VefAM=";
+  rev = "b712016926e8a9f803e0de4d48a7412dc9374538";
+  sha256 = "sha256-nPjijvGEQRGceuKt14YgJWpc1XUDxHqdi+P44Ywi3PU=";
 }
