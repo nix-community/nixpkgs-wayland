0bb5e785ef9ff7743304b13ffd066c7f0f5e7a04
Cole Botkens <cole.mickens+colebot@gmail.com>
2026-03-23 03:34:52 +0000

    auto-update: wl-clipboard: a783546b9453d02f9acbd2cb34d96d2f90b12bd2 => 67a7b937895bceec1ae5ccebb10216f63f70ca1b

--- pkgs/wl-clipboard/metadata.nix
+++ pkgs/wl-clipboard/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wl-clipboard";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "a783546b9453d02f9acbd2cb34d96d2f90b12bd2";
-  sha256 = "sha256-dT3sC9ymW1ceF0HMCe7Bv5QwZ4HZJKPeGVfqAPhNpKs=";
+  rev = "67a7b937895bceec1ae5ccebb10216f63f70ca1b";
+  sha256 = "sha256-c/EfjrA4H/MiedSVWLN6ZUipxwcsmBueeYJu5b09MGc=";
 }
