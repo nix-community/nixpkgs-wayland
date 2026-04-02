52e1cf3a0a42c2eb8f6be3af0bb7ddedf5d2c7a3
Cole Mickens <cole.mickens@gmail.com>
2026-04-02 14:54:35 -0300

    tree: swww->awww, replace source for awww; auto-update: awww: 8a3b1bf9f3363683097aa5467d3058cc2278da1a => 7a8fc2e646b97e5ae508a44d3449e3b41345d456

--- pkgs/swww/metadata.nix
+++
@@ -1,8 +0,0 @@
-rec {
-  owner = "Horus645";
-  repo = "swww";
-  repo_git = "https://github.com/${owner}/${repo}";
-  branch = "main";
-  rev = "8a3b1bf9f3363683097aa5467d3058cc2278da1a";
-  sha256 = "sha256-sw6bxF9PMEedVHvSxwkOVo9zSo45dt+2MT2fy6pnGJY=";
-}
