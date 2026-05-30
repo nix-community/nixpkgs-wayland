1bb88f0a732198b768d18367411347dc7330f4a3
Artturin <Artturin@artturin.com>
2026-05-30 17:48:46 +0300

    neatvnc: Skip

--- pkgs/neatvnc/metadata.nix
+++ pkgs/neatvnc/metadata.nix
@@ -6,4 +6,6 @@ rec {
   branch = "master";
   rev = "57cc64b3871c429800e25ed5724b1b2c7a04e134";
   sha256 = "sha256-yEWNiazRxc8G7ToqOcTtCXEuBCgXO64v31Xx1YeOPCM=";
+  # Until nettle 4.0+ is in nixpkgs
+  skip = true;
 }
