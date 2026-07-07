fcbe0c190d06988c99148d81fe8092268f6229f1
Cole Botkens <cole.mickens+colebot@gmail.com>
2026-07-07 15:29:30 +0000

    auto-update: eww: 865cf631d5bbb5f9fccc99b3f4cc80b9eeada18c => 4ded06345553e4c2bcf8179fc7438e53c5e54d89

--- pkgs/eww/metadata.nix
+++ pkgs/eww/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "eww";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "865cf631d5bbb5f9fccc99b3f4cc80b9eeada18c";
-  sha256 = "sha256-fL12XFMsf/efSlbzQc7cCI366CwETkM6sWpEfcF9s6A=";
+  rev = "4ded06345553e4c2bcf8179fc7438e53c5e54d89";
+  sha256 = "sha256-fnI8XgBowMxs/j9FGf+pQI2U8YjN8pTRqkVmFm8PBHE=";
 }
