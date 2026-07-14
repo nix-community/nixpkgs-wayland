0167888c6dc2e6990916321a6919054efb5d7954
Cole Botkens <cole.mickens+colebot@gmail.com>
2026-07-14 14:26:39 +0000

    auto-update: awww: 064e48aa7be551a9d29d7db0287f086a40903bde => 25ea4fd7a42359379da9ddadedda1c477caa4ae0

--- pkgs/awww/metadata.nix
+++ pkgs/awww/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "awww";
   repo_git = "https://codeberg.org/${owner}/${repo}";
   branch = "main";
-  rev = "064e48aa7be551a9d29d7db0287f086a40903bde";
-  sha256 = "sha256-Fg7jz+6l17qnNT9FsLwexBEdixZuEp+2CDE3JRCGyFo=";
+  rev = "25ea4fd7a42359379da9ddadedda1c477caa4ae0";
+  sha256 = "sha256-MPMVSBhSEl+ShSrWVN5qH3FiX2gg8LbYA8L3nuQw5VE=";
 }
