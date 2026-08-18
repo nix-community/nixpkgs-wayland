06dc890bd301f48e3b7fd7a3ef76ef4fd17911ad
Artturin <Artturin@artturin.com>
2026-08-18 17:40:05 +0300

    auto-update: waypipe: 1ac039b4d50e2658d284e750c182266cc00efe74 => dcedd03779982d5b5524b48d7a16602c0f497d76

--- pkgs/waypipe/metadata.nix
+++ pkgs/waypipe/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "waypipe";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "1ac039b4d50e2658d284e750c182266cc00efe74";
-  sha256 = "sha256-rSTphq/ZJItyp3DTcZyHxD8LvdA0FKCCaA0lw0TXQeA=";
+  rev = "dcedd03779982d5b5524b48d7a16602c0f497d76";
+  sha256 = "sha256-CQgDQJudxtUc7unORE/yAa+poopiceS/a4AMOfcsKP8=";
 }
