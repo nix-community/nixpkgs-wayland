56364676b4c3c64c085840d5512ab6a97d1fa02e
Artturin <Artturin@artturin.com>
2026-08-18 17:39:56 +0300

    auto-update: shotman: 00cc4a8e3e3d95411714c2f0ab80656a1e1efa7b => 8bca85dda785a51180969dac180baad5a52e69d3

--- pkgs/shotman/metadata.nix
+++ pkgs/shotman/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "shotman";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "main";
-  rev = "00cc4a8e3e3d95411714c2f0ab80656a1e1efa7b";
-  sha256 = "sha256-GzRkCBF6OjXLIi4j0thrVK6Ju0Q4TucCz97Tcoyk8P8=";
+  rev = "8bca85dda785a51180969dac180baad5a52e69d3";
+  sha256 = "sha256-ki54qZBaUl6W/YUEooS8p2HqXWDmOBEXoOLAg50soJU=";
 }
