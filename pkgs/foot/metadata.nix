cbb387df12452cfe573a14e980d887015549e29b
Artturin <Artturin@artturin.com>
2026-08-18 17:37:01 +0300

    auto-update: foot: 8db88cceb758b5be23e7db1fe74a48102ab07dc0 => 766e227e4fc343d8f2b9b7ee228b2cc7cfc60ee7

--- pkgs/foot/metadata.nix
+++ pkgs/foot/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "foot";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "8db88cceb758b5be23e7db1fe74a48102ab07dc0";
-  sha256 = "sha256-qtH5kL6u8ZxQa9ltDT+03+Q7bv5v3QCv9dx8m5SlVYE=";
+  rev = "766e227e4fc343d8f2b9b7ee228b2cc7cfc60ee7";
+  sha256 = "sha256-h1GD7ObYbIvhevh2EBgwJXsKvamqpX/OrnZnpl3EFFQ=";
 }
