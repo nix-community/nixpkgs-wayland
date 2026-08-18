8103bdc62b116a4fca84a321377cd01b98e6ecfa
Artturin <Artturin@artturin.com>
2026-08-18 17:40:01 +0300

    auto-update: swayidle: f54c11ac01916acbe1c7bbab50bfea22781efbcb => f089aacc03dba5aca16959ca45087990c9722097

--- pkgs/swayidle/metadata.nix
+++ pkgs/swayidle/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "swayidle";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "f54c11ac01916acbe1c7bbab50bfea22781efbcb";
-  sha256 = "sha256-y3020Ri4YMdk90L1pKhdML8F9/B4ji71H8ePZrqAGLw=";
+  rev = "f089aacc03dba5aca16959ca45087990c9722097";
+  sha256 = "sha256-RHpH7Jx1/AO85ilZzCP9e+DidfZiFWdj2AMhWHrkbXY=";
 }
