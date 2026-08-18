ece8f0a2ce7ac6865785e637b493b66681a2e621
Artturin <Artturin@artturin.com>
2026-08-18 17:40:12 +0300

    auto-update: wl-clipboard: 67a7b937895bceec1ae5ccebb10216f63f70ca1b => 16cf9d3fcccff525574364bf6ddc676352f49e92

--- pkgs/wl-clipboard/metadata.nix
+++ pkgs/wl-clipboard/metadata.nix
@@ -4,6 +4,6 @@ rec {
   repo = "wl-clipboard";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "67a7b937895bceec1ae5ccebb10216f63f70ca1b";
-  sha256 = "sha256-c/EfjrA4H/MiedSVWLN6ZUipxwcsmBueeYJu5b09MGc=";
+  rev = "16cf9d3fcccff525574364bf6ddc676352f49e92";
+  sha256 = "sha256-ZRcveeHv6445EIjSHpuNAq8j8L42pKqrp3aNh6B8t7s=";
 }
