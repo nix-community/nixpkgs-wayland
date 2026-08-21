e435a68c959816d3753a844ffc2c80a598c3afe1
Cole Botkens <cole.mickens+colebot@gmail.com>
2026-08-21 13:28:27 +0000

    auto-update: grim: f63e1d2ed7245053a41d1e94c891c47b523772b6 => 0a2c5c91e42c3acae26ef34fca1540af8e725c0c

--- pkgs/grim/metadata.nix
+++ pkgs/grim/metadata.nix
@@ -5,6 +5,6 @@ rec {
   repo = "grim";
   repo_git = "https://${domain}/${owner}/${repo}";
   branch = "master";
-  rev = "f63e1d2ed7245053a41d1e94c891c47b523772b6";
-  sha256 = "sha256-F7uOTuJ0nAyVCWtQDJgCl2IcjCYXodpnXCWDXSMuEG8=";
+  rev = "0a2c5c91e42c3acae26ef34fca1540af8e725c0c";
+  sha256 = "sha256-XBIOv21GX1CNr209+W3+6SYdLnBYCk8/DVgAjVdtacI=";
 }
